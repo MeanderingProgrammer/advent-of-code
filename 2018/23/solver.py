54c7e6cd8ac5c96bd5aa43ff123c125facd9c172
MeanderingProgrammer <suslikovvd@gmail.com>
2021-04-01 21:53:55 -0700

    Copy super elegant solution for 2018 day 23 from github. Figure out roughly how it works, honestly still at kind of a loss. Update README.

--- 2018/23/solver.py
+++ 2018/23/solver.py
@@ -1,6 +1,7 @@
-import math
+import heapq
+from collections import defaultdict
 from aoc_parser import Parser
-from aoc_board import Grid, Point
+from aoc_board import Point
 
 
 FILE_NAME = 'data'
@@ -17,9 +18,8 @@ class NanoBot:
         diff = self.pos - o
         return len(diff) <= self.r
 
-    def cube(self):
-        coords = self.pos.coords
-        return Cube(*[(c + self.r, c - self.r) for c in coords])
+    def __len__(self):
+        return len(self.pos)
 
     def __lt__(self, o):
         return self.r < o.r
@@ -36,87 +36,35 @@ def main():
     bots.sort()
 
     strongest_bot = bots[-1]
-
-    # Part 1: 383
     bots_in_range = [bot for bot in bots if strongest_bot.in_range(bot.pos)]
+    # Part 1: 383
     print('Part 1: {}'.format(len(bots_in_range)))
 
     # Part 2: 100474026
-    bounds = get_bounds(bots)
-    for i in range(10):
-        best_value, best_bounds = 0, None
-        x_bounds, y_bounds, z_bounds = split_bounds(bounds, 10)
-        for x_bound in x_bounds:
-            for y_bound in y_bounds:
-                for z_bound in z_bounds:
-                    value = approximate_value(x_bound, y_bound, z_bound, bots)
-                    if value > best_value:
-                        best_bounds = [x_bound, y_bound, z_bound]
-                        best_value = value
-        print(bounds)
-        print(best_bounds)
-        print(best_value)
-        bounds = best_bounds
+    print('Part 2: {}'.format(distance_of_most_overlap(bots)))
 
 
-def get_bounds(bots):
-    xs, ys, zs = [], [], []
+def distance_of_most_overlap(bots):
+    # A copy of: https://github.com/tterb/advent-of-code/blob/master/2018/day23.py
+    queue = []
+
     for bot in bots:
-        coords = bot.pos.coords
-        xs.append(coords[0])
-        ys.append(coords[1])
-        zs.append(coords[2])
-    return [
-        (min(xs), max(xs)),
-        (min(ys), max(ys)),
-        (min(zs), max(zs))
-    ]
+        # Positions that have a manhattan distance larger than bots manhattan
+        # distance minus radius will be included by this bots radius
+        heapq.heappush(queue, (len(bot) - bot.r, True))
+        # Positions that have a manhattan distance larger than the bots manhattan
+        # distance plus its radius are no longer in the range of this bot
+        heapq.heappush(queue, (len(bot) + bot.r + 1, False))
 
+    bots_in_range, in_range_distances = 0, defaultdict(set)
+    while len(queue) > 0:
+        distance, add = heapq.heappop(queue)
+        bots_in_range += 1 if add else -1
+        in_range_distances[bots_in_range].add(distance)
 
-def split_bounds(bounds, value):
-    step_sizes = [math.ceil((bound[1] - bound[0]) / value) for bound in bounds]
-    x_bounds, y_bounds, z_bounds = [], [], []
-    for i in range(value):
-        bound_starts = [(bound[0] + (i * step_sizes[j])) for j, bound in enumerate(bounds)]
-        bound_splits = [(bound_start, bound_start + step_sizes[j]) for j, bound_start in enumerate(bound_starts)]
-
-        x_bounds.append(bound_splits[0])
-        y_bounds.append(bound_splits[1])
-        z_bounds.append(bound_splits[2])
-
-    return x_bounds, y_bounds, z_bounds
-
-
-def approximate_value(x_bound, y_bound, z_bound, bots):
-    n = 20
-    values = []
-    for i in range(n + 1):
-        test_position = Point(
-            (((i) * x_bound[1]) + ((n - i) * x_bound[0])) // n,
-            (((i) * y_bound[1]) + ((n - i) * y_bound[0])) // n,
-            (((i) * z_bound[1]) + ((n - i) * z_bound[0])) // n
-        )
-        value = in_range_of_bots(test_position, bots)
-        values.append(value)
-    return max(values)
-
-
-def binary_search(points, bots, start_value):
-    if len(points) == 1:
-        return points[0]
-
-    mid = len(points) // 2
-    point = points[mid]
-    value = len(in_range_of_bots(point, bots))
-
-    if value <= start_value:
-        return binary_search(points[:mid], bots, start_value)
-    else:
-        return binary_search(points[mid:], bots, value)
-
-
-def in_range_of_bots(pos, bots):
-    return len([bot for bot in bots if bot.in_range(pos)])
+    max_in_range = max(in_range_distances.keys())
+    distances = in_range_distances[max_in_range]
+    return min(distances)
 
 
 def get_bots():
