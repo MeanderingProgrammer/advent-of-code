e721cf6fa3d3f451650211d3277fbcb31e822732
MeanderingProgrammer <suslikovvd@gmail.com>
2021-04-04 14:22:43 -0700

    Update 2016 day 11, still not fast but finished under 2 minutes

--- 2016/11/aoc_search.py
+++ 2016/11/aoc_search.py
@@ -8,9 +8,6 @@ def bfs(start, end, get_adjacent):
     while len(queue) > 0:
         length, item = heapq.heappop(queue)
 
-        if item in seen:
-            continue
-
         if item == end:
             return length
 
@@ -19,23 +16,4 @@ def bfs(start, end, get_adjacent):
         for adjacent in get_adjacent(item):
             if adjacent not in seen:
                 heapq.heappush(queue, (length + 1, adjacent))
-
-
-def connected(graph, start):
-    queue = [start]
-    seen = set()
-
-    while len(queue) > 0:
-        current = queue.pop()
-
-        if current in seen:
-            continue
-
-        seen.add(current)
-
-        for adjacent in graph[current]:
-            if adjacent not in seen:
-                queue.append(adjacent)
-
-    return seen
-
+                seen.add(adjacent)
