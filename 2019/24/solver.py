669631135281b300c7a29e457807ba0c940e3d83
MeanderingProgrammer <suslikovvd@gmail.com>
2021-04-18 11:52:45 -0700

    Fix implementation for 2019 Day 24. Add grid parser to commons, build ys such that top line has highest value. Modify grid string to go through ys in reverse order.

--- 2019/24/solver.py
+++ 2019/24/solver.py
@@ -1,201 +1,132 @@
 from commons.aoc_parser import Parser
+from commons.aoc_board import Point, Grid
 
 
-INNER_GRID = '?'
 ALIVE = '#'
 EMPTY = '.'
 
-
-class Point:
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get(self, grid):
-        return grid[self.y][self.x]
-
-    def left(self):
-        return Point(self.x-1, self.y)
-
-    def right(self):
-        return Point(self.x+1, self.y)
-
-    def up(self):
-        return Point(self.x, self.y-1)
-
-    def down(self):
-        return Point(self.x, self.y+1)
-
-    def is_outside(self):
-        if self.x < 0 or self.y < 0:
-            return True
-        elif self.x > 4 or self.y > 4:
-            return True
-        else:
-            return False
-
-    def is_inside(self):
-        return self.x == 2 and self.y == 2
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return '({}, {})'.format(self.x, self.y)
+MIDDLE = Point(2, 2)
 
 
-class Element:
+class Layout:
 
-    def __init__(self, value='.'):
-        self.value = value
-        self.next_state = None
+    def __init__(self, grid, recursive):
+        self.grids = [grid]
+        self.recursive = recursive
 
-    def alive(self):
-        return 1 if self.value == ALIVE else 0
+    def step(self):
+        if self.recursive:
+            self.grids = [self.new_grid()] + self.grids + [self.new_grid()]
 
-    def set_next_state(self, count):
-        if self.value == ALIVE:
-            self.next_state = ALIVE if count == 1 else EMPTY
-        else:
-            self.next_state = ALIVE if count in [1, 2] else EMPTY
+        next_grids = []
 
-    def update_state(self):
-        self.value = self.next_state
-        self.next_state = None
+        for level, grid in enumerate(self.grids):
+            next_grid = Grid()
 
-    def __repr__(self):
-        return str(self)
+            for point, value in grid.items():
+                if not self.recursive or point != MIDDLE:
+                    bugs_adjacent = self.get_bugs_adjacent(level, point)
+                    if value == ALIVE:
+                        next_value = ALIVE if bugs_adjacent == 1 else EMPTY
+                    else:
+                        next_value = ALIVE if bugs_adjacent in [1, 2] else EMPTY
+                    next_grid[point] = next_value
 
-    def __str__(self):
-        return INNER_GRID if self.value is None else self.value
+            next_grids.append(next_grid)
 
+        self.grids = next_grids
 
-class Grid:
+    def new_grid(self):
+        grid = Grid()
+        for point, value in self.grids[0].items():
+            grid[point] = EMPTY
+        return grid
 
-    def __init__(self, values=None):
-        if values is None:
-            self.grid = [[Element() for c in range(5)] for r in range(5)]
-        else:
-            self.grid = [[Element(element) for element in value] for value in values]
-
-    def step(self, previous_level=None, next_level=None):
-        previous_counts = [
-            0 if previous_level is None else previous_level.inner_top(),
-            0 if previous_level is None else previous_level.inner_bottom(),
-            0 if previous_level is None else previous_level.inner_left(),
-            0 if previous_level is None else previous_level.inner_right()
-        ]
-        next_counts = [
-            0 if next_level is None else next_level.outer_bottom(),
-            0 if next_level is None else next_level.outer_top(),
-            0 if next_level is None else next_level.outer_right(),
-            0 if next_level is None else next_level.outer_left()
-        ]
-        for r, row in enumerate(self.grid):
-            for c, value in enumerate(row):
-                point = Point(c, r)
-                if not point.is_inside():
-                    count = self.count_adjacent(point, previous_counts, next_counts)
-                    value.set_next_state(count)
-
-    def update(self):
-        for row in self.grid:
-            for value in row:
-                value.update_state()
-
-    def inner_left(self):
-        return self.grid[2][1].alive()
-
-    def inner_right(self):
-        return self.grid[2][3].alive()
-
-    def inner_top(self):
-        return self.grid[1][2].alive()
-
-    def inner_bottom(self):
-        return self.grid[3][2].alive()
-
-    def outer_left(self):
-        return sum([row[0].alive() for row in self.grid])
-
-    def outer_right(self):
-        return sum([row[4].alive() for row in self.grid])
-
-    def outer_top(self):
-        return sum([value.alive() for value in self.grid[0]])
-
-    def outer_bottom(self):
-        return sum([value.alive() for value in self.grid[4]])
-
-    def count_adjacent(self, point, previous_counts, next_counts):
-        adjacent_points = [
-            (point.up(), previous_counts[0], next_counts[0]),
-            (point.down(), previous_counts[1], next_counts[1]),
-            (point.left(), previous_counts[2], next_counts[2]),
-            (point.right(), previous_counts[3], next_counts[3])
-        ]
-
-        counts = []
-        for adjacent_point, outside, inside in adjacent_points:
-            if adjacent_point.is_outside():
-                counts.append(outside)
-            elif adjacent_point.is_inside():
-                counts.append(inside)
+    def get_bugs_adjacent(self, level, point):
+        bugs = 0
+        current_grid = self.grids[level]
+        for adjacent in point.adjacent():
+            if self.recursive and adjacent == MIDDLE:
+                # Count the correct inside edge down one level
+                inner_level = level - 1
+                if inner_level >= 0:
+                    inner_grid = self.grids[inner_level]
+                    bugs += self.count_inner(point, inner_grid)
+            elif self.recursive and adjacent not in current_grid:
+                # Get the correct value up one level
+                outer_level = level + 1
+                if outer_level < len(self.grids):
+                    outer_grid = self.grids[outer_level]
+                    bugs += self.count_outer(point, adjacent, outer_grid)
             else:
-                counts.append(adjacent_point.get(self.grid).alive())
-        return sum(counts)
+                # A standard point that we can check at the current level
+                if current_grid[adjacent] == ALIVE:
+                    bugs += 1
+        return bugs
 
-    def count(self):
-        return sum(self.flatten())
+    def count_inner(self, original, grid):
+        if original.up() == MIDDLE:
+            # If going up from original led to the inner grid then we are
+            # interested in the bottom row of points
+            point_of_interest = lambda point: point.y() == 0
+        elif original.down() == MIDDLE:
+            # If going down original led to the inner grid then we are
+            # interested in the top row of points
+            point_of_interest = lambda point: point.y() == 4
+        elif original.left() == MIDDLE:
+            # If going left from original led to the inner grid then we are
+            # interested in the right most points
+            point_of_interest = lambda point: point.x() == 4
+        elif original.right() == MIDDLE:
+            # If going right from original led to the inner grid then we are
+            # interested in the left most points
+            point_of_interest = lambda point: point.x() == 0
+        else:
+            raise Exception('Unhandled original point')
+        return sum([value == ALIVE for point, value in grid.items() if point_of_interest(point)])
+
+    def count_outer(self, original, adjacent, grid):
+        if original.up() == adjacent:
+            # If going up from original led to the outer grid then we are
+            # interested in the point just above the middle
+            point_of_interest = MIDDLE.up()
+        elif original.down() == adjacent:
+            # If going down from original led to the outer grid then we are
+            # interested in the point just below the middle
+            point_of_interest = MIDDLE.down()
+        elif original.left() == adjacent:
+            # If going left from original led to the outer grid then we are
+            # interested in the point just to the left of the middle
+            point_of_interest = MIDDLE.left()
+        elif original.right() == adjacent:
+            # If going right from original led to the outer grid then we are
+            # interested in the point just to the right of the middle
+            point_of_interest = MIDDLE.right()
+        else:
+            raise Exception('Unhandled adjacent point')
+        return 1 if grid[point_of_interest] == ALIVE else 0
 
     def diversity(self):
         total = 0
-        for i, value in enumerate(self.flatten()):
-            total += pow(2, i) * value
+        for grid in self.grids:
+            total += sum([pow(2, self.to_index(point)) for point, value in grid.items() if value == ALIVE])
         return total
 
-    def flatten(self):
-        return [value.alive() for row in self.grid for value in row]
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return '\n'.join([''.join([str(element) for element in row]) for row in self.grid])
-
-
-class RecursiveGrid:
-
-    def __init__(self, values):
-        self.levels = [
-            Grid(values)
-        ]
-
-    def step(self):
-        self.levels = [Grid()] + self.levels + [Grid()]
-        total_levels = len(self.levels)
-        for index, level in enumerate(self.levels):
-            previous_level = self.levels[index-1] if index > 0 else None
-            next_level = self.levels[index+1] if index < total_levels - 1 else None
-            level.step(previous_level, next_level)
-        # Update all levels at end to avoid using updated state in next state calculations
-        [level.update() for level in self.levels]
-
     def count_bugs(self):
-        return sum([level.count() for level in self.levels])
+        total = 0
+        for grid in self.grids:
+            total += sum([value == ALIVE for point, value in grid.items()])
+        return total
 
     def __repr__(self):
         return str(self)
 
     def __str__(self):
-        result = []
-        current_level = ((len(self.levels) - 1) // 2) * -1
-        for level in self.levels:
-            result.append('Depth {}:'.format(current_level))
-            result.append('{}'.format(level))
-            current_level += 1
-        return '\n'.join(result)
+        return '\n\n'.join([str(grid) for grid in self.grids])
+
+    @staticmethod
+    def to_index(point):
+        return (point.y() * 5) + point.x()
 
 
 def main():
@@ -207,23 +138,22 @@ def main():
 
 def solve_part_1():
     seen = set()
-    grid = Grid(get_data())
-    while str(grid) not in seen:
-        seen.add(str(grid))
-        grid.step()
-        grid.update()
-    return grid.diversity()
+    layout = Layout(get_grid(), False)
+    while str(layout) not in seen:
+        seen.add(str(layout))
+        layout.step()
+    return layout.diversity()
 
 
 def solve_part_2():
-    grid = RecursiveGrid(get_data())
+    layout = Layout(get_grid(), True)
     for i in range(200):
-        grid.step()
-    return grid.count_bugs()
+        layout.step()
+    return layout.count_bugs()
 
 
-def get_data():
-    return Parser().nested_lines()
+def get_grid():
+    return Parser().as_grid()
 
 
 if __name__ == '__main__':
