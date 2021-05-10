b757b3e20253e6853d0e1c5b85d022237a98daab
MeanderingProgrammer <suslikovvd@gmail.com>
2021-05-09 19:10:54 -0700

    Update 2017 day 22 to use grid parser and store directions in a deque

--- 2017/22/solver.py
+++ 2017/22/solver.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from commons.aoc_parser import Parser
 from commons.aoc_board import Grid, Point
 
@@ -7,18 +9,9 @@ WEAKENED = 'W'
 FLAGGED = 'F'
 INFECTED = '#'
 
-U = Point(0, -1)
-D = Point(0, 1)
-L = Point(-1, 0)
-R = Point(1, 0)
-
-DIRECTIONS = [
-    U, L, D, R
-]
-
 STATE_DIRECTION_CHANGE = {
-    CLEAN: 1,
     WEAKENED: 0,
+    CLEAN: 1,
     FLAGGED: 2,
     INFECTED: -1
 }
@@ -30,12 +23,17 @@ class Virus:
         self.grid = grid
         self.state_chage = state_chage
         self.position = self.get_start_position()
-        self.direction_index = 0
+
+        self.directions = deque([
+            Point(0, 1), 
+            Point(-1, 0), 
+            Point(0, -1), 
+            Point(1, 0)
+        ])
         self.infections = 0
 
     def burst(self):
-        state = self.grid[self.position]
-        state = CLEAN if state is None else state
+        state = self.grid.get(self.position, CLEAN)
 
         new_state = self.state_chage[state]
         self.grid[self.position] = new_state
@@ -43,22 +41,19 @@ class Virus:
         if new_state == INFECTED:
             self.infections += 1
 
-        update_direction = STATE_DIRECTION_CHANGE[state]
-        self.direction_index += update_direction
-        self.direction_index %= len(DIRECTIONS)
-
-        self.position += DIRECTIONS[self.direction_index]
+        self.directions.rotate(-STATE_DIRECTION_CHANGE[state])
+        self.position += self.directions[0]
 
     def get_start_position(self):
-        xs = list(self.grid.xs())
-        xs.sort()
-        x = xs[len(xs)//2]
-
-        ys = list(self.grid.ys())
-        ys.sort()
-        y = ys[len(ys)//2]
-
+        x = self.mid(self.grid.xs())
+        y = self.mid(self.grid.ys())
         return Point(x, y)
+
+    @staticmethod
+    def mid(values):
+        values = list(values)
+        values.sort()
+        return values[len(values) // 2]
 
 
 def main():
@@ -91,12 +86,7 @@ def run(n, state_change):
 
 
 def get_grid():
-    grid = Grid()
-    for y, row in enumerate(Parser().nested_lines()):
-        for x, value in enumerate(row):
-            point = Point(x, y)
-            grid[point] = value
-    return grid
+    return Parser().as_grid()
 
 
 if __name__ == '__main__':
