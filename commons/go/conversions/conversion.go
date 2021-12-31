d39473e43cfdca4bd2500da84611d58386559a81
MeanderingProgrammer <suslikovvd@gmail.com>
2021-12-31 14:54:14 -0800

    Modify day 3 to use commons

--- commons/go/conversions/conversion.go
+++ commons/go/conversions/conversion.go
@@ -10,3 +10,9 @@ func ToInt(value string) int {
 	utils.CheckError(err)
 	return result
 }
+
+func BinaryToDecimal(value string) int {
+	result, err := strconv.ParseInt(value, 2, 64)
+	utils.CheckError(err)
+	return int(result)
+}
