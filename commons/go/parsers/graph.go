44faf08802c1fb374721c4d9b2d1f050e8d13b1d
MeanderingProgrammer <suslikovvd@gmail.com>
2021-12-31 16:36:24 -0800

    Add graph / grid utility. Update day 4 to use it.

---
+++ commons/go/parsers/graph.go
@@ -0,0 +1,78 @@
+package parsers
+
+import(
+	"fmt"
+	"strings"
+)
+
+type Point struct {
+    X int
+    Y int
+}
+
+type Graph struct {
+    Grid map[Point]string
+    height int
+    width int
+}
+
+func (graph Graph) GetPoint(target string) (Point, bool) {
+    for point, value := range graph.Grid {
+        if value == target {
+            return point, true
+        }
+    }
+    return Point{}, false
+}
+
+func (graph Graph) Print() {
+    for y := 0; y < graph.height; y++ {
+        for x := 0; x < graph.width; x++ {
+            point := Point{X: x, Y: y}
+            value := graph.Grid[point]
+            fmt.Print(value)
+        }
+        fmt.Println()
+    }
+}
+
+type RowSplitter int
+const (
+	Field     RowSplitter = iota
+	Character
+)
+
+func (splitter RowSplitter) get() func(string)[]string {
+	switch splitter {
+	case Field: return fieldSplitter
+	case Character: return characterSplitter
+	default: panic(fmt.Sprintf("Unknown splitter: %d", splitter))
+	}
+}
+
+func fieldSplitter(row string) []string {
+	return strings.Fields(row)
+}
+
+func characterSplitter(row string) []string {
+	var result []string
+	for _, char := range row {
+		result = append(result, string(char))
+	}
+	return result
+}
+
+func ConstructGraph(rows []string, splitter RowSplitter) Graph {
+	grid, f := make(map[Point]string), splitter.get()
+	for y, row := range rows {
+        for x, value := range f(row) {
+            point := Point{X: x, Y: y}
+            grid[point] = value
+        }
+    }
+	return Graph{
+        Grid: grid, 
+        height: len(rows), 
+        width: len(f(rows[0])), 
+    }
+}
