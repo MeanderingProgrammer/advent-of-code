7af5c1ce1533bc7abe137fe1d39c5822b2c1ccb7
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-02 10:33:56 -0800

    Use file reader for day 8

--- commons/go/conversions/conversion.go
+++ commons/go/conversions/conversion.go
@@ -11,6 +11,10 @@ func ToInt(value string) int {
 	return result
 }
 
+func ToString(value int) string {
+	return strconv.Itoa(value)
+}
+
 func BinaryToDecimal(value string) int {
 	result, err := strconv.ParseInt(value, 2, 64)
 	utils.CheckError(err)
