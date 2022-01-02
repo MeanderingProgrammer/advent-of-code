752295410b9bb2b7f2dc23d29c23b0583bbcadb5
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-01 23:33:03 -0800

    Move days 6 & 7 to use new file reading utilities.

--- 2021/07/solver.go
+++ 2021/07/solver.go
@@ -1,66 +1,51 @@
 package main
 
-import(
-    "advent-of-code/commons/go/answers"
-    "io/ioutil"
-    "sort"
-    "strings"
-    "strconv"
+import (
+	"advent-of-code/commons/go/answers"
+	"advent-of-code/commons/go/files"
+	"advent-of-code/commons/go/parsers"
+	"advent-of-code/commons/go/utils"
+	"sort"
 )
 
 type Positions []int
 
-func (positions Positions) difference(f func(int) int, value int) int {
-    total := 0
-    for _, position := range positions {
-        difference := position - value
-        total += f(difference)
+func (positions Positions) minimize(f func(int) int) int {
+    start, end := positions[0], positions[len(positions) - 1]
+    minimum := positions.cost(f, start)
+    for i := start + 1; i <= end; i++ {
+        value := positions.cost(f, i)
+        minimum = utils.Min(minimum, value)
     }
-    return total
+    return minimum
+}
+
+func (positions Positions) cost(f func(int) int, targetPosition int) int {
+    cost := 0
+    for _, position := range positions {
+        distance := utils.Abs(position - targetPosition)
+        cost += f(distance)
+    }
+    return cost
 }
 
 func main() {
-    positions := getData()
+    positions := getPositions()
 
-    answers.Part1(352331, minimize(positions, absolute))
-    answers.Part2(99266250, minimize(positions, cumulative))
+    answers.Part1(352331, positions.minimize(linear))
+    answers.Part2(99266250, positions.minimize(exponential))
 }
 
-func absolute(difference int) int {
-    if difference < 0 {
-        difference *= -1
-    }
-    return difference
+func linear(distance int) int {
+    return distance
 }
 
-func cumulative(difference int) int {
-    if difference < 0 {
-        difference *= -1
-    }
-    return ((difference * difference) + difference) / 2
+func exponential(distance int) int {
+    return ((distance * distance) + distance) / 2
 }
 
-func getData() Positions {
-    data, _ := ioutil.ReadFile("data.txt")
-    rawPositions := strings.Split(string(data), ",")
-
-    var positions []int
-    for _, rawPosition := range rawPositions {
-        position, _ := strconv.Atoi(rawPosition)
-        positions = append(positions, position)
-    }
-
+func getPositions() Positions {
+    positions := parsers.IntCsv(files.Content())
     sort.Ints(positions)
     return positions
-}
-
-func minimize(positions Positions, f func(int) int) int {
-    minimum := positions.difference(f, positions[0])
-    for i := positions[0] + 1; i <= positions[len(positions) - 1]; i++ {
-        value := positions.difference(f, i)
-        if value < minimum {
-            minimum = value
-        }
-    }
-    return minimum
 }
