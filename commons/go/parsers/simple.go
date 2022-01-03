48856703a8d07215304d9035480423b69b406381
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-03 11:11:26 -0800

    Minor changes to day 18

--- commons/go/parsers/simple.go
+++ commons/go/parsers/simple.go
@@ -9,6 +9,10 @@ func SubstringAfter(s, sep string) string {
 	return strings.SplitN(s, sep, 2)[1]
 }
 
+func SplitAt(s string, split int) (string, string) {
+	return s[:split], s[split + 1:]
+}
+
 func IntCsv(s string) []int {
 	var result []int
 	for _, value := range strings.Split(s, ",") {
