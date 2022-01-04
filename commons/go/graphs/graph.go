155418c2dcafdcda2716fdb95c026986da1cb024
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-04 01:58:11 -0800

    Use vertices instead of points, change CharacterState to just Character.

--- commons/go/graphs/graph.go
+++ commons/go/graphs/graph.go
@@ -4,18 +4,23 @@ import (
 	"advent-of-code/commons/go/parsers"
 )
 
-type Graph map[parsers.Point][]parsers.Point
+type Vertex struct {
+	Point parsers.Point
+	Value string
+}
 
-func ConstructGraph(grid parsers.Grid) Graph {
+type Graph map[Vertex][]Vertex
+
+func ConstructGraph(grid parsers.Grid, f func(parsers.Point) string) Graph {
 	graph := make(Graph)
 	for _, point := range grid.Points() {
-		var connected []parsers.Point
+		var connected []Vertex
 		for _, adjacent := range point.Adjacent(false) {
 			if grid.Contains(adjacent) {
-				connected = append(connected, adjacent)
+				connected = append(connected, Vertex{Point: adjacent, Value: f(adjacent)})
 			}
 		}
-		graph[point] = connected
+		graph[Vertex{Point: point, Value: f(point)}] = connected
 	}
 	return graph
 }
