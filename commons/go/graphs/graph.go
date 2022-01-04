a136692017829bf1d91139986f6b9417cd02dfaf
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-04 01:25:25 -0800

    Fix day 23, remove several constructs, mainly around relying on position to get types

--- commons/go/graphs/graph.go
+++ commons/go/graphs/graph.go
@@ -1,1 +1,21 @@
 package graphs
+
+import (
+	"advent-of-code/commons/go/parsers"
+)
+
+type Graph map[parsers.Point][]parsers.Point
+
+func ConstructGraph(grid parsers.Grid) Graph {
+	graph := make(Graph)
+	for _, point := range grid.Points() {
+		var connected []parsers.Point
+		for _, adjacent := range point.Adjacent(false) {
+			if grid.Contains(adjacent) {
+				connected = append(connected, adjacent)
+			}
+		}
+		graph[point] = connected
+	}
+	return graph
+}
