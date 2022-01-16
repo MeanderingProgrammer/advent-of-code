aaa0ffa0290deb624d8a2b8a89498bc49c8a1996
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-16 10:47:30 -0800

    Change Java file reader to use boolean on test mode, rather than the file name directly.

--- 2019/18/Solver.java
+++ 2019/18/Solver.java
@@ -6,15 +6,13 @@ import maze.Maze;
 
 public class Solver {
 
-    private static final String FILE_NAME = "data";
-
     public static void main(String[] args) {
         Answer.part1(5402, solve(false));
         Answer.part2(2138, solve(true));
     }
 
     private static int solve(boolean splitMaze) {
-        FileReader fileReader = new FileReader(String.format("%s.txt", FILE_NAME));
+        FileReader fileReader = new FileReader();
         Maze maze = new Maze(fileReader.read(), splitMaze);
         return maze.complete();
     }
