c96f3f46a58813c1aab0f73c2a5817ca81c0329e
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-17 14:43:33 -0800

    Test out adding some first class functions, update some of the implementations to use these.

---
+++ commons/go/utils/functions.go
@@ -0,0 +1,36 @@
+package utils
+
+func Contains[T comparable](values []T, target T) bool {
+	for _, value := range values {
+		if value == target {
+			return true
+		}
+	}
+	return false
+}
+
+func Filter[T any](values []T, f func(T) bool) []T {
+	var result []T
+	for _, value := range values {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+func Map[T any, U any](values []T, f func(T) U) []U {
+	var result []U
+	for _, value := range values {
+		result = append(result, f(value))
+	}
+	return result
+}
+
+func Sum(values []int) int {
+	total := 0
+	for _, value := range values {
+		total += value
+	}
+	return total
+}
