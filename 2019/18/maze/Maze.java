2ef9ac017441fe29976661330c6dce7ccce5977e
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-23 22:56:02 -0800

    Introduce rough idea of a state to 2019 day 18. Should bring it more in line with a standard graph traversal.

--- 2019/18/maze/Maze.java
+++ 2019/18/maze/Maze.java
@@ -11,6 +11,8 @@ import java.util.stream.Collectors;
 
 import pojo.Position;
 
+import lombok.Value;
+
 public class Maze {
 
     private final List<Grid> grids;
@@ -78,42 +80,67 @@ public class Maze {
         List<Position> startingPositions = grids.stream()
             .map(Grid::getStartingPosition)
             .collect(Collectors.toList());
-        return solve(startingPositions, new HashSet<>(), new HashMap<>());
+        return solve(new State(startingPositions, new HashSet<>()), new HashMap<>());
     }
 
-    private int solve(
-            List<Position> positions,
-            Set<Character> keysCollected,
-            Map<List<Position>, Map<Set<Character>, Integer>> cache
-    ) {
-        if (totalKeys() == keysCollected.size()) {
+    private int solve(State state, Map<List<Position>, Map<Set<Character>, Integer>> cache) {
+        if (state.getKeys().size() == totalKeys()) {
             return 0;
         }
 
         List<Integer> distances = new ArrayList<>();
-        for (int i = 0; i < grids.size(); i++) {
-            for (Path path : grids.get(i).get(positions.get(i)).getPaths()) {
-                if (!keysCollected.containsAll(path.getKeysNeeded()) || keysCollected.contains(path.getKey())) {
-                    continue;
-                }
+        for (Move move : getMoves(state)) {
+            State nextState = state.move(move);
+            int subDistance = cache
+                .computeIfAbsent(nextState.getPositions(), k -> new HashMap<>())
+                .computeIfAbsent(nextState.getKeys(), k -> solve(nextState, cache));
 
-                List<Position> nextPositions = new ArrayList<>(positions);
-                nextPositions.set(i, path.getKeyPosition());
-
-                Set<Character> nextKeys = new HashSet<>(keysCollected);
-                nextKeys.add(path.getKey());
-
-                int subDistance = cache
-                    .computeIfAbsent(nextPositions, k -> new HashMap<>())
-                    .computeIfAbsent(nextKeys, k -> solve(nextPositions, k, cache));
-
-                distances.add(path.getDistance() + subDistance);
-            }
+            distances.add(move.getPath().getDistance() + subDistance);
         }
         return Collections.min(distances);
     }
 
+    private Set<Move> getMoves(State state) {
+        Set<Move> moves = new HashSet<>();
+        for (int i = 0; i < grids.size(); i++) {
+            Node node = grids.get(i).get(state.getPositions().get(i));
+            for (Path path : node.getPaths()) {
+                if (state.getKeys().contains(path.getKey())) {
+                    continue;
+                }
+                if (!state.getKeys().containsAll(path.getKeysNeeded())) {
+                    continue;
+                }
+                moves.add(new Move(i, path));
+            }
+        }
+        return moves;
+    }
+
     private int totalKeys() {
         return grids.get(0).totalKeys();
+    }
+
+    @Value
+    private static class State {
+
+        List<Position> positions;
+        Set<Character> keys;
+
+        public State move(Move move) {
+            List<Position> nextPositions = new ArrayList<>(positions);
+            nextPositions.set(move.getI(), move.getPath().getKeyPosition());
+
+            Set<Character> nextKeys = new HashSet<>(keys);
+            nextKeys.add(move.getPath().getKey());
+
+            return new State(nextPositions, nextKeys);
+        }
+    }
+
+    @Value
+    private static class Move {
+        int i;
+        Path path;
     }
 }
