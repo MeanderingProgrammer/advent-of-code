cafc05751132f5e099ca802650ab82aba6a02553
MeanderingProgrammer <suslikovvd@gmail.com>
2022-01-23 16:09:05 -0800

    Update Java commons code to use Lombok

--- commons/java/Position.java
+++ commons/java/Position.java
@@ -3,23 +3,23 @@ package pojo;
 import java.util.HashSet;
 import java.util.Set;
 
+import lombok.AccessLevel;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class Position {
 
-    private final int x;
-    private final int y;
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
+    int x;
+    int y;
 
     public Set<Position> adjacent() {
         Set<Position> result = new HashSet<>();
@@ -28,24 +28,5 @@ public class Position {
         result.add(new Position(x, y+1));
         result.add(new Position(x, y-1));
         return result;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other instanceof Position) {
-            Position o = (Position) other;
-            return toString().equals(o.toString());
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return toString().hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return String.format("(%d, %d)", this.x, this.y);
     }
 }
