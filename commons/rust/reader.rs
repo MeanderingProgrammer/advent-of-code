5533e5ee0441354e020bc1d088fcd27af50fc736
MeanderingProgrammer <suslikovvd@gmail.com>
2022-11-17 01:05:38 -0800

    Minor formatting change

--- commons/rust/reader.rs
+++ commons/rust/reader.rs
@@ -6,7 +6,6 @@ pub fn read_int() -> Vec<i64> {
     read(|line| line.parse::<i64>().unwrap())
 }
 
-
 fn read<T>(f: fn(String) -> T) -> Vec<T> {
     let reader = File::open("data.txt")
         .map(|file| BufReader::new(file))
