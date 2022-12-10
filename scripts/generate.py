f631e51027fe860c4a68edca86397c62eb697dd7
MeanderingProgrammer <suslikovvd@gmail.com>
2022-12-10 14:28:54 -0800

    Add type hints and sample file creation to generate script

--- scripts/generate.py
+++ scripts/generate.py
@@ -5,6 +5,7 @@ import os
 from pathlib import Path
 
 from component.language_factory import LanguageFactory
+from language.language import Language
 from pojo.day import Day
 
 ADVENT_COOKIE_FILE = '.adventofcode.session'
@@ -20,30 +21,38 @@ def main(day: Day, language_name: str):
     language.template_processing(day)
 
     copy_template_to(language, solution_path)
-    get_data_if_necessary(day, date_path)
+    create_sample_if_necessary(date_path)
+    create_data_if_necessary(day, date_path)
 
 
-def get_date_path(day: Day):
+def get_date_path(day: Day) -> Path:
     # Create date directory, okay if it already exists
     date_path = Path(day.year).joinpath(day.day)
     date_path.mkdir(parents=True, exist_ok=True)
     return date_path
 
 
-def get_solution_path(language, date_path):
+def get_solution_path(language: Language, date_path: Path) -> Path:
     solution_path = date_path.joinpath(language.solution_file)
     if solution_path.exists():
         raise Exception(f'Solution already exists under: {solution_path}')
     return solution_path
 
 
-def copy_template_to(language, solution_path):
+def copy_template_to(language: Language, solution_path: Path):
     template_file = f'scripts/templates/{language.solution_file}'
     print(f'Copying {template_file} to {solution_path}')
     os.system(f'cp {template_file} {solution_path}')
 
 
-def get_data_if_necessary(day: Day, date_path):
+def create_sample_if_necessary(date_path: Path):
+    sample_path = date_path.joinpath('sample.txt')
+    if not sample_path.exists():
+        print(f'Creating empty {sample_path}')
+        sample_path.touch()
+
+
+def create_data_if_necessary(day: Day, date_path: Path):
     data_path = date_path.joinpath('data.txt')
     if not data_path.exists():
         print(f'Creating data file under: {data_path}')
@@ -51,7 +60,7 @@ def get_data_if_necessary(day: Day, date_path):
             print('Downloading input using aoc-cli')
             download_input(day, data_path)
         else:
-            print('Creating empty {data_path} since aoc-cli is not setup')
+            print(f'Creating empty {data_path} since aoc-cli is not setup')
             data_path.touch()
     else:
         print(f'{data_path} already exists, leaving as is')
