beccf864e590015149c437d6a59634f4f7150ddd
MeanderingProgrammer <suslikovvd@gmail.com>
2022-12-10 16:24:44 -0800

    2022 day 9, needed to update point class with distance calculator

--- commons/rust/point.rs
+++ commons/rust/point.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Point {
     values: Vec<i64>,
 }
@@ -16,39 +16,37 @@ impl Point {
         }
     }
 
-    pub fn x(&self) -> i64 {
-        self.get(0)
+    pub fn dimensions(&self) -> usize {
+        self.values.len()
     }
 
-    pub fn add_x(&self, value: i64) -> Self {
-        self.add(0, value)
+    pub fn x(&self) -> i64 {
+        self.get(0)
     }
 
     pub fn y(&self) -> i64 {
         self.get(1)
     }
 
-    pub fn add_y(&self, value: i64) -> Self  {
-        self.add(1, value)
-    }
-
     pub fn z(&self) -> i64 {
         self.get(2)
     }
 
-    pub fn add_z(&self, value: i64) -> Self  {
-        self.add(2, value)
-    }
-
-    fn check_index(&self, index: usize) {
-        if index >= self.values.len() {
-            panic!("Cannot get index {} from {:?}", index, self.values)
-        }
-    }
-
-    fn get(&self, index: usize) -> i64 {
+    pub fn get(&self, index: usize) -> i64 {
         self.check_index(index);
         self.values[index]
+    }
+
+    pub fn add_x(&self, value: i64) -> Self {
+        self.add(0, value)
+    }
+
+    pub fn add_y(&self, value: i64) -> Self  {
+        self.add(1, value)
+    }
+
+    pub fn add_z(&self, value: i64) -> Self  {
+        self.add(2, value)
     }
 
     fn add(&self, index: usize, value: i64) -> Self {
@@ -56,5 +54,23 @@ impl Point {
         let mut copied = self.values.clone();
         copied[index] += value;
         Point { values: copied }
+    }
+
+    pub fn distance(&self, other: &Self) -> f64 {
+        if self.dimensions() != other.dimensions() {
+            panic!("Cannot get distance from {:?} to {:?}", self, other);
+        }
+        let mut sum_squares = 0;
+        for i in 0..self.dimensions() {
+            let diff = self.get(i) - other.get(i);
+            sum_squares += diff.pow(2);
+        }
+        (sum_squares as f64).sqrt()
+    }
+
+    fn check_index(&self, index: usize) {
+        if index >= self.dimensions() {
+            panic!("Cannot get index {} from {:?}", index, self.values);
+        }
     }
 }
