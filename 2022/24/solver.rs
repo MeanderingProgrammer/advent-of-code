0aafa6f2c037b851d9bf010f185903012957e797
Vlad Suslikov <suslikovvd@gmail.com>
2022-12-23 22:49:26 -0800

    Some more bits of day 24

--- 2022/24/solver.rs
+++ 2022/24/solver.rs
@@ -1,32 +1,71 @@
 use aoc_lib::answer;
-use aoc_lib::grid::Grid;
+use aoc_lib::grid::{Bound, Grid};
+use aoc_lib::point::Point;
 use aoc_lib::reader;
+use std::collections::HashMap;
 
-type BlizzardGrid = Grid<char>;
-
-struct Blizzard {
-    grid: BlizzardGrid,
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Right,
+    Down, 
+    Left,
 }
 
-impl Blizzard {
+type BlizzardGrid = Grid<char>;
+type Blizzards = HashMap<Point, Direction>;
+
+struct Valley {
+    bounds: Bound,
+    blizzards: Blizzards,
+}
+
+impl Valley {
     fn new(grid: BlizzardGrid) -> Self {
-        Blizzard { grid }
+        let bounds = grid.bounds(0);
+        let mut blizzards = Blizzards::new();
+        grid.points().iter()
+            .map(|point| (point, grid.get(point)))
+            .filter(|(_, &value)| value != '.')
+            .for_each(|(&point, value)| {
+                let direction = match value {
+                    '^' => Direction::Up,
+                    '>' => Direction::Right,
+                    'v' => Direction::Down,
+                    '<' => Direction::Left,
+                    _ => unreachable!(),
+                };
+                blizzards.insert(point.clone(), direction);
+            });
+        Self { bounds, blizzards }
     }
 
-    fn next(&mut self) {
+    fn start(&self) -> &Point {
+        self.bounds.lower()
+    }
 
+    fn end(&self) -> &Point {
+        self.bounds.upper()
+    }
+
+    fn next(&self) {
     }
 }
 
 fn main() {
-    let mut blizzard = Blizzard::new(reader::read_grid(|ch| match ch {
-        '<' | '^' | '>' | 'v' => Some(ch),
+    let mut valley = Valley::new(reader::read_grid(|ch| match ch {
+        '<' | '^' | '>' | 'v' | '.' => Some(ch),
         _ => None,    
     }));
-    println!("{}", blizzard.grid);
+    println!("{:?}", valley.blizzards);
+    println!("{:?}", valley.bounds);
+    println!("{:?}", valley.start());
+    println!("{:?}", valley.end());
+
+
     println!("");
-    blizzard.next();
-    println!("{}", blizzard.grid);
+    valley.next();
+    println!("{:?}", valley.blizzards);
 
     //answer::part1(v1, s1);
     //answer::part2(v2, s2);
