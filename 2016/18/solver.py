c11c41a8e35a89965912e5b9912e48d94bf016ff
MeanderingProgrammer <suslikovvd@gmail.com>
2023-05-26 02:22:08 -0700

    Speed up 2016 day 18 by optimizing state change check

--- 2016/18/solver.py
+++ 2016/18/solver.py
@@ -13,40 +13,49 @@ def main():
 
 
 def total_safe(n):
-    rows = [STARTING_ROW]
-    while len(rows) < n:
-        next_row = get_next_row(rows[-1])
-        rows.append(next_row)
-    return count_safe(rows)
+    previous_row = STARTING_ROW
+    safe = count_safe(previous_row)
+    for _ in range(n - 1):
+        next_row = get_next_row(previous_row)
+        previous_row = next_row
+        safe += count_safe(next_row)
+    return safe
 
 
 def get_next_row(previous_row):
     next_row = []
     for i in range(len(previous_row)):
-        left = previous_row[i - 1] if i > 0 else SAFE
-        center = previous_row[i]
-        right = previous_row[i + 1] if i < len(previous_row) - 1 else SAFE
-        element = get_element(left, center, right)
+        element = get_element(
+            previous_row[i - 1] if i > 0 else SAFE,
+            previous_row[i + 1] if i < len(previous_row) - 1 else SAFE,
+        )
         next_row.append(element)
     return ''.join(next_row)
 
 
-def get_element(left, center, right):
-    possible_trap = [
-        left == TRAP and center == TRAP and right == SAFE,
-        left == SAFE and center == TRAP and right == TRAP,
-        left == TRAP and center == SAFE and right == SAFE,
-        left == SAFE and center == SAFE and right == TRAP
-    ]
-    return TRAP if any(possible_trap) else SAFE
+def get_element(left, right):
+    '''
+    Raw conditions for a TRAP can be summed up as an or of:
+    1)  L &  C & ~R
+    2) ~L &  C &  R
+    3)  L & ~C & ~R
+    4) ~L & ~C &  R
+
+    Doing some simple grouping we learn that the center value plays no role:
+    (L & C & ~R) | (L & ~C & ~R) -> L & (C | ~C) & ~R -> L & ~R
+    (~L & C & R) | (~L & ~C & R) -> ~L & (C | ~C) & R -> ~L & R
+
+    Further simplifies to an exclusive or, i.e. not equal (one true one false)
+    (L & ~R) | (~L & R) -> L ^ R
+    '''
+    return TRAP if left != right else SAFE
 
 
-def count_safe(rows):
+def count_safe(row):
     count = 0
-    for row in rows:
-        for element in row:
-            if element == SAFE:
-                count += 1
+    for element in row:
+        if element == SAFE:
+            count += 1
     return count
 
 
