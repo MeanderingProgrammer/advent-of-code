1d68fe6ca44bcb7be5183d00dc14f6d3bce2cca1
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-12 18:47:04 -0700

    Introduce buildSrc plugins to share common gradle configurations

---
+++ buildSrc/src/main/kotlin/solution.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("common")
+    application
+}
+
+application {
+    mainClass.set("Solver")
+}
--- commons/java/build.gradle.kts
+++ commons/java/build.gradle.kts
@@ -1,25 +1,4 @@
 plugins {
+    id("common")
     `java-library`
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("com.google.guava:guava:32.1.3-jre")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
-sourceSets {
-    named("main") {
-        java.srcDir("src")
-    }
 }
