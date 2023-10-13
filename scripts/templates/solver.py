41a16feb1412d7b1e9bc7325303c4081003f7774
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-13 03:09:17 -0700

    Update python template to use aoc module

--- scripts/templates/solver.py
+++ scripts/templates/solver.py
@@ -1,14 +1,14 @@
-import commons.answer as answer
-from commons.aoc_parser import Parser
+from aoc import answer
+from aoc.parser import Parser
 
 
 def main():
     data = Parser().string()
     print(data)
 
-    #answer.part1(v1, s1)
-    #answer.part2(v2, s2)
+    # answer.part1(v1, s1)
+    # answer.part2(v2, s2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
