84c41b18d6159a1fc2d470117fee07206cfa6a72
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-15 05:27:59 -0700

    Update 2018/15 to calculate distances using BFS to all other nodes in the graph rather than re-computing to check for specific start and end. Minor stylistic changes like storing characters in a single list with a type, rather than using separate lists

--- 2018/15/solver.py
+++ 2018/15/solver.py
@@ -1,17 +1,26 @@
-from aoc import answer, search
+from aoc import answer
 from aoc.board import Point
 from aoc.parser import Parser
 from dataclasses import dataclass
-from operator import attrgetter
-from typing import Callable, List, Optional, Set
+from enum import auto, Enum
+from typing import Callable, Dict, List, Optional, Set, Tuple
+
+
+class Team(Enum):
+    GOBLIN = auto()
+    ELF = auto()
 
 
 @dataclass
 class Character:
-    symbol: str
+    team: Team
     position: Point
     damage: int
     hp: int = 200
+
+    @property
+    def alive(self) -> bool:
+        return self.hp > 0
 
     def attack(self, target: "Character") -> None:
         target.hp -= self.damage
@@ -19,76 +28,63 @@ class Character:
     def find_target(self, opponents: List["Character"]) -> Optional["Character"]:
         reach = self.position.adjacent()
         targets = [opponent for opponent in opponents if opponent.position in reach]
-        targets.sort(key=attrgetter("hp", "position"))
-        return None if len(targets) == 0 else targets[0]
-
-    def alive(self) -> bool:
-        return self.hp > 0
-
-    def __lt__(self, o):
-        return self.position < o.position
+        if len(targets) == 0:
+            return None
+        return min(targets, key=lambda character: (character.hp, character.position))
 
 
 @dataclass
 class Game:
     open_path: Set[Point]
-    goblins: List[Character]
-    elves: List[Character]
-    round_num: int = 0
+    characters: List[Character]
+    until_elf_death: bool
 
-    def play(self) -> int:
+    def play(self) -> Optional[int]:
+        round = 0
         while True:
-            if not self.make_move():
-                break
-            self.round_num += 1
-        return self.get_result()
-
-    def play_until_elf_dies(self) -> Optional[int]:
-        initial_elves = len(self.elves)
-        while True:
-            if not self.make_move():
-                break
-            if len(self.elves) != initial_elves:
+            status = self.round()
+            if status is None:
                 return None
-            self.round_num += 1
-        return self.get_result()
+            if not status:
+                break
+            round += 1
+        return round * sum([character.hp for character in self.characters])
 
-    def get_result(self) -> int:
-        hps = [character.hp for character in self.characters()]
-        return self.round_num * sum(hps)
-
-    def make_move(self) -> bool:
-        for character in self.characters():
-            if not character.alive():
-                continue
-
-            opponents = self.elves if character.symbol == "G" else self.goblins
-            if len(opponents) == 0:
-                return False
-
-            target = character.find_target(opponents)
-            if target is None:
-                move = self.get_move(character, opponents)
-                if move is not None:
-                    character.position = move
-                    target = character.find_target(opponents)
-
-            if target is None:
-                continue
-            character.attack(target)
-            if not target.alive():
-                opponents.remove(target)
+    def round(self) -> Optional[bool]:
+        ordered = sorted(self.characters, key=lambda character: character.position)
+        for character in ordered:
+            if character.alive:
+                status = self.move(character)
+                if not status:
+                    return status
         return True
 
-    def characters(self) -> List[Character]:
-        characters = self.goblins + self.elves
-        characters.sort()
-        return characters
+    def move(self, character: Character) -> Optional[bool]:
+        opponents = [
+            opponent for opponent in self.characters if opponent.team != character.team
+        ]
+        if len(opponents) == 0:
+            return False
+
+        target = character.find_target(opponents)
+        if target is None:
+            move = self.get_move(character, opponents)
+            if move is not None:
+                character.position = move
+                target = character.find_target(opponents)
+
+        if target is not None:
+            character.attack(target)
+            if not target.alive:
+                self.characters.remove(target)
+                if self.until_elf_death and target.team == Team.ELF:
+                    return None
+        return True
 
     def get_move(
         self, character: Character, opponents: List[Character]
     ) -> Optional[Point]:
-        occupied = set([other.position for other in self.characters()])
+        occupied = set([other.position for other in self.characters])
 
         def get_adjacent(point: Point) -> List[Point]:
             result = []
@@ -97,36 +93,37 @@ class Game:
                     result.append(adjacent)
             return result
 
-        opponent_adjacent = []
+        targets = []
         for opponent in opponents:
-            opponent_adjacent.extend(get_adjacent(opponent.position))
+            targets.extend(get_adjacent(opponent.position))
+        distances = self.calculate_distances(character.position, get_adjacent)
 
-        closest = self.closest(character.position, opponent_adjacent, get_adjacent)
-        if closest is not None:
-            return self.closest(closest, get_adjacent(character.position), get_adjacent)
-        return None
+        target_distances = [
+            (distances[point][0], point) for point in targets if point in distances
+        ]
+        if len(target_distances) == 0:
+            return None
+        closest = min(target_distances)[1]
+        while distances[closest][0] > 1:
+            closest = distances[closest][1]
+        return closest
 
-    def closest(
+    def calculate_distances(
         self,
         start: Point,
-        options: List[Point],
         get_adjacent: Callable[[Point], List[Point]],
-    ) -> Optional[Point]:
-        distances = dict()
-        for option in options:
-            distance = search.bfs(start, option, get_adjacent)
-            if distance is not None:
-                distances[option] = distance
-
-        if len(distances) == 0:
-            return None
-
-        min_dist = min(distances.values())
-        minimums = [
-            point for point, distance in distances.items() if distance == min_dist
-        ]
-        minimums.sort()
-        return minimums[0]
+    ) -> Dict[Point, Tuple[int, Point]]:
+        queue: List[Tuple[int, Point]] = [(0, start)]
+        distances: Dict[Point, Tuple[int, Point]] = {start: (0, start)}
+        while len(queue) > 0:
+            length, point = queue.pop(0)
+            for adjacent in get_adjacent(point):
+                if adjacent not in distances:
+                    queue.append((length + 1, adjacent))
+                parent_distance = (length + 1, point)
+                if adjacent not in distances or distances[adjacent] > parent_distance:
+                    distances[adjacent] = parent_distance
+        return distances
 
 
 def main() -> None:
@@ -136,23 +133,19 @@ def main() -> None:
 
 
 def play_game(data: List[List[str]], until_win: bool) -> int:
-    if until_win:
-        elf_damage, result = 10, None
-        while result is None:
-            elf_damage += 1
-            game = get_game(data, elf_damage)
-            result = game.play_until_elf_dies()
-        return result
-    else:
-        game = get_game(data, 3)
-        return game.play()
+    elf_damage = 10 if until_win else 3
+    while True:
+        result = get_game(data, elf_damage, until_win).play()
+        if result is not None:
+            return result
+        elf_damage += 1
 
 
-def get_game(data: List[List[str]], elf_damage: int) -> Game:
+def get_game(data: List[List[str]], elf_damage: int, until_elf_death: bool) -> Game:
     return Game(
         open_path=get_open_path(data),
-        goblins=get_characters(data, "G", 3),
-        elves=get_characters(data, "E", elf_damage),
+        characters=get_characters(data, elf_damage),
+        until_elf_death=until_elf_death,
     )
 
 
@@ -166,15 +159,19 @@ def get_open_path(data: List[List[str]]) -> Set[Point]:
     return open_path
 
 
-def get_characters(data: List[List[str]], symbol: str, damage: int) -> List[Character]:
+def get_characters(data: List[List[str]], elf_damage: int) -> List[Character]:
+    traits_mapping = dict(
+        G=(Team.GOBLIN, 3),
+        E=(Team.ELF, elf_damage),
+    )
     characters = []
     for y, row in enumerate(data):
         for x, value in enumerate(row):
-            if value == symbol:
-                character = Character(
-                    symbol=symbol, position=Point(x, y), damage=damage
+            if value in traits_mapping:
+                traits = traits_mapping[value]
+                characters.append(
+                    Character(team=traits[0], position=Point(x, y), damage=traits[1])
                 )
-                characters.append(character)
     return characters
 
 
