cb941e3f689f0fcc60cc8fbfc552eb285f9a500a
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-15 02:47:54 -0700

    Cleanup 2018 day 15, this unfortunately does considerably slow things down

--- 2018/15/solver.py
+++ 2018/15/solver.py
@@ -1,218 +1,146 @@
-from aoc import answer
-from aoc.board import Grid, Point
+from aoc import answer, search
+from aoc.board import Point
 from aoc.parser import Parser
+from dataclasses import dataclass
 from operator import attrgetter
+from typing import Callable, List, Optional, Set
 
 
+@dataclass
 class Character:
-    def __init__(self, symbol, position, attack):
-        self.symbol = symbol
-        self.position = position
-        self.hp = 200
-        self.attack = attack
+    symbol: str
+    position: Point
+    damage: int
+    hp: int = 200
 
-    def attacked(self, character):
-        self.hp -= character.attack
+    def attack(self, target: "Character") -> None:
+        target.hp -= self.damage
 
-    def alive(self):
+    def find_target(self, opponents: List["Character"]) -> Optional["Character"]:
+        reach = self.position.adjacent()
+        targets = [opponent for opponent in opponents if opponent.position in reach]
+        targets.sort(key=attrgetter("hp", "position"))
+        return None if len(targets) == 0 else targets[0]
+
+    def alive(self) -> bool:
         return self.hp > 0
-
-    def __eq__(self, o):
-        return str(self) == str(o)
-
-    def __hash__(self):
-        return hash(str(self))
 
     def __lt__(self, o):
         return self.position < o.position
 
-    def __repr__(self):
-        return str(self)
 
-    def __str__(self):
-        return "{} HP = {} @ {}".format(self.symbol, self.hp, self.position)
-
-
+@dataclass
 class Game:
-    def __init__(self, grid, goblins, elves):
-        self.grid = grid
-        self.goblins = goblins
-        self.elves = elves
-        self.round_num = 0
-        self.num_elves = len(self.elves)
+    open_path: Set[Point]
+    goblins: List[Character]
+    elves: List[Character]
+    round_num: int = 0
 
-    def play(self):
+    def play(self) -> int:
         while True:
             if not self.make_move():
                 break
             self.round_num += 1
         return self.get_result()
 
-    def play_until_elf_dies(self):
+    def play_until_elf_dies(self) -> Optional[int]:
+        initial_elves = len(self.elves)
         while True:
             if not self.make_move():
                 break
-            if len(self.elves) != self.num_elves:
+            if len(self.elves) != initial_elves:
                 return None
             self.round_num += 1
         return self.get_result()
 
-    def get_result(self):
+    def get_result(self) -> int:
         hps = [character.hp for character in self.characters()]
         return self.round_num * sum(hps)
 
-    def make_move(self):
+    def make_move(self) -> bool:
         for character in self.characters():
-            if character in self.characters():
-                to_attack = self.can_attack(character)
-                if len(to_attack) == 0:
-                    if not self.move(character):
-                        return False
-                    to_attack = self.can_attack(character)
-                    if len(to_attack) > 0:
-                        self.attack(character, to_attack)
-                else:
-                    self.attack(character, to_attack)
+            if not character.alive():
+                continue
+
+            opponents = self.elves if character.symbol == "G" else self.goblins
+            if len(opponents) == 0:
+                return False
+
+            target = character.find_target(opponents)
+            if target is None:
+                move = self.get_move(character, opponents)
+                if move is not None:
+                    character.position = move
+                target = character.find_target(opponents)
+
+            if target is None:
+                continue
+            character.attack(target)
+            if not target.alive():
+                opponents.remove(target)
         return True
 
-    def characters(self):
+    def characters(self) -> List[Character]:
         characters = self.goblins + self.elves
         characters.sort()
         return characters
 
-    def opponents(self, character):
-        symbol = character.symbol
-        if symbol == "G":
-            return self.elves
-        elif symbol == "E":
-            return self.goblins
-        else:
-            raise Exception("Unknown symbol: {}".format(symbol))
+    def get_move(
+        self, character: Character, opponents: List[Character]
+    ) -> Optional[Point]:
+        occupied = set([other.position for other in self.characters()])
 
-    def can_attack(self, character):
-        symbol = character.symbol
-        adjacent = character.position.adjacent()
+        def get_adjacent(point: Point) -> List[Point]:
+            result = []
+            for adjacent in point.adjacent():
+                if adjacent in self.open_path and adjacent not in occupied:
+                    result.append(adjacent)
+            return result
 
-        to_attack = []
-        for opponent in self.opponents(character):
-            if opponent.position in adjacent:
-                to_attack.append(opponent)
-        to_attack.sort(key=attrgetter("hp", "position"))
-        return to_attack
-
-    def move(self, character):
-        seen = [other.position for other in self.characters()]
-        possibilities = self.get_possibilities(character, seen)
-        if possibilities is None:
-            return False
-
-        if len(possibilities) > 0:
-            closest = self.get_closest(character.position, possibilities, seen)
-            if closest is not None:
-                next_position = self.get_closest(
-                    closest, self.adjacent(character.position, set(seen)), seen
-                )
-                character.position = next_position
-        return True
-
-    def get_possibilities(self, character, seen):
-        opponents = self.opponents(character)
-        if len(opponents) == 0:
-            return None
-
-        possibilities = []
+        opponent_adjacent = []
         for opponent in opponents:
-            possibilities.extend(self.adjacent(opponent.position, set(seen)))
-        return possibilities
+            opponent_adjacent.extend(get_adjacent(opponent.position))
 
-    def adjacent(self, position, seen=None):
-        if seen is None:
-            seen = set()
-
-        result = []
-        for adjacent in position.adjacent():
-            if self.grid[adjacent] == "." and adjacent not in seen:
-                result.append(adjacent)
-        return result
-
-    def get_closest(self, start, possibilities, seen):
-        distances = []
-        for possibility in possibilities:
-            distance = self.distance(start, possibility, set(seen))
-            if distance is not None:
-                distances.append((possibility, distance))
-
-        if len(distances) > 0:
-            min_dist = min([distance[1] for distance in distances])
-            minimums = [
-                distance[0] for distance in distances if distance[1] == min_dist
-            ]
-            minimums.sort()
-            return minimums[0]
-
-    def distance(self, start, end, seen):
-        queue = [(start, 0)]
-        current = (None, None)
-
-        while len(queue) > 0 and current[0] != end:
-            current = queue.pop(0)
-            for adjacent in self.adjacent(current[0], seen):
-                queue.append((adjacent, current[1] + 1))
-                seen.add(adjacent)
-
-        return current[1] if current[0] == end else None
-
-    def attack(self, character, to_attack):
-        to_attack = to_attack[0]
-        to_attack.attacked(character)
-        if not to_attack.alive():
-            self.remove(to_attack)
-
-    def remove(self, character):
-        symbol = character.symbol
-        if symbol == "G":
-            self.goblins.remove(character)
-        elif symbol == "E":
-            self.elves.remove(character)
-        else:
-            raise Exception("Unknown symbol: {}".format(symbol))
-
-    def __getitem__(self, position):
-        for character in self.characters():
-            if character.position == position:
-                return character
+        closest = self.closest(character.position, opponent_adjacent, get_adjacent)
+        if closest is not None:
+            return self.closest(closest, get_adjacent(character.position), get_adjacent)
         return None
 
-    def __contains__(self, position):
-        return self[position] is not None
+    def closest(
+        self,
+        start: Point,
+        options: List[Point],
+        get_adjacent: Callable[[Point], List[Point]],
+    ) -> Optional[Point]:
+        distances = dict()
+        for option in options:
+            distance = search.bfs(start, option, get_adjacent)
+            if distance is not None:
+                distances[option] = distance
 
-    def __repr__(self):
-        return str(self)
+        if len(distances) == 0:
+            return None
 
-    def __str__(self):
-        result = []
-        for y, row in enumerate(str(self.grid).split("\n")):
-            result_row = []
-            for x, value in enumerate(row):
-                position = Point(x, y)
-                value = value if position not in self else self[position].symbol
-                result_row.append(value)
-            result.append("".join(result_row))
-        return "\n".join(result)
+        min_dist = min(distances.values())
+        minimums = [
+            point for point, distance in distances.items() if distance == min_dist
+        ]
+        minimums.sort()
+        return minimums[0]
 
 
-def main():
+def main() -> None:
     data = Parser().nested_lines()
     answer.part1(214731, play_game(data, False))
     answer.part2(53222, play_game(data, True))
 
 
-def play_game(data, play_until_elf_win):
-    if play_until_elf_win:
-        attack, result = 10, None
+def play_game(data: List[List[str]], until_win: bool) -> int:
+    if until_win:
+        elf_damage, result = 10, None
         while result is None:
-            attack += 1
-            game = get_game(data, attack)
+            elf_damage += 1
+            game = get_game(data, elf_damage)
             result = game.play_until_elf_dies()
         return result
     else:
@@ -220,29 +148,33 @@ def play_game(data, play_until_elf_win):
         return game.play()
 
 
-def get_game(data, attack):
+def get_game(data: List[List[str]], elf_damage: int) -> Game:
     return Game(
-        get_grid(data), get_characters(data, "G", 3), get_characters(data, "E", attack)
+        open_path=get_open_path(data),
+        goblins=get_characters(data, "G", 3),
+        elves=get_characters(data, "E", elf_damage),
     )
 
 
-def get_grid(data):
-    grid = Grid()
+def get_open_path(data: List[List[str]]) -> Set[Point]:
+    open_path = set()
     for y, row in enumerate(data):
         for x, value in enumerate(row):
-            point = Point(x, y)
-            value = "#" if value == "#" else "."
-            grid[point] = value
-    return grid
+            if value != "#":
+                point = Point(x, y)
+                open_path.add(point)
+    return open_path
 
 
-def get_characters(data, symbol, attack):
+def get_characters(data: List[List[str]], symbol: str, damage: int) -> List[Character]:
     characters = []
     for y, row in enumerate(data):
         for x, value in enumerate(row):
             if value == symbol:
-                point = Point(x, y)
-                characters.append(Character(symbol, point, attack))
+                character = Character(
+                    symbol=symbol, position=Point(x, y), damage=damage
+                )
+                characters.append(character)
     return characters
 
 
