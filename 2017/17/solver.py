75ed4874a9061e2871df297569b1edc08b0362ed
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-15 15:28:24 -0700

    Add type hints to 2017/17

--- 2017/17/solver.py
+++ 2017/17/solver.py
@@ -1,36 +1,28 @@
 from aoc import answer
 from collections import deque
 
-STEPS = 344
-
 
 class Lock:
-    def __init__(self, steps):
+    def __init__(self, steps: int):
         self.q = deque()
         self.steps = steps
 
-    def insert(self, value):
+    def insert(self, value: int) -> None:
         self.q.rotate(-(self.steps + 1))
         self.q.appendleft(value)
 
-    def after(self, value):
+    def after(self, value: int) -> int:
         index = self.q.index(value)
         return self.q[index + 1]
 
-    def __repr__(self):
-        return str(self)
 
-    def __str__(self):
-        return str(self.q)
-
-
-def main():
+def main() -> None:
     answer.part1(996, run_lock_after(2_017, 2_017))
     answer.part2(1898341, run_lock_after(50_000_000, 0))
 
 
-def run_lock_after(steps, after):
-    lock = Lock(STEPS)
+def run_lock_after(steps: int, after: int) -> int:
+    lock = Lock(344)
     for i in range(steps + 1):
         lock.insert(i)
     return lock.after(after)
