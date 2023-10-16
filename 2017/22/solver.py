20e467f1d45909b540a447c8f459a11e76fdbc5f
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-16 12:40:04 -0700

    Add type hints to 2017/22

--- 2017/22/solver.py
+++ 2017/22/solver.py
@@ -1,7 +1,8 @@
 from aoc import answer
-from aoc.board import Point
+from aoc.board import Grid, Point
 from aoc.parser import Parser
 from collections import deque
+from typing import Dict
 
 CLEAN = "."
 WEAKENED = "W"
@@ -12,30 +13,24 @@ STATE_DIRECTION_CHANGE = {WEAKENED: 0, CLEAN: 1, FLAGGED: 2, INFECTED: -1}
 
 
 class Virus:
-    def __init__(self, grid, state_chage):
+    def __init__(self, grid: Grid, state_chage: Dict[str, str]):
         self.grid = grid
         self.state_chage = state_chage
-        self.position = self.get_start_position()
+        self.position = Point(Virus.mid(grid.xs()), Virus.mid(grid.ys()))
 
         self.directions = deque([Point(0, 1), Point(-1, 0), Point(0, -1), Point(1, 0)])
         self.infections = 0
 
-    def burst(self):
-        state = self.grid.get(self.position, CLEAN)
+    def burst(self) -> None:
+        state: str = self.grid.get(self.position, CLEAN)
 
         new_state = self.state_chage[state]
         self.grid[self.position] = new_state
-
         if new_state == INFECTED:
             self.infections += 1
 
         self.directions.rotate(-STATE_DIRECTION_CHANGE[state])
         self.position += self.directions[0]
-
-    def get_start_position(self):
-        x = self.mid(self.grid.xs())
-        y = self.mid(self.grid.ys())
-        return Point(x, y)
 
     @staticmethod
     def mid(values):
@@ -44,7 +39,7 @@ class Virus:
         return values[len(values) // 2]
 
 
-def main():
+def main() -> None:
     simplified_state_change = {CLEAN: INFECTED, INFECTED: CLEAN}
     answer.part1(5575, run(10_000, simplified_state_change))
 
@@ -57,15 +52,11 @@ def main():
     answer.part2(2511991, run(10_000_000, expanded_state_change))
 
 
-def run(n, state_change):
-    virus = Virus(get_grid(), state_change)
-    for i in range(n):
+def run(n: int, state_change: Dict[str, str]) -> int:
+    virus = Virus(Parser().as_grid(), state_change)
+    for _ in range(n):
         virus.burst()
     return virus.infections
-
-
-def get_grid():
-    return Parser().as_grid()
 
 
 if __name__ == "__main__":
