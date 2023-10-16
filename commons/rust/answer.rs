3761b4b2444def5a588125d440ea265b7a24639b
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-15 18:58:30 -0700

    Migrate 2018/11 to rust, speeds up from 20 seconds to less than 1

--- commons/rust/answer.rs
+++ commons/rust/answer.rs
@@ -1,21 +1,20 @@
-use std::fmt::Display;
 use std::cmp::PartialEq;
+use std::fmt::Debug;
 
-
-pub trait Solution: Display + PartialEq {}
-impl<T: Display + PartialEq> Solution for T {}
+pub trait Solution: Debug + PartialEq {}
+impl<T: Debug + PartialEq> Solution for T {}
 
 pub fn part1<T: Solution>(expected: T, result: T) {
-	part(1, expected, result);
+    part(1, expected, result);
 }
 
 pub fn part2<T: Solution>(expected: T, result: T) {
-	part(2, expected, result);
+    part(2, expected, result);
 }
 
 fn part<T: Solution>(part: i64, expected: T, result: T) {
-	if expected != result {
-	    panic!("Part {part} incorrect, expected {expected} but got {result}");
-	}
-	println!("Part {part}: {result}");
+    if expected != result {
+        panic!("Part {part} incorrect, expected {expected:?} but got {result:?}");
+    }
+    println!("Part {part}: {result:?}");
 }
