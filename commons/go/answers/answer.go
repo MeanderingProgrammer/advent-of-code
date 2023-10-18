d610e0ddb23a74d5b157697a39356cb523e5ab6f
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-18 01:40:03 -0700

    Make golang answer module generic on input

--- commons/go/answers/answer.go
+++ commons/go/answers/answer.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 )
 
-func Part1(expected, result int) {
+func Part1[T comparable](expected, result T) {
 	part(1, expected, result)
 }
 
-func Part2(expected, result int) {
+func Part2[T comparable](expected, result T) {
 	part(2, expected, result)
 }
 
-func part(part, expected, result int) {
+func part[T comparable](part int, expected T, result T) {
 	if expected != result {
-		panic(fmt.Sprintf("Part %d incorrect, expected %d but got %d", part, expected, result))
+		panic(fmt.Sprintf("Part %d incorrect, expected %v but got %v", part, expected, result))
 	}
-	fmt.Println(fmt.Sprintf("Part %d: %d", part, result))
+	fmt.Println(fmt.Sprintf("Part %d: %v", part, result))
 }
