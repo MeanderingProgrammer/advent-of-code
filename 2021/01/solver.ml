d0927bf10afb2d55c2561cfa4eecddf343cd8222
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-20 14:52:22 -0700

    Use ocamlformat

--- 2021/01/solver.ml
+++ 2021/01/solver.ml
@@ -1,22 +1,21 @@
 let increases values =
-    let rec f acc values = match values with
-        | x1 :: xs -> (
-            match xs with
-                | x2 :: _ -> if x2 > x1
-                    then f (acc + 1) xs
-                    else f acc xs
-                | [] -> acc
-        )
-        | [] -> acc
-    in f 0 values
+  let rec f acc values =
+    match values with
+    | x1 :: x2 :: xs ->
+        if x2 > x1 then f (acc + 1) (x2 :: xs) else f acc (x2 :: xs)
+    | _ -> acc
+  in
+  f 0 values
 
 let winsorize values =
-    let rec f acc values = match values with
-        | x1 :: x2 :: x3 :: xs -> f ((x1 + x2 + x3) :: acc) (x2 :: x3 :: xs)
-        | _ -> acc
-    in f [] values
+  let rec f acc values =
+    match values with
+    | x1 :: x2 :: x3 :: xs -> f ((x1 + x2 + x3) :: acc) (x2 :: x3 :: xs)
+    | _ -> acc
+  in
+  f [] values
 
-let () = 
-    let values = Aoc.Reader.read_ints in
-        Aoc.Answer.part1 1292 (increases values) string_of_int;
-        Aoc.Answer.part2 1262 (increases (List.rev (winsorize values))) string_of_int
+let () =
+  let values = Aoc.Reader.read_ints in
+  Aoc.Answer.part1 1292 (increases values) string_of_int;
+  Aoc.Answer.part2 1262 (increases (List.rev (winsorize values))) string_of_int
--- commons/ocaml/answer.ml
+++ commons/ocaml/answer.ml
@@ -1,13 +1,10 @@
 exception Invalid_input
 
 let part n expected actual f =
-    if expected != actual then (
-        Printf.printf "Part %d: expected %s found %s\n" n (f expected) (f actual);
-        raise Invalid_input
-    ) else Printf.printf "Part %d: %s\n" n (f actual)
+  if expected != actual then (
+    Printf.printf "Part %d: expected %s found %s\n" n (f expected) (f actual);
+    raise Invalid_input)
+  else Printf.printf "Part %d: %s\n" n (f actual)
 
-let part1 expected actual f =
-    part 1 expected actual f
-
-let part2 expected actual f =
-    part 2 expected actual f
+let part1 expected actual f = part 1 expected actual f
+let part2 expected actual f = part 2 expected actual f
