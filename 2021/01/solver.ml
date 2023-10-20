b873f65cc83fd0ca9957060b0513813bb140852b
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-20 03:50:37 -0700

    Finish 2021/01 in ocaml

--- 2021/01/solver.ml
+++ 2021/01/solver.ml
@@ -1,16 +1,22 @@
-let part1 values = 
+let increases values =
     let rec f acc values = match values with
-        | v1 :: vs -> (
-            match vs with
-                | v2 :: _ -> if v2 > v1 
-                    then f (acc + 1) vs
-                    else f acc vs
+        | x1 :: xs -> (
+            match xs with
+                | x2 :: _ -> if x2 > x1
+                    then f (acc + 1) xs
+                    else f acc xs
                 | [] -> acc
         )
         | [] -> acc
     in f 0 values
 
+let winsorize values =
+    let rec f acc values = match values with
+        | x1 :: x2 :: x3 :: xs -> f ((x1 + x2 + x3) :: acc) (x2 :: x3 :: xs)
+        | _ -> acc
+    in f [] values
 
 let () = 
     let values = Aoc.Reader.read_ints in
-        Aoc.Answer.part1 1292 (part1 values) string_of_int
+        Aoc.Answer.part1 1292 (increases values) string_of_int;
+        Aoc.Answer.part2 1262 (increases (List.rev (winsorize values))) string_of_int
