707840464e173923405cd126725f4294615ba048
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-23 13:19:13 -0700

    Add file system struct to 2022/07 to avoid messing with tree directly

--- 2022/07/solver.rs
+++ 2022/07/solver.rs
@@ -3,104 +3,91 @@ use aoc_lib::reader;
 use id_tree::InsertBehavior;
 use id_tree::{Node, NodeId, Tree};
 
-#[allow(dead_code)]
 #[derive(Debug)]
-struct File {
-    name: String,
-    size: i64,
+struct FileSystem {
+    tree: Tree<i64>,
+    current: NodeId,
 }
 
-impl File {
-    fn new_node(name: String, size: i64) -> Node<Self> {
-        Node::new(File { name, size })
+impl FileSystem {
+    fn new() -> Self {
+        let mut tree = Tree::<i64>::new();
+        let current = tree.insert(Node::new(0), InsertBehavior::AsRoot).unwrap();
+        Self { tree, current }
+    }
+
+    fn move_up(&mut self) {
+        let current_node = self.tree.get(&self.current).unwrap();
+        self.current = current_node.parent().unwrap().clone();
+    }
+
+    fn move_down(&mut self) {
+        self.current = self.insert(0);
+    }
+
+    fn insert_file(&mut self, size: i64) {
+        self.insert(size);
+    }
+
+    fn insert(&mut self, size: i64) -> NodeId {
+        let behavior = InsertBehavior::UnderNode(&self.current);
+        self.tree.insert(Node::new(size), behavior).unwrap()
+    }
+
+    fn directories(&self) -> Vec<i64> {
+        self.tree
+            .traverse_pre_order(&self.tree.root_node_id().unwrap())
+            .unwrap()
+            .filter(|node| !node.children().is_empty())
+            .map(|node| self.get_size(node))
+            .collect()
+    }
+
+    fn get_size(&self, node: &Node<i64>) -> i64 {
+        let mut total_size = *node.data();
+        for child in node.children() {
+            total_size += self.get_size(self.tree.get(child).unwrap());
+        }
+        total_size
     }
 }
 
 fn main() {
     // Heavily inspired by https://fasterthanli.me/series/advent-of-code-2022/part-7
-    // I have a lot of gaps in my usage of borrows and mutable references, was really helpful!
+    let fs = get_file_system();
+    let directories = fs.directories();
 
-    let tree = create_file_system_tree();
+    let p1_directories = directories.iter().filter(|&&size| size <= 100_000);
+    answer::part1(1141028, p1_directories.sum());
 
-    let directory_sizes: Vec<i64> = tree
-        .traverse_pre_order(tree.root_node_id().unwrap())
-        .unwrap()
-        .filter(|node| !node.children().is_empty())
-        .map(|node| get_size(&tree, node))
-        .collect();
-
-    let size_used = directory_sizes.iter().max().unwrap();
+    let size_used = directories.iter().max().unwrap();
     let space_needed = 30_000_000 - (70_000_000 - size_used);
-
-    answer::part1(
-        1141028,
-        directory_sizes
-            .iter()
-            .filter(|&&size| size <= 100_000)
-            .sum(),
-    );
-    answer::part2(
-        8278005,
-        *directory_sizes
-            .iter()
-            .filter(|&&size| size >= space_needed)
-            .min()
-            .unwrap(),
-    );
+    let p2_directories = directories.iter().filter(|&&size| size >= space_needed);
+    answer::part2(8278005, *p2_directories.min().unwrap());
 }
 
-fn create_file_system_tree() -> Tree<File> {
-    let mut tree = Tree::<File>::new();
-    let root = tree
-        .insert(File::new_node("/".to_string(), 0), InsertBehavior::AsRoot)
-        .unwrap();
-    let mut current: NodeId = root;
-
-    let lines = reader::read_lines();
-    for line in lines {
+fn get_file_system() -> FileSystem {
+    let mut fs = FileSystem::new();
+    reader::read_lines().iter().for_each(|line| {
         let parts: Vec<&str> = line.split(" ").collect();
         match (parts[0], parts[1]) {
             ("$", "cd") => match parts[2] {
                 "/" => {
-                    // Do not support going back to root currently
+                    // Do not support going back to root
                 }
-                ".." => {
-                    current = tree.get(&current).unwrap().parent().unwrap().clone();
-                }
-                path => {
-                    current = tree
-                        .insert(
-                            File::new_node(path.to_string(), 0),
-                            InsertBehavior::UnderNode(&current),
-                        )
-                        .unwrap();
-                }
+                ".." => fs.move_up(),
+                _ => fs.move_down(),
             },
             ("$", "ls") => {
-                // Nothing to do when we see an ls, we just know that he next lines
-                // will inform our tree structure contents
+                // Nothing to do when we see an ls, we just know that the
+                // next lines will inform our file system structure
             }
             ("dir", _) => {
-                // A directory in a list does nothing, will process it when we change
-                // into the directory
+                // A directory in a list does nothing, will process it when
+                // we change into the directory
             }
-            (file_size, file_name) => {
-                tree.insert(
-                    File::new_node(file_name.to_string(), file_size.parse::<i64>().unwrap()),
-                    InsertBehavior::UnderNode(&current),
-                )
-                .unwrap();
-            }
+            (file_size, _) => fs.insert_file(file_size.parse().unwrap()),
         };
-    }
-
-    tree
-}
-
-fn get_size(tree: &Tree<File>, node: &Node<File>) -> i64 {
-    let mut total_size = node.data().size;
-    for child in node.children() {
-        total_size += get_size(tree, tree.get(child).unwrap());
-    }
-    total_size
+    });
+    fs
 }
