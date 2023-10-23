34cccf1d591ab8f02f08549a72b7008bada9e71a
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-23 13:53:54 -0700

    Split parsing and logic portions of 2022/09

--- 2022/09/solver.rs
+++ 2022/09/solver.rs
@@ -2,28 +2,72 @@ use aoc_lib::answer;
 use aoc_lib::point::Point;
 use aoc_lib::reader;
 use std::collections::HashSet;
+use std::str::FromStr;
 
-fn main() {
-    let lines = reader::read_lines();
-    answer::part1(6563, follow_trail(lines.as_slice(), 2));
-    answer::part2(2653, follow_trail(lines.as_slice(), 10));
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
 
-fn follow_trail(lines: &[String], length: usize) -> usize {
+impl FromStr for Direction {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "U" => Ok(Self::Up),
+            "D" => Ok(Self::Down),
+            "L" => Ok(Self::Left),
+            "R" => Ok(Self::Right),
+            _ => Err("Unkown direction value".to_string()),
+        }
+    }
+}
+
+impl Direction {
+    fn move_point(&self, p: &Point) -> Point {
+        match self {
+            Self::Up => p.add_y(1),
+            Self::Down => p.add_y(-1),
+            Self::Left => p.add_x(-1),
+            Self::Right => p.add_x(1),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Motion {
+    direction: Direction,
+    amount: i64,
+}
+
+impl FromStr for Motion {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (direction, amount) = s.split_once(" ").unwrap();
+        Ok(Self {
+            direction: direction.parse()?,
+            amount: amount.parse().unwrap(),
+        })
+    }
+}
+
+fn main() {
+    let motions = reader::read(|line| line.parse::<Motion>().unwrap());
+    answer::part1(6563, follow_trail(&motions, 2));
+    answer::part2(2653, follow_trail(&motions, 10));
+}
+
+fn follow_trail(motions: &Vec<Motion>, length: usize) -> usize {
     let mut rope = vec![Point::new(2); length];
     let mut tail_locations: HashSet<Point> = HashSet::new();
 
-    lines.iter().for_each(|line| {
-        let (direction, amount) = line.split_once(" ").unwrap();
-        let n = amount.parse::<i64>().unwrap();
-        for _ in 0..n {
-            rope[0] = match direction {
-                "R" => rope[0].add_x(1),
-                "L" => rope[0].add_x(-1),
-                "U" => rope[0].add_y(1),
-                "D" => rope[0].add_y(-1),
-                _ => unreachable!(),
-            };
+    motions.iter().for_each(|motion| {
+        for _ in 0..motion.amount {
+            rope[0] = motion.direction.move_point(&rope[0]);
             for i in 0..(rope.len() - 1) {
                 rope[i + 1] = adjust_trail(&rope[i], &rope[i + 1]);
             }
@@ -44,11 +88,9 @@ fn adjust_trail(front: &Point, back: &Point) -> Point {
 }
 
 fn get_adjustment(tail_val: i64, head_val: i64) -> i64 {
-    if tail_val < head_val {
-        1
-    } else if tail_val == head_val {
-        0
-    } else {
-        -1
+    match tail_val.cmp(&head_val) {
+        std::cmp::Ordering::Less => 1,
+        std::cmp::Ordering::Equal => 0,
+        std::cmp::Ordering::Greater => -1,
     }
 }
