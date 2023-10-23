0be75ec06e98d17158b3bc04fe34e6da4633ab23
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-23 12:21:58 -0700

    Use a closure with 2022/06

--- 2022/06/solver.rs
+++ 2022/06/solver.rs
@@ -4,14 +4,12 @@ use itertools::Itertools;
 
 fn main() {
     let data = reader::read_chars();
-
     answer::part1(1909, first_unique_sequence(&data, 4));
     answer::part2(3380, first_unique_sequence(&data, 14));
 }
 
 fn first_unique_sequence(chars: &Vec<char>, length: usize) -> usize {
-    (0..chars.len())
-        .position(|i| &chars[i..i + length].iter().unique().count() == &length)
-        .unwrap()
-        + length
+    let unique_length = |start| chars[start..start + length].iter().unique().count();
+    let start_pos = (0..chars.len()).position(|i| unique_length(i) == length);
+    start_pos.unwrap() + length
 }
