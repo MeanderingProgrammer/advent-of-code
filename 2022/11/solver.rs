316cbdab534a37f6e83970080a7f3ce7f61d6677
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-24 12:11:31 -0700

    Use nom parser for 2022/11, more lines but more robust, good writing practice

--- 2022/11/solver.rs
+++ 2022/11/solver.rs
@@ -1,10 +1,54 @@
 use aoc_lib::answer;
 use aoc_lib::reader;
+use nom::{
+    bytes::complete::{tag, take},
+    character::complete::{alphanumeric1, digit1, newline},
+    combinator::map_res,
+    multi::separated_list1,
+    sequence::tuple,
+    IResult,
+};
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum Operator {
+    Add,
+    Multiply,
+}
+
+impl FromStr for Operator {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "+" => Ok(Self::Add),
+            "*" => Ok(Self::Multiply),
+            _ => Err("Unknown operator".to_string()),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Value {
+    Old,
+    Number(i64),
+}
+
+impl FromStr for Value {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "old" => Ok(Self::Old),
+            num => Ok(Self::Number(num.parse().unwrap())),
+        }
+    }
+}
 
 #[derive(Debug)]
 struct Monkey {
     items: Vec<i64>,
-    expression: String,
+    operation: (Operator, Value),
     divisible_by: i64,
     true_monkey: usize,
     false_monkey: usize,
@@ -12,16 +56,63 @@ struct Monkey {
 }
 
 impl Monkey {
+    fn from_str(input: &str) -> IResult<&str, Self> {
+        // Monkey 0:
+        //  Starting items: 97, 81, 57, 57, 91, 61
+        //  Operation: new = old * 7
+        //  Test: divisible by 11
+        //    If true: throw to monkey 5
+        //    If false: throw to monkey 6
+
+        let (input, _) = tuple((tag("Monkey "), digit1, tag(":"), newline))(input)?;
+        let (input, items) = tuple((
+            tag("  Starting items: "),
+            separated_list1(tag(", "), map_res(digit1, |s: &str| s.parse())),
+            newline,
+        ))(input)?;
+        let (input, operation) = tuple((
+            tag("  Operation: new = old "),
+            map_res(take(1usize), |s: &str| s.parse()),
+            tag(" "),
+            map_res(alphanumeric1, |s: &str| s.parse()),
+            newline,
+        ))(input)?;
+        let (input, divisible_by) = tuple((
+            tag("  Test: divisible by "),
+            map_res(digit1, |s: &str| s.parse()),
+            newline,
+        ))(input)?;
+        let (input, true_monkey) = tuple((
+            tag("    If true: throw to monkey "),
+            map_res(digit1, |s: &str| s.parse()),
+            newline,
+        ))(input)?;
+        let (input, false_monkey) = tuple((
+            tag("    If false: throw to monkey "),
+            map_res(digit1, |s: &str| s.parse()),
+        ))(input)?;
+
+        Ok((
+            input,
+            Self {
+                items: items.1,
+                operation: (operation.1, operation.3),
+                divisible_by: divisible_by.1,
+                true_monkey: true_monkey.1,
+                false_monkey: false_monkey.1,
+                inspections: 0,
+            },
+        ))
+    }
+
     fn apply_operation(&self, item: i64) -> i64 {
-        let parts: Vec<&str> = self.expression.split(" ").collect();
-        let v2 = match parts[2] {
-            "old" => item,
-            num => num.parse::<i64>().unwrap(),
+        let v2 = match self.operation.1 {
+            Value::Old => item,
+            Value::Number(num) => num,
         };
-        match parts[1] {
-            "+" => item + v2,
-            "*" => item * v2,
-            _ => unreachable!(),
+        match self.operation.0 {
+            Operator::Add => item + v2,
+            Operator::Multiply => item * v2,
         }
     }
 }
@@ -33,12 +124,15 @@ struct ItemMove {
 }
 
 fn main() {
-    answer::part1(56350, get_monkey_business(20, true));
-    answer::part2(13954061248, get_monkey_business(10_000, false));
+    answer::part1(56350, monkey_business(20, true));
+    answer::part2(13954061248, monkey_business(10_000, false));
 }
 
-fn get_monkey_business(rounds: usize, reduce_worry: bool) -> i64 {
-    let mut monkeys = get_monkeys();
+fn monkey_business(rounds: usize, reduce_worry: bool) -> i64 {
+    let mut monkeys: Vec<Monkey> = reader::read_full_groups()
+        .iter()
+        .map(|group| Monkey::from_str(group).unwrap().1)
+        .collect();
 
     // This can be the least common multiple, but doesn't need to be to work
     let mut multiple_of_all = 1;
@@ -80,34 +174,4 @@ fn get_monkey_business(rounds: usize, reduce_worry: bool) -> i64 {
     inspections.reverse();
 
     inspections[0] * inspections[1]
-}
-
-fn get_monkeys() -> Vec<Monkey> {
-    reader::read_group_lines()
-        .iter()
-        .map(|group| Monkey {
-            items: group[1]
-                .split_once(": ")
-                .unwrap()
-                .1
-                .split(", ")
-                .map(|item| item.parse::<i64>().unwrap())
-                .collect(),
-            expression: group[2].split_once(" = ").unwrap().1.to_string(),
-            divisible_by: group[3].split(" ").last().unwrap().parse::<i64>().unwrap(),
-            true_monkey: group[4]
-                .split(" ")
-                .last()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap(),
-            false_monkey: group[5]
-                .split(" ")
-                .last()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap(),
-            inspections: 0,
-        })
-        .collect()
 }
