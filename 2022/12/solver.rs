f184007570a108dc7850eaee8b3600824d2e1d57
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-24 12:34:21 -0700

    Minor edits to 2022/12

--- 2022/12/solver.rs
+++ 2022/12/solver.rs
@@ -8,33 +8,28 @@ use std::collections::HashSet;
 fn main() {
     let (grid, start, end) = get_graph();
     answer::part1(472, bfs(&grid, &start, &end).unwrap());
-    answer::part2(465, shortest_bfs(&grid, &end));
+    answer::part2(465, shortest_bfs(&grid, &end).unwrap());
 }
 
-fn shortest_bfs(grid: &Grid<i64>, end: &Point) -> i64 {
+fn shortest_bfs(grid: &Grid<i64>, end: &Point) -> Option<i64> {
     grid.points_with_value(get_offset('a'))
         .iter()
         .map(|start| bfs(grid, start, end))
         .filter(|result| result.is_some())
         .map(|result| result.unwrap())
         .min()
-        .unwrap()
 }
 
 fn bfs(grid: &Grid<i64>, start: &Point, end: &Point) -> Option<i64> {
     let mut queue = PriorityQueue::new();
     queue.push(start.clone(), 0);
-
     let mut seen: HashSet<Point> = HashSet::new();
-
     while !queue.is_empty() {
         let (point, distance) = queue.pop().unwrap();
-        if point == end.clone() {
+        if &point == end {
             return Some(distance * -1);
-        } else {
-            seen.insert(point.clone());
         }
-
+        seen.insert(point.clone());
         let max_height = grid.get(&point) + 1;
         for neighbor in point.neighbors() {
             if !seen.contains(&neighbor) && can_go(grid, &neighbor, &max_height) {
@@ -42,7 +37,6 @@ fn bfs(grid: &Grid<i64>, start: &Point, end: &Point) -> Option<i64> {
             }
         }
     }
-
     None
 }
 
@@ -52,23 +46,14 @@ fn can_go(grid: &Grid<i64>, point: &Point, max_height: &i64) -> bool {
 
 fn get_graph() -> (Grid<i64>, Point, Point) {
     let mut grid = reader::read_grid(|ch| Some(get_offset(ch)));
-    let mut start = Point::new_2d(0, 0);
-    let mut end = Point::new_2d(0, 0);
 
-    for p in grid.points() {
-        let value = grid.get(p);
-        if value == &get_offset('S') {
-            start = p.to_owned().clone();
-        }
-        if value == &get_offset('E') {
-            end = p.to_owned().clone();
-        }
-    }
-
+    let start = grid.points_with_value(get_offset('S'))[0].clone();
     grid.add(start.clone(), get_offset('a'));
+
+    let end = grid.points_with_value(get_offset('E'))[0].clone();
     grid.add(end.clone(), get_offset('z'));
 
-    (grid, start, end)
+    (grid, start.clone(), end.clone())
 }
 
 fn get_offset(ch: char) -> i64 {
