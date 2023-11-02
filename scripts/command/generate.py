a7cd42036050ebc9f680cde5f8b7cb8515e26962
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-02 13:14:50 -0700

    Minor changes to generate script

--- scripts/command/generate.py
+++ scripts/command/generate.py
@@ -6,8 +6,6 @@ from pathlib import Path
 from language.language import Language
 from pojo.day import Day
 
-ADVENT_COOKIE_FILE = ".adventofcode.session"
-
 
 @dataclass(frozen=True)
 class Generator:
@@ -15,35 +13,27 @@ class Generator:
     language: Language
 
     def generate(self) -> None:
-        date_path = self.__get_date_path()
-        self.__check_solution_path(date_path)
-
-        # At this point we can assume this is the first time we are processing
-        # this day for this language, since the solution path does not exist
-        self.__copy_template_to(date_path)
-        self.language.template_processing(self.day)
-        self.__create_sample_if_necessary(date_path)
-        self.__create_data_if_necessary(date_path)
-
-    def __get_date_path(self) -> Path:
-        # Create date directory, okay if it already exists
-        date_path = Path(self.day.year).joinpath(self.day.day)
-        date_path.mkdir(parents=True, exist_ok=True)
-        return date_path
-
-    def __check_solution_path(self, date_path: Path) -> None:
-        solution_path = date_path.joinpath(self.language.solution_file)
+        # Create day directory, okay if it already exists
+        self.day.dir().mkdir(parents=True, exist_ok=True)
+        solution_path = self.day.dir().joinpath(self.language.solution_file)
         if solution_path.exists():
             raise Exception(f"Solution already exists under: {solution_path}")
 
-    def __copy_template_to(self, date_path: Path) -> None:
+        # At this point we can assume this is the first time we are processing
+        # this day for this language, since the solution path does not exist
+        self.__copy_template_files()
+        self.language.template_processing(self.day)
+        self.__create_sample()
+        self.__create_data()
+
+    def __copy_template_files(self) -> None:
         template_directory = Path(f"scripts/templates/{self.language.name}")
         if not template_directory.is_dir():
             raise Exception(f"No template defined in {template_directory}")
         for template_file in template_directory.glob("**/*"):
             if template_file.is_file():
                 file_path = template_file.relative_to(template_directory)
-                destination = date_path.joinpath(file_path)
+                destination = self.day.dir().joinpath(file_path)
                 # Create parent directory if needed for nested solution files
                 if not destination.parent.exists():
                     destination.parent.mkdir(parents=True)
@@ -51,32 +41,31 @@ class Generator:
                     print(f"Copying {template_file} to {destination}")
                     os.system(f"cp {template_file} {destination}")
 
-    def __create_sample_if_necessary(self, date_path: Path) -> None:
-        sample_path = date_path.joinpath("sample.txt")
+    def __create_sample(self) -> None:
+        sample_path = self.day.dir().joinpath("sample.txt")
         if not sample_path.exists():
             print(f"Creating empty {sample_path}")
             sample_path.touch()
 
-    def __create_data_if_necessary(self, date_path: Path) -> None:
-        data_path = date_path.joinpath("data.txt")
-        if not data_path.exists():
-            print(f"Creating data file under: {data_path}")
-            if Path(ADVENT_COOKIE_FILE).exists() and shutil.which("aoc") is not None:
-                print("Downloading input using aoc-cli")
-                self.__download_input(data_path)
-            else:
-                print(f"Creating empty {data_path} since aoc-cli is not setup")
-                data_path.touch()
-        else:
+    def __create_data(self) -> None:
+        data_path = self.day.dir().joinpath("data.txt")
+        if data_path.exists():
             print(f"{data_path} already exists, leaving as is")
+            return
 
-    def __download_input(self, data_path: Path) -> None:
-        download_command = [
-            "aoc download",
-            f"--year {self.day.year}",
-            f"--day {self.day.day}",
-            f"--input-file {data_path}",
-            "--input-only",
-            f"--session-file ./{ADVENT_COOKIE_FILE}",
-        ]
-        os.system(" ".join(download_command))
+        print(f"Creating data file at: {data_path}")
+        advent_cookie_file = ".adventofcode.session"
+        if Path(advent_cookie_file).exists() and shutil.which("aoc") is not None:
+            print("Downloading input using aoc-cli")
+            download_command = [
+                "aoc download",
+                f"--year {self.day.year}",
+                f"--day {self.day.day}",
+                f"--input-file {data_path}",
+                "--input-only",
+                f"--session-file ./{advent_cookie_file}",
+            ]
+            os.system(" ".join(download_command))
+        else:
+            print(f"Creating empty {data_path} since aoc-cli is not setup")
+            data_path.touch()
