9f36b246cb9fe10a8652d14b07a37ff91dfb80e8
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-03 09:17:54 -0700

    Provide a metavar implementation for language which uses the Choice logic.

--- scripts/advent.py
+++ scripts/advent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from typing import Optional, Tuple
+from typing import Optional, Tuple, override
 
 import click
 from args.generate_template import GenerateTemplate
@@ -14,13 +14,18 @@ from language.language import Language
 
 class LanguageType(click.ParamType):
     name = "language"
+    factory = LanguageFactory()
 
-    def convert(self, value, param, ctx) -> Language:
-        factory = LanguageFactory()
-        if value in factory.get_names():
-            return factory.get_by_name(value)
+    @override
+    def get_metavar(self, param) -> str:
+        return click.Choice(self.factory.get_names()).get_metavar(param)
+
+    @override
+    def convert(self, value: str, param, ctx) -> Language:
+        if value in self.factory.get_names():
+            return self.factory.get_by_name(value)
         else:
-            error = f"{value} is not a valid language: {factory.get_names()}"
+            error = f"{value} is not a valid language: {self.factory.get_names()}"
             self.fail(error, param, ctx)
 
 
