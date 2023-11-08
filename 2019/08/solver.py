362a160000e444e97f5f754dd0c5d7909ae44878
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-07 23:57:03 -0800

    Update 2019/08 to use answer module, add type hints

--- 2019/08/solver.py
+++ 2019/08/solver.py
@@ -1,76 +1,72 @@
+from dataclasses import dataclass
+from typing import List
+
 from aoc import answer
 from aoc.parser import Parser
 
 
+@dataclass(frozen=True)
 class Layer:
-    def __init__(self):
-        self.data = []
+    data: List[List[int]]
 
-    def add(self, row):
-        row = [int(pixel) for pixel in row]
-        self.data.append(row)
-
-    def get_count(self, value):
+    def get_count(self, value: int) -> int:
         return sum([pixel == value for row in self.data for pixel in row])
 
-    def get_pixel(self, r, c):
+    def get_pixel(self, r: int, c: int) -> int:
         return self.data[r][c]
 
-    def __repr__(self):
-        return str(self)
 
-    def __str__(self):
-        return str(self.data)
-
-
+@dataclass(frozen=True)
 class LayeredImage:
-    def __init__(self, data, width, height):
-        self.layers = []
-        self.width, self.height = width, height
+    layers: List[Layer]
+    width: int
+    height: int
 
-        layer_size = width * height
-        for l in range(len(data) // layer_size):
-            layer_start = l * layer_size
-            layer = Layer()
-            for h in range(height):
-                row_start = (h * width) + layer_start
-                row = data[row_start : row_start + width]
-                layer.add(row)
-            self.layers.append(layer)
-
-    def get_fewest(self):
+    def get_fewest(self) -> Layer:
         counts = [layer.get_count(0) for layer in self.layers]
         return self.layers[counts.index(min(counts))]
 
-    def flatten(self):
+    def flatten(self) -> str:
         rows = []
         for r in range(self.height):
-            row = []
-            for c in range(self.width):
-                row.append(self.get_pixel(r, c))
+            row = [self.get_pixel(r, c) for c in range(self.width)]
             rows.append("".join(row))
         return "\n".join(rows)
 
-    def get_pixel(self, r, c):
+    def get_pixel(self, r: int, c: int) -> str:
         pixels = [layer.get_pixel(r, c) for layer in self.layers]
         pixel = next(pixel for pixel in pixels if pixel != 2)
         return "." if pixel == 0 else "#"
 
-    def __str__(self):
-        return str(self.layers)
 
-
-def main():
-    image = LayeredImage(get_image_data(), 25, 6)
+def main() -> None:
+    image = get_image(25, 6)
     layer = image.get_fewest()
     answer.part1(1965, layer.get_count(1) * layer.get_count(2))
-    # Part 2: GZKJY
-    print("Part 2")
-    print(image.flatten())
+    expected = [
+        ".##..####.#..#...##.#...#",
+        "#..#....#.#.#.....#.#...#",
+        "#......#..##......#..#.#.",
+        "#.##..#...#.#.....#...#..",
+        "#..#.#....#.#..#..#...#..",
+        ".###.####.#..#..##....#..",
+    ]
+    answer.part2("\n" + "\n".join(expected), "\n" + image.flatten())
 
 
-def get_image_data():
-    return Parser().string()
+def get_image(width: int, height: int) -> LayeredImage:
+    data = Parser().string()
+    layers = []
+    layer_size = width * height
+    for l in range(len(data) // layer_size):
+        layer_start = l * layer_size
+        layer_data = []
+        for h in range(height):
+            row_start = (h * width) + layer_start
+            row = data[row_start : row_start + width]
+            layer_data.append(list(map(int, row)))
+        layers.append(Layer(layer_data))
+    return LayeredImage(layers, width, height)
 
 
 if __name__ == "__main__":
