8390c703df9022d7376b557a4c752a34c8044032
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-08 00:17:25 -0800

    Update 2019/11 to use answer module, add type hints

--- 2019/11/solver.py
+++ 2019/11/solver.py
@@ -1,23 +1,28 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+
 from aoc import answer
+from aoc.board import Grid, Point
 from aoc.int_code import Computer
 from aoc.parser import Parser
 
+DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
+
+@dataclass
 class Direction:
-    def __init__(self):
-        self.index = 0
-        self.directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    index: int = 0
 
-    def rotate(self, value):
+    def rotate(self, value: int) -> None:
         self.index += value if value == 1 else -1
 
-    def step(self, position):
-        to_go = self.directions[self.index % len(self.directions)]
+    def step(self, position: Tuple[int, int]) -> Tuple[int, int]:
+        to_go = DIRECTIONS[self.index % len(DIRECTIONS)]
         return position[0] + to_go[0], position[1] + to_go[1]
 
 
 class PaintBot:
-    def __init__(self, memory, starting_color):
+    def __init__(self, memory: List[int], starting_color: int):
         self.computer = Computer(self)
         self.computer.set_memory(memory)
 
@@ -28,13 +33,13 @@ class PaintBot:
         self.position = (0, 0)
         self.grid[self.position] = starting_color
 
-    def run(self):
+    def run(self) -> None:
         self.computer.run()
 
-    def get_input(self):
+    def get_input(self) -> int:
         return self.grid[self.position] if self.position in self.grid else 0
 
-    def add_output(self, value):
+    def add_output(self, value: int) -> None:
         if self.color:
             self.grid[self.position] = value
         else:
@@ -42,36 +47,31 @@ class PaintBot:
             self.position = self.direction.step(self.position)
         self.color = not self.color
 
-    def get_grid(self):
-        xs = [position[0] for position in self.grid]
-        ys = [position[1] for position in self.grid]
-
-        rows = []
-        for y in range(min(ys), max(ys) + 1):
-            row = []
-            for x in range(max(xs), min(xs) - 1, -1):
-                value = self.grid.get((x, y), 0)
-                value = "." if value == 0 else "#"
-                row.append(value)
-            rows.append("".join(row))
-        return "\n".join(rows)
+    def get_grid(self) -> Grid:
+        grid = Grid()
+        for position, value in self.grid.items():
+            point = Point(*position)
+            grid[point] = "." if value == 0 else "#"
+        return grid
 
 
 def main():
     answer.part1(1909, len(run(0).grid))
-    # Part 2: JUFEKHPH
-    print("Part 2")
-    print(run(1).get_grid())
+    expected = [
+        "...##.#..#.####.####.#..#.#..#.###..#..#...",
+        "....#.#..#.#....#....#.#..#..#.#..#.#..#...",
+        "....#.#..#.###..###..##...####.#..#.####...",
+        "....#.#..#.#....#....#.#..#..#.###..#..#...",
+        ".#..#.#..#.#....#....#.#..#..#.#....#..#...",
+        "..##...##..#....####.#..#.#..#.#....#..#...",
+    ]
+    answer.part2("\n" + "\n".join(expected), "\n" + str(run(1).get_grid()))
 
 
-def run(setting):
-    bot = PaintBot(get_memory(), setting)
+def run(setting: int) -> PaintBot:
+    bot = PaintBot(Parser().int_csv(), setting)
     bot.run()
     return bot
-
-
-def get_memory():
-    return Parser().int_csv()
 
 
 if __name__ == "__main__":
