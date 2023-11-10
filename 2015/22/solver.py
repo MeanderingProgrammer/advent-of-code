370c43c129afa53e6abde64b86066d5ec90854f4
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-10 11:59:50 -0800

    Remove need to unsafe hash by copying to new objects, generalize everything to Stats

--- 2015/22/solver.py
+++ 2015/22/solver.py
@@ -5,99 +5,92 @@ from aoc import answer, search
 from aoc.parser import Parser
 
 
-@dataclass(unsafe_hash=True, order=True)
-class Character:
-    hp: int
-    attack: int
-    armor: int
+@dataclass(frozen=True, order=True)
+class Stats:
+    hp: int = 0
+    attack: int = 0
+    armor: int = 0
 
-    def add(self, values: tuple[int, int, int]) -> None:
-        self.hp += values[0]
-        self.attack += values[1]
-        self.armor += values[2]
+    def add(self, other: Self) -> Self:
+        return Stats(
+            hp=self.hp + other.hp,
+            attack=self.attack + other.attack,
+            armor=self.armor + other.armor,
+        )
 
-    def copy(self) -> Self:
-        return Character(self.hp, self.attack, self.armor)
+    def subtract(self, other: Self) -> Self:
+        return Stats(
+            hp=self.hp - other.hp,
+            attack=self.attack - other.attack,
+            armor=self.armor - other.armor,
+        )
 
 
 @dataclass(frozen=True)
 class SpellEffect:
-    boost: tuple[int, int, int]
-    player: tuple[int, int, int]
-    enemy: tuple[int, int, int]
+    cost: int
+    boost: Stats = Stats()
+    player: Stats = Stats()
+    enemy: Stats = Stats()
 
-    def setup(self, player: Character) -> None:
-        player.add(self.boost)
+    def setup(self, player: Stats) -> Stats:
+        return player.add(self.boost)
 
-    def turn(self, player: Character, enemy: Character) -> None:
-        player.add(self.player)
-        enemy.add(self.enemy)
+    def turn(self, player: Stats, enemy: Stats) -> tuple[Stats, Stats]:
+        return player.add(self.player), enemy.add(self.enemy)
 
-    def cleanup(self, player: Character) -> None:
-        player.add((-self.boost[0], -self.boost[1], -self.boost[2]))
+    def cleanup(self, player: Stats) -> Stats:
+        return player.subtract(self.boost)
 
 
 EFFECTS: dict[str, SpellEffect] = {
-    "Magic Missile": SpellEffect(boost=(0, 0, 0), player=(0, 0, 0), enemy=(-4, 0, 0)),
-    "Drain": SpellEffect(boost=(0, 0, 0), player=(2, 0, 0), enemy=(-2, 0, 0)),
-    "Shield": SpellEffect(boost=(0, 0, 7), player=(0, 0, 0), enemy=(0, 0, 0)),
-    "Poison": SpellEffect(boost=(0, 0, 0), player=(0, 0, 0), enemy=(-3, 0, 0)),
-    "Recharge": SpellEffect(boost=(0, 0, 0), player=(0, 101, 0), enemy=(0, 0, 0)),
+    "Magic Missile": SpellEffect(cost=53, enemy=Stats(hp=-4)),
+    "Drain": SpellEffect(cost=73, player=Stats(hp=2), enemy=Stats(hp=-2)),
+    "Shield": SpellEffect(cost=113, boost=Stats(armor=7)),
+    "Poison": SpellEffect(cost=173, enemy=Stats(hp=-3)),
+    "Recharge": SpellEffect(cost=229, player=Stats(attack=101)),
 }
 
 
-@dataclass(unsafe_hash=True)
+@dataclass(frozen=True)
 class Spell:
     name: str
-    cost: int
     turns: int
 
-    def setup(self, player: Character) -> None:
-        EFFECTS[self.name].setup(player)
+    def effect(self) -> SpellEffect:
+        return EFFECTS[self.name]
 
-    def apply(self, player: Character, enemy: Character) -> None:
-        EFFECTS[self.name].turn(player, enemy)
-        self.turns -= 1
-
-    def cleanup(self, player: Character) -> None:
-        EFFECTS[self.name].cleanup(player)
-
-    def copy(self) -> Self:
-        return Spell(self.name, self.cost, self.turns)
+    def move(self) -> Self:
+        return Spell(self.name, self.turns - 1)
 
 
-SPELL_BOOK = [
-    Spell(name="Magic Missile", cost=53, turns=1),
-    Spell(name="Drain", cost=73, turns=1),
-    Spell(name="Shield", cost=113, turns=6),
-    Spell(name="Poison", cost=173, turns=6),
-    Spell(name="Recharge", cost=229, turns=5),
+SPELLS = [
+    Spell(name="Magic Missile", turns=1),
+    Spell(name="Drain", turns=1),
+    Spell(name="Shield", turns=6),
+    Spell(name="Poison", turns=6),
+    Spell(name="Recharge", turns=5),
 ]
 
 
 @dataclass(frozen=True, order=True)
 class Game:
-    player: Character
-    enemy: Character
-    damage: int
+    player: Stats
+    enemy: Stats
+    damage: Stats
     spells: frozenset[Spell]
 
     def get_moves(self) -> list[tuple[int, Self]]:
         if self.player.hp <= 0:
             return []
-
-        spells = []
-        for spell in SPELL_BOOK:
-            if self.can_perform(spell):
-                spells.append(spell.copy())
-
-        adjacent = []
-        for spell in spells:
-            adjacent.append((spell.cost, self.move(spell)))
-        return adjacent
+        return [
+            (spell.effect().cost, self.move(spell))
+            for spell in SPELLS
+            if self.can_perform(spell)
+        ]
 
     def can_perform(self, spell: Spell) -> bool:
-        if self.player.attack < spell.cost:
+        if self.player.attack < spell.effect().cost:
             return False
         for active_spell in self.spells:
             if active_spell.name == spell.name:
@@ -106,33 +99,34 @@ class Game:
         return True
 
     def move(self, spell: Spell) -> Self:
-        player, enemy = self.player.copy(), self.enemy.copy()
-        spells = [spell.copy() for spell in self.spells]
+        player, enemy, spells = self.player, self.enemy, list(self.spells)
 
-        player.hp -= self.damage
-        Game.run_spells(player, enemy, spells)
+        player = player.subtract(self.damage)
+        player, enemy, spells = Game.run_spells(player, enemy, spells)
 
-        spell.setup(player)
-        player.attack -= spell.cost
+        player = player.subtract(Stats(attack=spell.effect().cost))
+        player = spell.effect().setup(player)
         spells.append(spell)
 
-        player.hp -= self.damage
-        Game.run_spells(player, enemy, spells)
-        player.hp -= max(1, enemy.attack - player.armor)
+        player = player.subtract(self.damage)
+        player, enemy, spells = Game.run_spells(player, enemy, spells)
+        player = player.subtract(Stats(hp=max(1, enemy.attack - player.armor)))
 
         return Game(player, enemy, self.damage, frozenset(spells))
 
     @staticmethod
-    def run_spells(player: Character, enemy: Character, spells: list[Spell]) -> None:
-        inactive: list[Spell] = []
+    def run_spells(
+        player: Stats, enemy: Stats, spells: list[Spell]
+    ) -> tuple[Stats, Stats, list[Spell]]:
+        active_spells: list[Spell] = []
         for spell in spells:
-            spell.apply(player, enemy)
+            player, enemy = spell.effect().turn(player, enemy)
+            spell = spell.move()
             if spell.turns == 0:
-                inactive.append(spell)
-
-        for spell in inactive:
-            spell.cleanup(player)
-            spells.remove(spell)
+                player = spell.effect().cleanup(player)
+            else:
+                active_spells.append(spell)
+        return player, enemy, active_spells
 
 
 def main() -> None:
@@ -143,7 +137,12 @@ def main() -> None:
 
 
 def play_game(hp: int, attack: int, damage: int) -> Optional[int]:
-    game = Game(Character(50, 500, 0), Character(hp, attack, 0), damage, frozenset())
+    game = Game(
+        player=Stats(hp=50, attack=500),
+        enemy=Stats(hp=hp, attack=attack),
+        damage=Stats(hp=damage),
+        spells=frozenset(),
+    )
     return search.bfs_complete(
         (0, game),
         lambda current: current.enemy.hp <= 0,
