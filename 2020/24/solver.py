3b4cf4ec83258232df4f77f34173bbb2a1e1649e
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-14 04:21:54 -0800

    Simply 2020/24 logic, remove classes, use simple types, runs in about a half a second now

--- 2020/24/solver.py
+++ 2020/24/solver.py
@@ -1,148 +1,85 @@
+from dataclasses import dataclass
+
 from aoc import answer
 from aoc.parser import Parser
 
-
-class Tile:
-    def __init__(self):
-        self.color = "w"
-
-    def flip(self):
-        self.color = "w" if self.is_black() else "b"
-
-    def is_black(self):
-        return self.color == "b"
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return str(self.color)
+Point = tuple[int, int]
+DIRECTIONS: dict[str, Point] = dict(
+    e=(2, 0),
+    w=(-2, 0),
+    ne=(1, 1),
+    nw=(-1, 1),
+    se=(1, -1),
+    sw=(-1, -1),
+)
 
 
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.directions = {
-            "e": (2, 0),
-            "w": (-2, 0),
-            "ne": (1, 1),
-            "nw": (-1, 1),
-            "se": (1, -1),
-            "sw": (-1, -1),
-        }
-
-    def go(self, instruction):
-        direction = self.directions[instruction]
-        return Point(self.x + direction[0], self.y + direction[1])
-
-    def get_adjacent(self):
-        adjacent = []
-        for direction in self.directions.values():
-            adjacent.append(self + direction)
-        return adjacent
-
-    def __add__(self, other):
-        return Point(self.x + other[0], self.y + other[1])
-
-    def __eq__(self, other):
-        return str(self) == str(other)
-
-    def __hash__(self):
-        return hash(str(self))
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "({}, {})".format(self.x, self.y)
+def add(p1: Point, p2: Point) -> Point:
+    return (p1[0] + p2[0], p1[1] + p2[1])
 
 
-class Floor:
-    def __init__(self):
-        self.floor = {}
-        self.floor[Point(0, 0)] = Tile()
-
-    def follow_path(self, path):
-        point = Point(0, 0)
-        for instruction in path.instructions:
-            point = point.go(instruction)
-            if point not in self.floor:
-                self.floor[point] = Tile()
-        return self.floor[point]
-
-    def transform(self):
-        self.pad_around_black()
-
-        to_flip = []
-        for point in self.floor:
-            tile = self.floor[point]
-            are_black = [
-                self.floor.get(adjacent_point, Tile()).is_black()
-                for adjacent_point in point.get_adjacent()
-            ]
-            black_count = sum(are_black)
-
-            if tile.is_black():
-                if black_count == 0 or black_count > 2:
-                    to_flip.append(tile)
-            else:
-                if black_count == 2:
-                    to_flip.append(tile)
-
-        for tile in to_flip:
-            tile.flip()
-
-    def pad_around_black(self):
-        points = list(self.floor.keys())
-        for point in points:
-            if self.floor[point].is_black():
-                for adjacent_point in point.get_adjacent():
-                    if adjacent_point not in self.floor:
-                        self.floor[adjacent_point] = Tile()
-
-    def count_black_tiles(self):
-        is_black = [tile.is_black() for tile in self.floor.values()]
-        return sum(is_black)
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return str(self.floor)
+def neighbors(p: Point) -> list[Point]:
+    return [add(p, direction) for direction in DIRECTIONS.values()]
 
 
 class Path:
-    def __init__(self, path):
-        self.instructions = []
-        it = iter(path)
-        for letter in it:
-            instruction = letter if letter in ["e", "w"] else letter + next(it)
-            self.instructions.append(instruction)
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return str(self.instructions)
+    def __init__(self, path: str):
+        self.instructions: list[Point] = []
+        path_iter = iter(path)
+        for letter in path_iter:
+            instruction = letter if letter in ["e", "w"] else letter + next(path_iter)
+            self.instructions.append(DIRECTIONS[instruction])
 
 
-def main():
-    floor = Floor()
-    paths = get_paths()
+@dataclass(frozen=True)
+class Floor:
+    floor: dict[Point, bool]
 
+    def follow_path(self, path: Path) -> None:
+        point = (0, 0)
+        for instruction in path.instructions:
+            point = add(point, instruction)
+            if point not in self.floor:
+                self.floor[point] = True
+        self.flip(point)
+
+    def transform(self) -> None:
+        self.pad_around_black()
+        to_flip: list[Point] = []
+        for point, tile in self.floor.items():
+            flip_counts = [2] if tile else [0, 3, 4, 5, 6]
+            black_count = sum(
+                [not self.floor.get(neighbor, True) for neighbor in neighbors(point)]
+            )
+            if black_count in flip_counts:
+                to_flip.append(point)
+        [self.flip(point) for point in to_flip]
+
+    def pad_around_black(self) -> None:
+        for point in list(self.floor.keys()):
+            if self.floor[point]:
+                continue
+            for neighbor in neighbors(point):
+                if neighbor in self.floor:
+                    continue
+                self.floor[neighbor] = True
+
+    def flip(self, point: Point) -> None:
+        self.floor[point] = not self.floor[point]
+
+    def count_black(self):
+        return sum([not tile for tile in self.floor.values()])
+
+
+def main() -> None:
+    floor = Floor(floor={(0, 0): True})
+    paths = [Path(line) for line in Parser().lines()]
     for path in paths:
-        floor.follow_path(path).flip()
-    answer.part1(320, floor.count_black_tiles())
-
-    days = 100
-    for day in range(days):
+        floor.follow_path(path)
+    answer.part1(320, floor.count_black())
+    for _ in range(100):
         floor.transform()
-    answer.part2(3777, floor.count_black_tiles())
-
-
-def get_paths():
-    return [Path(line) for line in Parser().lines()]
+    answer.part2(3777, floor.count_black())
 
 
 if __name__ == "__main__":
