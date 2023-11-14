48cc361fe1e26c4cede1390239df9391585b1340
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-13 21:30:18 -0800

    Rewrite 2015/20 in Rust, runs in 0.25 seconds instead of 7

---
+++ 2015/20/solver.rs
@@ -0,0 +1,26 @@
+use aoc_lib::answer;
+use aoc_lib::reader;
+use std::cmp::min;
+
+fn main() {
+    let goal = reader::read_int()[0] as usize;
+    answer::part1(665280, find_first(goal, false));
+    answer::part2(705600, find_first(goal, true));
+}
+
+fn find_first(goal: usize, lazy: bool) -> usize {
+    let mut houses: Vec<usize> = vec![0; goal / 10];
+    for i in 1..houses.len() {
+        let last_house = if lazy { i * 50 + 1 } else { houses.len() };
+        for house in (i..min(last_house, houses.len())).step_by(i) {
+            houses[house] += i * if lazy { 11 } else { 10 };
+        }
+    }
+    houses
+        .into_iter()
+        .enumerate()
+        .filter(|(_, house)| house >= &goal)
+        .map(|(i, _)| i)
+        .next()
+        .unwrap()
+}
