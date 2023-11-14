40b79447e594f9c408b663b02be6f2b5820db972
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-14 06:21:03 -0800

    Improve 2017/20 by using tuple, better hashing

--- 2017/20/solver.py
+++ 2017/20/solver.py
@@ -1,78 +1,70 @@
+from dataclasses import dataclass
+
 from aoc import answer
-from aoc.board import Point
 from aoc.parser import Parser
 
+Vector = tuple[int, int, int]
 
+
+def parse_vector(value: str) -> tuple[str, Vector]:
+    name, value = value.split("=")
+    x, y, z = [int(val) for val in value[1:-1].split(",")]
+    return name, (x, y, z)
+
+
+def add(v1: Vector, v2: Vector) -> Vector:
+    return (v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2])
+
+
+def size(v: Vector) -> int:
+    return abs(v[0]) + abs(v[1]) + abs(v[2])
+
+
+@dataclass
 class Particle:
-    def __init__(self, value):
-        self.id = value[0]
-        values = value[1].split(", ")
-
-        components = {}
-        for value in values:
-            value = value.split("=")
-
-            name = value[0]
-            value = value[1][1:-1]
-            value = [int(val) for val in value.split(",")]
-            components[name] = Point(*value)
-
-        self.pos = components["p"]
-        self.vel = components["v"]
-        self.acc = components["a"]
+    id: int
+    pos: Vector
+    vel: Vector
+    acc: Vector
 
     def step(self):
-        self.vel += self.acc
-        self.pos += self.vel
-
-    def __eq__(self, o):
-        return str(self) == str(o)
-
-    def __hash__(self):
-        return hash(str(self))
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "{}: p={}, v={}, a={}".format(self.id, self.pos, self.vel, self.acc)
+        self.vel = add(self.vel, self.acc)
+        self.pos = add(self.pos, self.vel)
 
 
-def main():
+def main() -> None:
     answer.part1(161, run_simulation(False)[0].id)
     answer.part2(438, len(run_simulation(True)))
 
 
-def run_simulation(cleanup):
-    particles = get_particles()
-    for i in range(1_000):
-        seen = set()
-        to_remove = set()
+def run_simulation(cleanup: bool) -> list[Particle]:
+    def parse_particle(i: int, line: str) -> Particle:
+        components: dict[str, Vector] = dict()
+        for value in line.split(", "):
+            name, vector = parse_vector(value)
+            components[name] = vector
+        return Particle(
+            id=i, pos=components["p"], vel=components["v"], acc=components["a"]
+        )
 
+    particles = [parse_particle(i, line) for i, line in enumerate(Parser().lines())]
+    for _ in range(1_000):
+        seen, to_remove = set(), set()
         for particle in particles:
             particle.step()
-            pos = particle.pos
-
-            if pos in seen:
+            if particle.pos in seen:
                 if cleanup:
-                    to_remove.add(pos)
+                    to_remove.add(particle.pos)
             else:
-                seen.add(pos)
+                seen.add(particle.pos)
 
-        delete_particles = set()
-        for particle in particles:
-            if particle.pos in to_remove:
-                delete_particles.add(particle)
+        delete_particles = [
+            particle for particle in particles if particle.pos in to_remove
+        ]
+        [particles.remove(particle) for particle in delete_particles]
 
-        for delete_particle in delete_particles:
-            particles.remove(delete_particle)
-
-    particles.sort(key=lambda particle: len(particle.pos))
+    particles.sort(key=lambda particle: size(particle.pos))
     return particles
-
-
-def get_particles():
-    return [Particle(line) for line in enumerate(Parser().lines())]
 
 
 if __name__ == "__main__":
