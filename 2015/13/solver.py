652df5961aea5f28f2ae1c3ce7100ac53db0a8cb
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-16 15:54:49 -0800

    Improve logic in 2015/13, though it is slower.

--- 2015/13/solver.py
+++ 2015/13/solver.py
@@ -1,60 +1,47 @@
-import itertools
 from collections import defaultdict
+from dataclasses import dataclass
+from itertools import permutations
 
 from aoc import answer
 from aoc.parser import Parser
 
 
-def main():
-    answer.part1(709, get_max_score(False))
-    answer.part2(668, get_max_score(True))
+@dataclass(frozen=True)
+class People:
+    people: tuple[str, ...]
+
+    def score(self, graph: dict[tuple[str, str], int]) -> int:
+        scores = [
+            graph[(person, self[i - 1])] + graph[(person, self[i + 1])]
+            for i, person in enumerate(self.people)
+        ]
+        return sum(scores)
+
+    def __getitem__(self, i: int) -> str:
+        return self.people[i % len(self.people)]
 
 
-def get_max_score(include_self):
+def main() -> None:
     graph = get_graph()
-    scores = []
-    for permutation in get_permutations(graph, include_self):
-        score = get_score(permutation, graph)
-        scores.append(score)
-    return max(scores)
+    answer.part1(709, get_max_score(graph, False))
+    answer.part2(668, get_max_score(graph, True))
 
 
-def get_permutations(graph, include_self):
-    keys = set(graph.keys())
-    if include_self:
-        keys.add("MeanderingProgrammer")
-    return itertools.permutations(keys)
-
-
-def get_score(permutation, graph):
-    score = 0
-    for i in range(len(permutation)):
-        current = permutation[i]
-        values = graph[current]
-
-        left = permutation[(i - 1) % len(permutation)]
-        right = permutation[(i + 1) % len(permutation)]
-
-        score += values[left]
-        score += values[right]
-    return score
-
-
-def get_graph():
-    graph = defaultdict(lambda: defaultdict(int))
+def get_graph() -> dict[tuple[str, str], int]:
+    multiplier: dict[str, int] = dict(gain=1, lose=-1)
+    graph: dict[tuple[str, str], int] = defaultdict(int)
     for line in Parser().lines():
-        line = line[:-1].split()
-
-        who = line[0]
-
-        multiplier = 1 if line[2] == "gain" else -1
-        amount = int(line[3])
-        weight = multiplier * amount
-
-        adjacent = line[10]
-
-        graph[who][adjacent] = weight
+        parts = line[:-1].split()
+        graph[(parts[0], parts[-1])] = multiplier[parts[2]] * int(parts[3])
     return graph
+
+
+def get_max_score(graph: dict[tuple[str, str], int], include_self: bool) -> int:
+    all_people = set([pair[0] for pair in graph.keys()])
+    if include_self:
+        all_people.add("Myself")
+    scores = [People(people).score(graph) for people in permutations(all_people)]
+    return max(scores)
 
 
 if __name__ == "__main__":
