5438aa296124eb3982cc34ed820ba06b2e481047
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-17 15:00:31 -0800

    Implement initial version of int-code in Rust, use for 2019/02

---
+++ 2019/02/solver.rs
@@ -0,0 +1,28 @@
+use aoc_lib::answer;
+use aoc_lib::int_code::{Computer, NoopBus};
+use aoc_lib::reader;
+
+fn main() {
+    answer::part1(6627023, run(12, 2));
+    answer::part1(4019, get_goal().unwrap());
+}
+
+fn run(v1: i64, v2: i64) -> i64 {
+    let mut memory = reader::read_csv();
+    memory[1] = v1;
+    memory[2] = v2;
+    let mut computer = Computer::new(NoopBus::new(), memory);
+    computer.run();
+    computer.get(0)
+}
+
+fn get_goal() -> Option<i64> {
+    for noun in 0..100 {
+        for verb in 0..100 {
+            if run(noun, verb) == 19_690_720 {
+                return Some(100 * noun + verb);
+            }
+        }
+    }
+    None
+}
