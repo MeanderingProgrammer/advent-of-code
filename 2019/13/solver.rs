ce2221f6d4db4d0edc8cc3894d3dba83eff63a2c
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-17 16:53:00 -0800

    Rewrite 2019/13 in rust, runs in about half a second now. Finished off remaining int-code features.

---
+++ 2019/13/solver.rs
@@ -0,0 +1,77 @@
+use aoc_lib::answer;
+use aoc_lib::int_code::{Bus, Computer};
+use aoc_lib::reader;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Game {
+    tile_buffer: Vec<i64>,
+    tile_freq: HashMap<String, i64>,
+    tile_x: HashMap<String, i64>,
+    score: i64,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            tile_buffer: Vec::new(),
+            tile_freq: HashMap::new(),
+            tile_x: HashMap::new(),
+            score: 0,
+        }
+    }
+}
+
+impl Bus for Game {
+    fn active(&self) -> bool {
+        true
+    }
+
+    fn get_input(&self) -> i64 {
+        let diff = self.tile_x["ball"] - self.tile_x["horizontal paddle"];
+        1.min(diff.max(-1))
+    }
+
+    fn add_output(&mut self, value: i64) {
+        self.tile_buffer.push(value);
+        if self.tile_buffer.len() == 3 {
+            let x = self.tile_buffer[0];
+            let y = self.tile_buffer[1];
+            let value = self.tile_buffer[2];
+            if x == -1 && y == 0 {
+                self.score = value;
+            } else {
+                let name = match value {
+                    0 => "empty",
+                    1 => "wall",
+                    2 => "block",
+                    3 => "horizontal paddle",
+                    4 => "ball",
+                    _ => panic!("Unknown tile value"),
+                };
+                *self.tile_freq.entry(name.to_string()).or_insert(0) += 1;
+                self.tile_x.insert(name.to_string(), x);
+            }
+            self.tile_buffer.clear();
+        }
+    }
+}
+
+fn main() {
+    answer::part1(363, play_game(false));
+    answer::part2(17159, play_game(true));
+}
+
+fn play_game(play_for_free: bool) -> i64 {
+    let mut memory = reader::read_csv();
+    if play_for_free {
+        memory[0] = 2;
+    }
+    let mut computer = Computer::new(Game::new(), memory);
+    computer.run();
+    if play_for_free {
+        computer.bus.score
+    } else {
+        *computer.bus.tile_freq.get("block").unwrap()
+    }
+}
--- commons/rust/int_code.rs
+++ commons/rust/int_code.rs
@@ -13,7 +13,8 @@ impl Parameter {
 
     fn get<T: Bus + Debug>(&self, computer: &Computer<T>) -> i64 {
         match self.mode {
-            0 => computer.get(self.index(computer)),
+            0 | 2 => computer.get(self.index(computer)),
+            1 => self.value,
             _ => panic!("Unknown mode get: {0}", self.mode),
         }
     }
@@ -21,6 +22,7 @@ impl Parameter {
     fn index<T: Bus + Debug>(&self, computer: &Computer<T>) -> usize {
         match self.mode {
             0 => self.value as usize,
+            2 => computer.base + (self.value as usize),
             _ => panic!("Unknown mode index: {0}", self.mode),
         }
     }
@@ -30,6 +32,11 @@ impl Parameter {
 enum Response {
     SetValue(usize, i64),
     Halt,
+    Store(usize),
+    Load(i64),
+    Jump(usize),
+    Noop,
+    AdjustBase(usize),
 }
 
 trait Instruction<T>: Debug {
@@ -38,49 +45,24 @@ trait Instruction<T>: Debug {
 }
 
 #[derive(Debug)]
-struct Addition {
+struct Math {
+    f: fn(i64, i64) -> i64,
     v1: Parameter,
     v2: Parameter,
     v3: Parameter,
 }
 
-impl Addition {
-    fn new(v1: Parameter, v2: Parameter, v3: Parameter) -> Self {
-        Self { v1, v2, v3 }
+impl Math {
+    fn new(f: fn(i64, i64) -> i64, v1: Parameter, v2: Parameter, v3: Parameter) -> Self {
+        Self { f, v1, v2, v3 }
     }
 }
 
-impl<T: Bus + Debug> Instruction<T> for Addition {
+impl<T: Bus + Debug> Instruction<T> for Math {
     fn process(&self, computer: &Computer<T>) -> Response {
         Response::SetValue(
             self.v3.index(computer),
-            self.v1.get(computer) + self.v2.get(computer),
-        )
-    }
-
-    fn len(&self) -> usize {
-        3
-    }
-}
-
-#[derive(Debug)]
-struct Multiplication {
-    v1: Parameter,
-    v2: Parameter,
-    v3: Parameter,
-}
-
-impl Multiplication {
-    fn new(v1: Parameter, v2: Parameter, v3: Parameter) -> Self {
-        Self { v1, v2, v3 }
-    }
-}
-
-impl<T: Bus + Debug> Instruction<T> for Multiplication {
-    fn process(&self, computer: &Computer<T>) -> Response {
-        Response::SetValue(
-            self.v3.index(computer),
-            self.v1.get(computer) * self.v2.get(computer),
+            (self.f)(self.v1.get(computer), self.v2.get(computer)),
         )
     }
 
@@ -98,13 +80,128 @@ impl Halt {
     }
 }
 
-impl<T> Instruction<T> for Halt {
+impl<T: Bus + Debug> Instruction<T> for Halt {
     fn process(&self, _: &Computer<T>) -> Response {
         Response::Halt
     }
 
     fn len(&self) -> usize {
         0
+    }
+}
+
+#[derive(Debug)]
+struct Store {
+    v1: Parameter,
+}
+
+impl Store {
+    fn new(v1: Parameter) -> Self {
+        Self { v1 }
+    }
+}
+
+impl<T: Bus + Debug> Instruction<T> for Store {
+    fn process(&self, computer: &Computer<T>) -> Response {
+        Response::Store(self.v1.index(computer))
+    }
+
+    fn len(&self) -> usize {
+        1
+    }
+}
+
+#[derive(Debug)]
+struct Load {
+    v1: Parameter,
+}
+
+impl Load {
+    fn new(v1: Parameter) -> Self {
+        Self { v1 }
+    }
+}
+
+impl<T: Bus + Debug> Instruction<T> for Load {
+    fn process(&self, computer: &Computer<T>) -> Response {
+        Response::Load(self.v1.get(computer))
+    }
+
+    fn len(&self) -> usize {
+        1
+    }
+}
+
+#[derive(Debug)]
+struct JumpIf {
+    f: fn(i64) -> bool,
+    v1: Parameter,
+    v2: Parameter,
+}
+
+impl JumpIf {
+    fn new(f: fn(i64) -> bool, v1: Parameter, v2: Parameter) -> Self {
+        Self { f, v1, v2 }
+    }
+}
+
+impl<T: Bus + Debug> Instruction<T> for JumpIf {
+    fn process(&self, computer: &Computer<T>) -> Response {
+        if (self.f)(self.v1.get(computer)) {
+            Response::Jump(self.v2.get(computer) as usize)
+        } else {
+            Response::Noop
+        }
+    }
+
+    fn len(&self) -> usize {
+        2
+    }
+}
+
+#[derive(Debug)]
+struct Comparison {
+    f: fn(i64, i64) -> bool,
+    v1: Parameter,
+    v2: Parameter,
+    v3: Parameter,
+}
+
+impl Comparison {
+    fn new(f: fn(i64, i64) -> bool, v1: Parameter, v2: Parameter, v3: Parameter) -> Self {
+        Self { f, v1, v2, v3 }
+    }
+}
+
+impl<T: Bus + Debug> Instruction<T> for Comparison {
+    fn process(&self, computer: &Computer<T>) -> Response {
+        let is_true = (self.f)(self.v1.get(computer), self.v2.get(computer));
+        Response::SetValue(self.v3.index(computer), if is_true { 1 } else { 0 })
+    }
+
+    fn len(&self) -> usize {
+        3
+    }
+}
+
+#[derive(Debug)]
+struct BaseAdjuster {
+    v1: Parameter,
+}
+
+impl BaseAdjuster {
+    fn new(v1: Parameter) -> Self {
+        Self { v1 }
+    }
+}
+
+impl<T: Bus + Debug> Instruction<T> for BaseAdjuster {
+    fn process(&self, computer: &Computer<T>) -> Response {
+        Response::AdjustBase(self.v1.get(computer) as usize)
+    }
+
+    fn len(&self) -> usize {
+        1
     }
 }
 
@@ -154,7 +251,11 @@ impl<T: Bus + Debug> Computer<T> {
     }
 
     pub fn get(&self, index: usize) -> i64 {
-        self.memory[index]
+        if index < self.memory.len() {
+            self.memory[index]
+        } else {
+            0
+        }
     }
 
     pub fn run(&mut self) {
@@ -166,27 +267,77 @@ impl<T: Bus + Debug> Computer<T> {
         let response = instruction.process(self);
         match response {
             Response::SetValue(index, value) => {
-                self.memory[index] = value;
+                self.set(index, value);
                 self.pointer += 1 + instruction.len();
                 true
             }
             Response::Halt => false,
+            Response::Store(index) => {
+                self.set(index, self.bus.get_input());
+                self.pointer += 1 + instruction.len();
+                true
+            }
+            Response::Load(value) => {
+                self.bus.add_output(value);
+                self.pointer += 1 + instruction.len();
+                true
+            }
+            Response::Jump(index) => {
+                self.pointer = index;
+                true
+            }
+            Response::Noop => {
+                self.pointer += 1 + instruction.len();
+                true
+            }
+            Response::AdjustBase(amount) => {
+                self.base += amount;
+                self.pointer += 1 + instruction.len();
+                true
+            }
         }
     }
 
     fn next(&self) -> Box<dyn Instruction<T>> {
         let opcode = self.memory[self.pointer] % 100;
         match opcode {
-            1 => Box::new(Addition::new(
+            1 => Box::new(Math::new(
+                |v1, v2| v1 + v2,
                 self.parameter(1),
                 self.parameter(2),
                 self.parameter(3),
             )),
-            2 => Box::new(Multiplication::new(
+            2 => Box::new(Math::new(
+                |v1, v2| v1 * v2,
                 self.parameter(1),
                 self.parameter(2),
                 self.parameter(3),
             )),
+            3 => Box::new(Store::new(self.parameter(1))),
+            4 => Box::new(Load::new(self.parameter(1))),
+            5 => Box::new(JumpIf::new(
+                |value| value != 0,
+                self.parameter(1),
+                self.parameter(2),
+            )),
+            6 => Box::new(JumpIf::new(
+                |value| value == 0,
+                self.parameter(1),
+                self.parameter(2),
+            )),
+            7 => Box::new(Comparison::new(
+                |v1, v2| v1 < v2,
+                self.parameter(1),
+                self.parameter(2),
+                self.parameter(3),
+            )),
+            8 => Box::new(Comparison::new(
+                |v1, v2| v1 == v2,
+                self.parameter(1),
+                self.parameter(2),
+                self.parameter(3),
+            )),
+            9 => Box::new(BaseAdjuster::new(self.parameter(1))),
             99 => Box::new(Halt::new()),
             _ => panic!("Unknown opcode: {opcode}"),
         }
@@ -194,8 +345,15 @@ impl<T: Bus + Debug> Computer<T> {
 
     fn parameter(&self, i: usize) -> Parameter {
         let code = self.memory[self.pointer];
-        let mode_char = code.to_string().chars().nth(i + 2).unwrap_or('0');
+        let mode_char = code.to_string().chars().rev().nth(i + 1).unwrap_or('0');
         let mode = mode_char.to_digit(10).unwrap() as u8;
         Parameter::new(self.memory[self.pointer + i], mode)
+    }
+
+    fn set(&mut self, index: usize, value: i64) {
+        if index >= self.memory.len() {
+            self.memory.resize(index + 1, 0);
+        }
+        self.memory[index] = value;
     }
 }
