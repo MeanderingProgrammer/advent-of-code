014f2345f86acf273a6811fde77106636b8d77e8
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-19 06:22:35 -0800

    Add wait groups and batches to 2016/14

--- 2016/14/solver.go
+++ 2016/14/solver.go
@@ -5,84 +5,125 @@ import (
 	"advent-of-code/commons/go/files"
 	"crypto/md5"
 	"encoding/hex"
-	"github.com/gammazero/deque"
 	"strconv"
 	"strings"
+	"sync"
+)
+
+const (
+	batches   = 8
+	batchSize = 64
+	offset    = 1_000
 )
 
 type hashInfo struct {
-	value string
-	cinqs []rune
+	index   int
+	value   string
+	triples []rune
+	cinqs   []rune
 }
 
-func main() {
-	prefix := strings.TrimSpace(files.Content())
-	answers.Part1(15168, generate(prefix, 64, 1))
-	answers.Part2(20864, generate(prefix, 64, 2_017))
+type hashSearch struct {
+	prefix string
+	hashes int
 }
 
-func generate(prefix string, n int, hashes int) int {
-	index := 0
-	infos := deque.New[hashInfo]()
-	for index < 1_000 {
-		infos.PushBack(getHash(prefix, index, hashes))
-		index++
-	}
-
-	keys := []int{}
-	for len(keys) < n {
-		info := infos.PopFront()
-		infos.PushBack(getHash(prefix, index, hashes))
-		triples := getRepeats(info.value, 3)
-		if len(triples) > 0 {
-			if contains(infos, triples[0]) {
-				keys = append(keys, index-infos.Len())
-			}
+func (h hashSearch) runBatch(start int) []hashInfo {
+	result := []hashInfo{}
+	for i := start; i < start+batchSize; i++ {
+		value := h.getHash(i)
+		if len(value.triples) > 0 {
+			result = append(result, value)
 		}
-		index++
 	}
-	return keys[len(keys)-1]
+	return result
 }
 
-func getHash(prefix string, index int, hashes int) hashInfo {
-	value := prefix + strconv.Itoa(index)
-	for i := 0; i < hashes; i++ {
-		hasher := md5.New()
-		hasher.Write([]byte(value))
-		value = hex.EncodeToString(hasher.Sum(nil))
+func (h hashSearch) getHash(index int) hashInfo {
+	value := h.prefix + strconv.Itoa(index)
+	for i := 0; i < h.hashes; i++ {
+		result := md5.Sum([]byte(value))
+		value = hex.EncodeToString(result[:])
 	}
-	return hashInfo{value: value, cinqs: getRepeats(value, 5)}
+	return hashInfo{index: index, triples: getRepeats(value, 3), cinqs: getRepeats(value, 5)}
 }
 
 func getRepeats(value string, length int) []rune {
 	repeats := []rune{}
 	for i := 0; i <= len(value)-length; i++ {
 		subString := value[i : i+length]
-		repeat := sameRune(subString)
-		if repeat != nil {
-			repeats = append(repeats, *repeat)
+		if sameChar(subString) {
+			repeats = append(repeats, rune(subString[0]))
 		}
 	}
 	return repeats
 }
 
-func sameRune(value string) *rune {
-	runes := []rune(value)
-	firstRune := runes[0]
-	for i := 1; i < len(value); i++ {
-		if runes[i] != firstRune {
-			return nil
+func sameChar(value string) bool {
+	firstChar := rune(value[0])
+	for _, char := range value[1:] {
+		if char != firstChar {
+			return false
 		}
 	}
-	return &firstRune
+	return true
 }
 
-func contains(infos *deque.Deque[hashInfo], target rune) bool {
-	for i := 0; i < infos.Len(); i++ {
-		info := infos.At(i)
-		for _, value := range info.cinqs {
-			if value == target {
-				return true
+func main() {
+	prefix := strings.TrimSpace(files.Content())
+	answers.Part1(15168, generate(hashSearch{prefix: prefix, hashes: 1}))
+	answers.Part2(20864, generate(hashSearch{prefix: prefix, hashes: 2_017}))
+}
+
+func generate(search hashSearch) int {
+	infos := make(map[int]hashInfo)
+	for i := 0; i < offset; i++ {
+		value := search.getHash(i)
+		if len(value.triples) > 0 {
+			infos[i] = value
+		}
+	}
+
+	index := 0
+	keys := []int{}
+	var mu sync.Mutex
+	for len(keys) < 64 {
+		var wg sync.WaitGroup
+		for i := 0; i < batches; i++ {
+			wg.Add(1)
+			go func(start int) {
+				defer wg.Done()
+				for _, value := range search.runBatch(start) {
+					mu.Lock()
+					infos[value.index] = value
+					mu.Unlock()
+				}
+			}(offset + index + (i * batchSize))
+		}
+		wg.Wait()
+
+		end := index + (batches * batchSize)
+		for index < end {
+			info, ok := infos[index]
+			if ok {
+				if contains(infos, index+1, info.triples[0]) {
+					keys = append(keys, index)
+				}
+			}
+			index++
+		}
+	}
+	return keys[63]
+}
+
+func contains(infos map[int]hashInfo, index int, target rune) bool {
+	for i := index; i < index+offset; i++ {
+		info, ok := infos[i]
+		if ok {
+			for _, value := range info.cinqs {
+				if value == target {
+					return true
+				}
 			}
 		}
 	}
