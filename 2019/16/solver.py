7dd9ca551cf929f423609b9e10cdf54ef6641189
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-19 08:40:43 -0800

    Update 2019/16 implementation

--- 2019/16/solver.py
+++ 2019/16/solver.py
@@ -1,72 +1,54 @@
 from aoc import answer
 from aoc.parser import Parser
 
+PATTERN = [0, 1, 0, -1]
+
 
 class Transmission:
-    def __init__(self, raw, apply_offset):
+    def __init__(self, raw: str, apply_offset: bool):
         self.digits = [int(value) for value in raw]
-        self.apply_offset = apply_offset
-        if self.apply_offset:
-            offset = self.first_n(7)
-            self.digits = self.digits[offset:]
+        if apply_offset:
+            self.digits = self.digits[self.first_n(7) :]
 
-    def forward(self):
-        if self.apply_offset:
-            self.digits = self.forward_with_offset()
-        else:
-            self.digits = self.forward_from_start()
+    def start(self) -> None:
+        self.digits = [self.get_new_digit(i) for i in range(len(self.digits))]
 
-    def forward_from_start(self):
-        return [self.get_new_digit(i) for i in range(len(self.digits))]
-
-    def get_new_digit(self, i):
-        pattern, pattern_index = self.get_pattern(i + 1), 1
-        new_digit = 0
+    def get_new_digit(self, i: int) -> int:
+        pattern_index, new_digit = 0, 0
         for digit in self.digits:
-            new_digit += pattern[pattern_index % len(pattern)] * digit
             pattern_index += 1
+            new_digit += PATTERN[(pattern_index // (i + 1)) % len(PATTERN)] * digit
         return abs(new_digit) % 10
 
-    def forward_with_offset(self):
-        # Going from back to front each digit is the
-        # current sum % 10
-        # This only applies in the middle of a set
-        # of digits and does not hold to the start
-        self.digits.reverse()
-
+    def offset(self) -> None:
+        # Going from back to front each digit is the current sum % 10
+        # This only applies in the middle of a set of digits and does not hold to the start
         new_digits, current_sum = [], 0
-        for digit in self.digits:
+        for digit in reversed(self.digits):
             current_sum += digit
             new_digits.append(current_sum % 10)
         new_digits.reverse()
+        self.digits = new_digits
 
-        return new_digits
-
-    def first_n(self, n):
+    def first_n(self, n: int) -> int:
         first_n_digits = [str(digit) for digit in self.digits[:n]]
         as_string = "".join(first_n_digits)
         return int(as_string)
 
-    @staticmethod
-    def get_pattern(length):
-        components = [[0] * length, [1] * length, [0] * length, [-1] * length]
-        return [value for component in components for value in component]
 
-
-def main():
+def main() -> None:
     answer.part1(77038830, apply_fft(1, False))
     answer.part2(28135104, apply_fft(10_000, True))
 
 
-def apply_fft(repeats, apply_offset):
-    transmission = get_transmission(repeats, apply_offset)
-    for i in range(100):
-        transmission.forward()
+def apply_fft(repeats: int, apply_offset: bool) -> int:
+    transmission = Transmission(Parser().string() * repeats, apply_offset)
+    for _ in range(100):
+        if not apply_offset:
+            transmission.start()
+        else:
+            transmission.offset()
     return transmission.first_n(8)
-
-
-def get_transmission(repeats, apply_offset):
-    return Transmission(Parser().string() * repeats, apply_offset)
 
 
 if __name__ == "__main__":
