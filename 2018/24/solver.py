35a59af839a95aa4216f3a5fd14a7eb570580a55
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-19 13:10:05 -0800

    Overhaul 2018/24 runs about 4x faster now

--- 2018/24/solver.py
+++ 2018/24/solver.py
@@ -1,223 +1,177 @@
+import re
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Optional, Self
+
 from aoc import answer
 from aoc.parser import Parser
 
 
+class Category(Enum):
+    IMMUNE = auto()
+    INFECTION = auto()
+
+
+@dataclass
 class Group:
-    def __init__(self, group, id, raw):
-        self.id = (group, id)
-        self.units = self.get_units(raw)
-        self.hp = self.get_hp(raw)
+    id: int
+    category: Category
+    units: int
+    hp: int
+    weaknesses: list[str]
+    immunities: list[str]
+    damage: int
+    damage_type: str
+    initiative: int
 
-        traits = self.get_traits(raw)
-        self.weaknesses = traits.get("weak", [])
-        self.immunities = traits.get("immune", [])
-
-        self.damage = self.get_damage(raw)
-        self.damage_type = self.get_damage_type(raw)
-        self.initiative = self.get_initiative(raw)
-        self.boost = None
-
-    def effective_power(self):
-        return self.units * self.damage
-
-    def apply_damage(self, damage_dealt):
-        units_destroyed = damage_dealt // self.hp
-        self.units -= units_destroyed
-
-    def is_dead(self):
+    @property
+    def dead(self) -> bool:
         return self.units <= 0
 
-    def target_selection_value(self):
-        return self.effective_power(), self.initiative
+    @property
+    def power(self) -> int:
+        return self.units * self.damage
 
-    def target_value(self, o):
-        return self.would_deal(o), o.effective_power(), o.initiative
-
-    def attack_value(self):
-        return self.initiative
-
-    def immune_to(self, o):
-        return o.damage_type in self.immunities
-
-    def weak_to(self, o):
-        return o.damage_type in self.weaknesses
-
-    def would_deal(self, o):
-        multiplier = 1
-        if o.immune_to(self):
+    def calculate_damage(self, o: Self) -> int:
+        if self.damage_type in o.immunities:
             multiplier = 0
-        elif o.weak_to(self):
+        elif self.damage_type in o.weaknesses:
             multiplier = 2
-        return self.effective_power() * multiplier
+        else:
+            multiplier = 1
+        return self.power * multiplier
 
-    def __eq__(self, o):
+    def hit(self, damage: int) -> None:
+        units_destroyed = damage // self.hp
+        self.units -= units_destroyed
+
+    def __eq__(self, o: Optional[Self]) -> bool:
         if o is None:
             return False
-        return str(self.id) == str(o.id)
+        return self.id == o.id
 
-    def __hash__(self):
-        return hash(str(self.id))
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return (
-            "{"
-            + ", ".join(
-                [
-                    "Units = {}".format(self.units),
-                    "HP = {}".format(self.hp),
-                    "Damage = {}".format(self.damage),
-                ]
-            )
-            + "}"
-        )
-
-    @staticmethod
-    def get_units(raw):
-        return int(raw.split()[0])
-
-    @staticmethod
-    def get_hp(raw):
-        return int(raw.split()[4])
-
-    @staticmethod
-    def get_traits(raw):
-        traits = {}
-
-        start = raw.find("(")
-        if start == -1:
-            return traits
-        end = raw.index(")")
-        raw = raw[start + 1 : end]
-
-        for part in raw.split("; "):
-            trait_details = part.split(" to ")
-            traits[trait_details[0]] = trait_details[1].split(", ")
-
-        return traits
-
-    @staticmethod
-    def get_damage(raw):
-        return int(raw.split()[-6])
-
-    @staticmethod
-    def get_damage_type(raw):
-        return raw.split()[-5]
-
-    @staticmethod
-    def get_initiative(raw):
-        return int(raw.split()[-1])
+    def __hash__(self) -> int:
+        return self.id
 
 
+@dataclass
 class Battle:
-    def __init__(self, armies):
-        self.immune_system = armies[0]
-        self.infection = armies[1]
-        self.assigned = {}
-
-    def boost_immune(self, value):
-        for group in self.immune_system:
-            group.damage += value
+    groups: list[Group]
 
     def simulate(self):
         current, previous = None, 0
-        while self.should_continue() and current != previous:
-            self.assign_targets()
+        while len(self.groups) > 0 and current != previous:
             self.attack()
-            self.assigned = {}
             previous = current
             current = self.winning_units()
 
-    def assign_targets(self):
-        groups = self.get_groups()
-        groups.sort(key=lambda group: group.target_selection_value(), reverse=True)
+    def attack(self) -> None:
+        assigned = self.assign_targets()
+        groups = list(assigned)
+        groups.sort(key=lambda group: group.initiative, reverse=True)
         for group in groups:
-            to_attack = self.get_group_to_attack(group)
-            self.assigned[group] = to_attack
+            if group.dead:
+                continue
+            target = assigned[group]
+            damage = group.calculate_damage(target)
+            target.hit(damage)
+            if target.dead:
+                self.groups.remove(target)
 
-    def get_group_to_attack(self, group):
-        options = self.immune_system if group in self.infection else self.infection
-        options = [option for option in options if option not in self.assigned.values()]
-        options.sort(key=lambda option: group.target_value(option), reverse=True)
+    def assign_targets(self) -> dict[Group, Group]:
+        assigned: dict[Group, Group] = dict()
+        groups = list(self.groups)
+        groups.sort(key=lambda group: (group.power, group.initiative), reverse=True)
+        for group in groups:
+            target = self.get_target(group, list(assigned.values()))
+            if target is not None:
+                assigned[group] = target
+        return assigned
+
+    def get_target(self, group: Group, assigned: list[Group]) -> Optional[Group]:
+        opponents = {
+            Category.IMMUNE: Category.INFECTION,
+            Category.INFECTION: Category.IMMUNE,
+        }[group.category]
+
+        options = self.get_category(opponents)
+        options = [option for option in options if option not in assigned]
+        options.sort(
+            key=lambda option: (
+                group.calculate_damage(option),
+                option.power,
+                option.initiative,
+            ),
+            reverse=True,
+        )
         if len(options) == 0:
             return None
-        to_attack = options[0]
-        if group.would_deal(to_attack) == 0:
-            return None
-        return to_attack
+        else:
+            return None if group.calculate_damage(options[0]) == 0 else options[0]
 
-    def attack(self):
-        groups = self.get_groups()
-        groups.sort(key=lambda group: group.attack_value(), reverse=True)
-        for group in groups:
-            to_attack = self.assigned[group]
-            if not group.is_dead() and to_attack is not None:
-                damage_dealt = group.would_deal(to_attack)
-                to_attack.apply_damage(damage_dealt)
-                self.cleanup(to_attack)
+    def immune_won(self) -> bool:
+        immune = self.get_category(Category.IMMUNE)
+        infection = self.get_category(Category.INFECTION)
+        return len(immune) > 0 and len(infection) == 0
 
-    def should_continue(self):
-        return len(self.immune_system) > 0 and len(self.infection) > 0
+    def get_category(self, category: Category) -> list[Group]:
+        return [group for group in self.groups if group.category == category]
 
-    def cleanup(self, group):
-        if group.is_dead():
-            if group in self.immune_system:
-                self.immune_system.remove(group)
-            else:
-                self.infection.remove(group)
-
-    def immune_won(self):
-        return len(self.immune_system) > 0 and len(self.infection) == 0
-
-    def winning_units(self):
-        return sum([group.units for group in self.get_groups()])
-
-    def get_groups(self):
-        return self.immune_system + self.infection
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "\n".join(
-            [
-                "Immune System:\n{}".format(self.immune_system),
-                "Infection:\n{}".format(self.infection),
-            ]
-        )
+    def winning_units(self) -> int:
+        return sum([group.units for group in self.groups])
 
 
-def main():
+def main() -> None:
     answer.part1(16086, solve_part_1())
     answer.part2(3957, solve_part_2())
 
 
 def solve_part_1():
-    battle = Battle(get_armies())
+    battle = get_battle(0)
     battle.simulate()
     return battle.winning_units()
 
 
-def solve_part_2():
-    boost = 0
-    immune_won = False
-    while not immune_won:
-        boost += 1
-        battle = Battle(get_armies())
-        battle.boost_immune(boost)
+def solve_part_2() -> int:
+    immune_boost = 0
+    while True:
+        immune_boost += 1
+        battle = get_battle(immune_boost)
         battle.simulate()
-        immune_won |= battle.immune_won()
+        if battle.immune_won():
+            return battle.winning_units()
 
-    return battle.winning_units()
 
+def get_battle(immune_boost: int) -> Battle:
+    def parse_group(id: int, category: Category, raw: str, boost: int) -> Group:
+        traits = dict()
+        match = re.search(r"\((.*)\)", raw)
+        if match is not None:
+            for raw_trait in match.group(1).split("; "):
+                trait_type, trait_values = raw_trait.split(" to ")
+                traits[trait_type] = trait_values.split(", ")
+        parts = raw.split()
+        return Group(
+            id=id,
+            category=category,
+            units=int(parts[0]),
+            hp=int(parts[4]),
+            weaknesses=traits.get("weak", []),
+            immunities=traits.get("immune", []),
+            damage=int(parts[-6]) + boost,
+            damage_type=parts[-5],
+            initiative=int(parts[-1]),
+        )
 
-def get_armies():
-    armies = []
-    for i, group in enumerate(Parser().line_groups()):
-        armies.append([Group(i, j, g) for j, g in enumerate(group[1:])])
-    return armies
+    immune, infection = Parser().line_groups()
+    id, groups = 0, []
+    for value in immune[1:]:
+        groups.append(parse_group(id, Category.IMMUNE, value, immune_boost))
+        id += 1
+    for value in infection[1:]:
+        groups.append(parse_group(id, Category.INFECTION, value, 0))
+        id += 1
+    return Battle(groups)
 
 
 if __name__ == "__main__":
