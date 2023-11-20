4ec2cbfabc8d6e979ecfa6de5188c0b2485c6dea
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-20 14:31:50 -0800

    Update logic for 2016/16 to remove dataclass and use bools rather than strings. About twice as fast now.

--- 2016/16/solver.py
+++ 2016/16/solver.py
@@ -1,46 +1,26 @@
-from dataclasses import dataclass
-
 from aoc import answer
 from aoc.parser import Parser
 
 
-@dataclass
-class Curve:
-    value: str
-
-    def modify(self):
-        reverse_flipped = self.flip(self.value[::-1])
-        self.value += "0" + reverse_flipped
-
-    @staticmethod
-    def flip(value):
-        result = []
-        for v in value:
-            add = "0" if v == "1" else "1"
-            result.append(add)
-        return "".join(result)
-
-
 def main() -> None:
-    curve = Curve(Parser(strip=True).string())
-    answer.part1("10010101010011101", fill_disk(curve, 272))
-    answer.part2("01100111101101111", fill_disk(curve, 35_651_584))
+    answer.part1("10010101010011101", fill_disk(272))
+    answer.part2("01100111101101111", fill_disk(35_651_584))
 
 
-def fill_disk(curve: Curve, length: int) -> str:
-    while len(curve.value) < length:
-        curve.modify()
-    return get_checksum(curve.value[:length])
+def fill_disk(length: int) -> str:
+    curve: list[bool] = [value == "1" for value in Parser(strip=True).string()]
+    while len(curve) < length:
+        flipped = [not value for value in curve[::-1]]
+        curve.append(False)
+        curve.extend(flipped)
+    checksum = get_checksum(curve[:length])
+    return "".join(["1" if value else "0" for value in checksum])
 
 
-def get_checksum(value: str) -> str:
-    result = []
-    for i in range(len(value) // 2):
-        start_index = i * 2
-        first, second = value[start_index], value[start_index + 1]
-        add = "1" if first == second else "0"
-        result.append(add)
-    result = "".join(result)
+def get_checksum(value: list[bool]) -> list[bool]:
+    result: list[bool] = []
+    for i in range(0, len(value), 2):
+        result.append(value[i] == value[i + 1])
     return result if len(result) % 2 == 1 else get_checksum(result)
 
 
