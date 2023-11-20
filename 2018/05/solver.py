cfbfbf6cade88c9f45f38849d2b0a42da756d091
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-19 21:15:21 -0800

    Improve implementation of 2018/05, about 30% faster because we inlined the logic of the staticmethod, crazy

--- 2018/05/solver.py
+++ 2018/05/solver.py
@@ -1,74 +1,52 @@
+from dataclasses import dataclass
+from typing import Optional, Self
+
 from aoc import answer
 from aoc.parser import Parser
 
 
+@dataclass
 class Polymer:
-    def __init__(self, units):
-        self.units = units
+    units: list[str]
 
-    def get_unit_types(self):
-        unit_types = set()
-        for unit in self.units:
-            unit_types.add(unit.lower())
-        return unit_types
-
-    def remove_unit_type(self, unit_type):
-        new_polymer_units = ""
-        for unit in self.units:
-            if unit.lower() != unit_type:
-                new_polymer_units += unit
-        return Polymer(new_polymer_units)
-
-    def react(self):
-        reaction_index = self.get_reaction_index()
+    def react(self) -> int:
+        reaction_index = self.get_reaction_index(None)
         while reaction_index is not None:
-            self.units = self.units[:reaction_index] + self.units[reaction_index + 2 :]
+            del self.units[reaction_index]
+            del self.units[reaction_index]
             reaction_index = self.get_reaction_index(reaction_index)
-
-    def get_reaction_index(self, previous=None):
-        start = 0 if previous is None else previous
-        for i, unit in enumerate(self.units[start:-1]):
-            next_unit = self.units[start + i + 1]
-            if self.collide(unit, next_unit):
-                return start + i
-        return None if previous is None else self.get_reaction_index()
-
-    def __len__(self):
         return len(self.units)
 
-    def __repr__(self):
-        return str(self)
+    def get_reaction_index(self, previous: Optional[int]) -> Optional[int]:
+        start = 0 if previous is None else previous
+        for i, u1 in enumerate(self.units[start:-1]):
+            u2 = self.units[start + i + 1]
+            if u1.islower() != u2.islower() and u1.lower() == u2.lower():
+                return start + i
+        return None if previous is None else self.get_reaction_index(None)
 
-    def __str__(self):
-        return str(self.units)
+    def get_unit_types(self) -> set[str]:
+        return set([unit.lower() for unit in set(self.units)])
 
-    @staticmethod
-    def collide(unit, next_unit):
-        if unit.islower() and next_unit.islower():
-            return False
-        if unit.isupper() and next_unit.isupper():
-            return False
-        return unit.lower() == next_unit.lower()
+    def remove_unit_type(self, unit_type: str) -> Self:
+        return type(self)([unit for unit in self.units if unit.lower() != unit_type])
 
 
-def main():
-    polymer = Polymer(Parser().string())
+def main() -> None:
+    polymer = Polymer([value for value in Parser().string()])
     answer.part1(11242, solve_part_1(polymer))
     answer.part2(5492, solve_part_2(polymer))
 
 
-def solve_part_1(polymer):
-    polymer.react()
-    return len(polymer)
+def solve_part_1(polymer: Polymer) -> int:
+    return polymer.react()
 
 
-def solve_part_2(polymer):
-    lengths = []
-    unit_types = polymer.get_unit_types()
-    for unit_type in unit_types:
-        new_polymer = polymer.remove_unit_type(unit_type)
-        new_polymer.react()
-        lengths.append(len(new_polymer))
+def solve_part_2(polymer: Polymer) -> int:
+    lengths = [
+        polymer.remove_unit_type(unit_type).react()
+        for unit_type in polymer.get_unit_types()
+    ]
     return min(lengths)
 
 
