fe6de3b1536c6bb0e85c25ef95d2fc48262df68b
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-21 13:23:01 -0800

    Update python version of 2015/06 to look closer to Rust version, no impact on runtime

--- 2015/06/solver.py
+++ 2015/06/solver.py
@@ -1,13 +1,17 @@
+from typing import Callable
+
 from aoc import answer
 from aoc.parser import Parser
 
-SINGLE = dict(
+Actions = dict[str, Callable[[int], int]]
+
+SINGLE: Actions = dict(
     on=lambda _: 1,
     off=lambda _: 0,
     toggle=lambda current: 1 - current,
 )
 
-DIMABLE = dict(
+DIMABLE: Actions = dict(
     on=lambda current: current + 1,
     off=lambda current: max(current - 1, 0),
     toggle=lambda current: current + 2,
@@ -21,12 +25,11 @@ class Direction:
         self.start = Direction.to_point(values[-3])
         self.end = Direction.to_point(values[-1])
 
-    def apply(self, grid: list[tuple[int, int]]) -> None:
+    def apply(self, grid: list[int], actions: Actions) -> None:
         for x in range(self.start[0], self.end[0] + 1):
             for y in range(self.start[1], self.end[1] + 1):
                 index = (x * 1_000) + y
-                lit, bright = grid[index]
-                grid[index] = (SINGLE[self.action](lit), DIMABLE[self.action](bright))
+                grid[index] = actions[self.action](grid[index])
 
     @staticmethod
     def to_point(coords: str) -> tuple[int, int]:
@@ -35,21 +38,16 @@ class Direction:
 
 
 def main() -> None:
-    grid = run_grid()
-    answer.part1(400410, sum_grid(grid, 0))
-    answer.part2(15343601, sum_grid(grid, 1))
-
-
-def run_grid() -> list[tuple[int, int]]:
     directions = [Direction(line) for line in Parser().lines()]
-    grid: list[tuple[int, int]] = [(0, 0)] * 1_000_000
+    answer.part1(400410, apply_all(directions, SINGLE))
+    answer.part2(15343601, apply_all(directions, DIMABLE))
+
+
+def apply_all(directions: list[Direction], actions: Actions) -> int:
+    grid: list[int] = [0] * 1_000_000
     for direction in directions:
-        direction.apply(grid)
-    return grid
-
-
-def sum_grid(grid: list[tuple[int, int]], index: int) -> int:
-    return sum([value[index] for value in grid])
+        direction.apply(grid, actions)
+    return sum(grid)
 
 
 if __name__ == "__main__":
