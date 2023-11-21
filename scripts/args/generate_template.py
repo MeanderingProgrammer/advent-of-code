1654285eb000a05167546e298c8da8ccbb401beb
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-21 10:21:06 -0800

    Use enums for different strategy types rather than dictionaries

--- scripts/args/generate_template.py
+++ scripts/args/generate_template.py
@@ -1,27 +1,24 @@
-from typing import Callable
+from enum import StrEnum, auto
 
 from component.day_factory import DayFactory
 from pojo.day import Day
 
 
-class GenerateTemplate:
-    def __init__(self):
-        self.__templates: dict[str, Callable[[], Day]] = {
-            "next": GenerateTemplate.__next,
-            "current": GenerateTemplate.__current,
-        }
+class GenerateName(StrEnum):
+    NEXT = auto()
+    CURRENT = auto()
 
+
+class GenerateTemplate:
     def get_names(self) -> list[str]:
-        return list(self.__templates.keys())
+        return [generate_name.value for generate_name in GenerateName]
 
     def get(self, name: str) -> Day:
-        return self.__templates[name]()
-
-    @staticmethod
-    def __next() -> Day:
-        latest_day = DayFactory().get_latest()
-        return latest_day.add(1)
-
-    @staticmethod
-    def __current() -> Day:
-        return DayFactory().get_latest()
+        generate_name = GenerateName(name)
+        if generate_name == GenerateName.NEXT:
+            latest_day = DayFactory().get_latest()
+            return latest_day.add(1)
+        elif generate_name == GenerateName.CURRENT:
+            return DayFactory().get_latest()
+        else:
+            raise Exception(f"Unhandled name: {generate_name}")
