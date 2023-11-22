bb0bb2c950eca3fe3d40c872843f82aa4e1db3b9
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-21 20:41:46 -0800

    Move 2015/23 off of general Computer used in 2016, implement own class

--- 2015/23/solver.py
+++ 2015/23/solver.py
@@ -1,6 +1,25 @@
+from dataclasses import dataclass
+
 from aoc import answer
-from aoc.computer import Computer
 from aoc.parser import Parser
+
+
+@dataclass(frozen=True)
+class Computer:
+    registers: dict[str, int]
+
+    def get(self, register: str) -> int:
+        return self.registers[register]
+
+    def set(self, register: str, value: int) -> None:
+        self.registers[register] = value
+
+    def run(self, instructions: list["Instruction"]) -> None:
+        ip = 0
+        while ip >= 0 and ip < len(instructions):
+            instruction = instructions[ip]
+            move = instruction.run(self)
+            ip += move
 
 
 class Instruction:
@@ -8,52 +27,43 @@ class Instruction:
         self.op, args = raw.split(" ", 1)
         self.args = [arg for arg in args.split(", ")]
 
-    def run(self, computer):
+    def run(self, computer: Computer) -> int:
         if self.op == "hlf":
             current = computer.get(self.args[0])
             computer.set(self.args[0], current // 2)
-            computer.move(1)
+            return 1
         elif self.op == "tpl":
             current = computer.get(self.args[0])
             computer.set(self.args[0], current * 3)
-            computer.move(1)
+            return 1
         elif self.op == "inc":
             current = computer.get(self.args[0])
             computer.set(self.args[0], current + 1)
-            computer.move(1)
+            return 1
         elif self.op == "jmp":
-            amount = int(self.args[0])
-            computer.move(amount)
+            return int(self.args[0])
         elif self.op == "jie":
             current = computer.get(self.args[0])
             amount = int(self.args[1])
-            amount = amount if current % 2 == 0 else 1
-            computer.move(amount)
+            return amount if current % 2 == 0 else 1
         elif self.op == "jio":
             current = computer.get(self.args[0])
             amount = int(self.args[1])
-            amount = amount if current == 1 else 1
-            computer.move(amount)
+            return amount if current == 1 else 1
         else:
-            raise Exception("Unknown operation: {}".format(self.op))
+            raise Exception(f"Unknown operation: {self.op}")
 
 
-def main():
-    answer.part1(170, run_computer(False))
-    answer.part2(247, run_computer(True))
+def main() -> None:
+    answer.part1(170, run_computer(0))
+    answer.part2(247, run_computer(1))
 
 
-def run_computer(set_a):
-    computer = Computer(["a", "b"])
-    if set_a:
-        computer.set("a", 1)
-    instructions = get_instructions()
+def run_computer(a_value: int) -> int:
+    computer = Computer(registers=dict(a=a_value, b=0))
+    instructions = [Instruction(line) for line in Parser().lines()]
     computer.run(instructions)
     return computer.get("b")
-
-
-def get_instructions():
-    return [Instruction(line) for line in Parser().lines()]
 
 
 if __name__ == "__main__":
