e58aa9e4eae80463f82bcd62c28db0216c89d975
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-21 21:31:52 -0800

    Move all instruction logic for int-code of 2016 into Computer class, removing all the copy pasta

--- 2016/25/solver.py
+++ 2016/25/solver.py
@@ -3,146 +3,24 @@ from aoc.computer import Computer
 from aoc.parser import Parser
 
 
-class Setter:
-    def __init__(self, register, value, absolute):
-        self.register = register
-        self.value = value
-        self.absolute = absolute
-
-    def run(self, computer):
-        value = computer.get(self.value)
-        value = value if self.absolute else computer.get(self.register) + value
-        computer.set(self.register, value)
-        computer.move(1)
-
-    def toggle(self):
-        if self.absolute:
-            return Jump(self.value, self.register)
-        else:
-            value = "1" if self.value == "-1" else "-1"
-            self.value = value
-            return self
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        operation = "=" if self.absolute else "+="
-        return "{} {} {}".format(self.register, operation, self.value)
-
-
-class Jump:
-    def __init__(self, register, value):
-        self.register = register
-        self.value = value
-
-    def run(self, computer):
-        if computer.get(self.register) != 0:
-            computer.move(computer.get(self.value))
-        else:
-            computer.move(1)
-
-    def toggle(self):
-        return Setter(self.value, self.register, True)
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "Jump {} if {} != 0".format(self.value, self.register)
-
-
-class Toggle:
-    def __init__(self, register):
-        self.register = register
-
-    def run(self, computer):
-        ip = computer.ip + computer.get(self.register)
-        instructions = computer.instructions
-        if ip < len(instructions):
-            instruction = instructions[ip]
-            instructions[ip] = instruction.toggle()
-        computer.move(1)
-
-    def toggle(self):
-        return Setter(self.register, "1", False)
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "Toggle: {}".format(self.register)
-
-
-class Output:
-    def __init__(self, register):
-        self.register = register
-
-    def run(self, computer):
-        value = computer.get(self.register)
-        computer.output(value)
-        if not self.outputs_valid(computer.outputs):
-            raise Exception("Invalid Output Values")
-        computer.move(1)
-
-    def outputs_valid(self, outputs):
-        pattern = [0, 1]
-        for i, value in enumerate(outputs):
-            expected = pattern[i % len(pattern)]
-            if value != expected:
-                return False
-        return True
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "Output: {}".format(self.register)
-
-
-def main():
+def main() -> None:
     answer.part1(198, run_until_success())
 
 
-def run_until_success():
+def run_until_success() -> int:
     i = 0
     while True:
         try:
             run_computer(i)
             return i
         except Exception:
-            # Do nothing, move onto next iteration
             pass
         i += 1
 
 
-def run_computer(initial_value):
-    computer = Computer(["a", "b", "c", "d"], 100)
-    computer.set("a", initial_value)
-    computer.run(get_instructions())
-
-
-def get_instructions():
-    instructions = []
-    for line in Parser().lines():
-        parts = line.split()
-        op = parts[0]
-        if op == "cpy":
-            instruction = Setter(parts[2], parts[1], True)
-        elif op == "inc":
-            instruction = Setter(parts[1], "1", False)
-        elif op == "dec":
-            instruction = Setter(parts[1], "-1", False)
-        elif op == "jnz":
-            instruction = Jump(parts[1], parts[2])
-        elif op == "tgl":
-            instruction = Toggle(parts[1])
-        elif op == "out":
-            instruction = Output(parts[1])
-        else:
-            raise Exception("Unknown operation: {}".format(op))
-        instructions.append(instruction)
-    return instructions
+def run_computer(initial_value: int):
+    computer = Computer(registers=dict(a=initial_value, b=0, c=0, d=0), num_outputs=100)
+    computer.run(Parser().lines())
 
 
 if __name__ == "__main__":
