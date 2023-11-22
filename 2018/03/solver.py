d7b8c4ad968a434061e6815d2c49e16d9828766e
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-22 14:25:34 -0800

    Improve 2018/03 by using a tuple instead of Point class

--- 2018/03/solver.py
+++ 2018/03/solver.py
@@ -1,57 +1,67 @@
 import re
+from dataclasses import dataclass
 
 from aoc import answer
-from aoc.board import Grid, Point
 from aoc.parser import Parser
 
-CLAIM_PATTERN = "^#(.*) @ (.*),(.*): (.*)x(.*)$"
+Point = tuple[int, int]
 
 
+@dataclass(frozen=True)
 class Claim:
-    def __init__(self, value):
-        match = re.match(CLAIM_PATTERN, value)
-        self.claim_id = int(match[1])
-        self.point = Point(int(match[2]), int(match[3]))
-        self.width = int(match[4])
-        self.height = int(match[5])
+    claim_id: int
+    point: Point
+    width: int
+    height: int
 
-    def get_points(self):
-        points = set()
+    def get_points(self) -> list[Point]:
+        points: list[Point] = []
         for x in range(self.width):
             for y in range(self.height):
-                points.add(self.point + Point(x, y))
+                points.append((self.point[0] + x, self.point[1] + y))
         return points
 
-    def __repr__(self):
-        return str(self)
 
-    def __str__(self):
-        return "{} @ {}: {}x{}".format(
-            self.claim_id, self.point, self.width, self.height
-        )
+def main() -> None:
+    grid: dict[Point, list[int]] = get_overlap_grid()
+
+    all_claims: set[int] = set()
+    multiple_claims: set[int] = set()
+    multiple_claim_points: int = 0
+    for claim_ids in grid.values():
+        all_claims.update(claim_ids)
+        if len(claim_ids) > 1:
+            multiple_claims.update(claim_ids)
+            multiple_claim_points += 1
+
+    answer.part1(120408, multiple_claim_points)
+    answer.part2(1276, next(iter(all_claims.difference(multiple_claims))))
 
 
-def main():
-    grid = Grid()
-    all_claims = set()
-
-    for line in Parser().lines():
-        claim = Claim(line)
+def get_overlap_grid() -> dict[Point, list[int]]:
+    grid: dict[Point, list[int]] = dict()
+    for claim in get_claims():
         for point in claim.get_points():
-            all_claims.add(claim.claim_id)
             if point not in grid:
                 grid[point] = []
             grid[point].append(claim.claim_id)
+    return grid
 
-    multiple_claims = 0
-    for point, claims_on_point in grid.items():
-        if len(claims_on_point) > 1:
-            multiple_claims += 1
-            for claim_on_point in claims_on_point:
-                all_claims.discard(claim_on_point)
 
-    answer.part1(120408, multiple_claims)
-    answer.part2(1276, next(iter(all_claims)))
+def get_claims() -> list[Claim]:
+    claim_pattern = "^#(.*) @ (.*),(.*): (.*)x(.*)$"
+    claims: list[Claim] = []
+    for line in Parser().lines():
+        match = re.match(claim_pattern, line)
+        assert match is not None
+        claim = Claim(
+            claim_id=int(match[1]),
+            point=(int(match[2]), int(match[3])),
+            width=int(match[4]),
+            height=int(match[5]),
+        )
+        claims.append(claim)
+    return claims
 
 
 if __name__ == "__main__":
