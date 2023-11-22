921f8e0366e4107343245f5204520154b55765c6
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-21 21:31:52 -0800

    Move all instruction logic for int-code of 2016 into Computer class, removing all the copy pasta

--- 2016/23/solver.py
+++ 2016/23/solver.py
@@ -5,110 +5,17 @@ from aoc.computer import Computer
 from aoc.parser import Parser
 
 
-class Setter:
-    def __init__(self, register, value, absolute):
-        self.register = register
-        self.value = value
-        self.absolute = absolute
-
-    def run(self, computer):
-        value = computer.get(self.value)
-        value = value if self.absolute else computer.get(self.register) + value
-        computer.set(self.register, value)
-        computer.move(1)
-
-    def toggle(self):
-        if self.absolute:
-            return Jump(self.value, self.register)
-        else:
-            value = "1" if self.value == "-1" else "-1"
-            self.value = value
-            return self
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        operation = "=" if self.absolute else "+="
-        return "{} {} {}".format(self.register, operation, self.value)
-
-
-class Jump:
-    def __init__(self, register, value):
-        self.register = register
-        self.value = value
-
-    def run(self, computer):
-        if computer.get(self.register) != 0:
-            computer.move(computer.get(self.value))
-        else:
-            computer.move(1)
-
-    def toggle(self):
-        return Setter(self.value, self.register, True)
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "Jump {} if {} != 0".format(self.value, self.register)
-
-
-class Toggle:
-    def __init__(self, register):
-        self.register = register
-
-    def run(self, computer):
-        ip = computer.ip + computer.get(self.register)
-        instructions = computer.instructions
-        if ip < len(instructions):
-            instruction = instructions[ip]
-            instructions[ip] = instruction.toggle()
-        computer.move(1)
-
-    def toggle(self):
-        return Setter(self.register, "1", False)
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "Toggle: {}".format(self.register)
-
-
-def main():
-    answer.part1(11662, run_computer(7))
+def main() -> None:
+    answer.part1(11662, run(7))
     # Ends up simplifying to n! + 77*86
     # Did some reverse engineering
     answer.part2(479008222, math.factorial(12) + 77 * 86)
 
 
-def run_computer(num_eggs):
-    computer = Computer(["a", "b", "c", "d"])
-    computer.set("a", num_eggs)
-    computer.run(get_instructions())
+def run(num_eggs: int) -> int:
+    computer = Computer(registers=dict(a=num_eggs, b=0, c=0, d=0))
+    computer.run(Parser().lines())
     return computer.get("a")
-
-
-def get_instructions():
-    instructions = []
-    for line in Parser().lines():
-        parts = line.split()
-        op = parts[0]
-        if op == "cpy":
-            instruction = Setter(parts[2], parts[1], True)
-        elif op == "inc":
-            instruction = Setter(parts[1], "1", False)
-        elif op == "dec":
-            instruction = Setter(parts[1], "-1", False)
-        elif op == "jnz":
-            instruction = Jump(parts[1], parts[2])
-        elif op == "tgl":
-            instruction = Toggle(parts[1])
-        else:
-            raise Exception("Unknown operation: {}".format(op))
-        instructions.append(instruction)
-    return instructions
 
 
 if __name__ == "__main__":
--- commons/python/aoc/computer.py
+++ commons/python/aoc/computer.py
@@ -1,37 +1,153 @@
+import abc
+from dataclasses import dataclass, field
+from typing import Optional, override
+
+
+class Instruction(abc.ABC):
+    @abc.abstractmethod
+    def run(self, computer: "Computer") -> int:
+        pass
+
+    @abc.abstractmethod
+    def toggle(self) -> "Instruction":
+        pass
+
+
+@dataclass(kw_only=True)
+class Setter(Instruction):
+    register: str
+    value: str
+    absolute: bool
+
+    @override
+    def run(self, computer: "Computer") -> int:
+        value = computer.get(self.value)
+        value = value if self.absolute else computer.get(self.register) + value
+        computer.set(self.register, value)
+        return 1
+
+    @override
+    def toggle(self) -> Instruction:
+        if self.absolute:
+            return Jump(register=self.value, value=self.register)
+        else:
+            self.value = "1" if self.value == "-1" else "-1"
+            return self
+
+
+@dataclass(frozen=True, kw_only=True)
+class Jump(Instruction):
+    register: str
+    value: str
+
+    @override
+    def run(self, computer: "Computer") -> int:
+        if computer.get(self.register) != 0:
+            return computer.get(self.value)
+        else:
+            return 1
+
+    @override
+    def toggle(self) -> Instruction:
+        return Setter(register=self.value, value=self.register, absolute=True)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Toggle(Instruction):
+    register: str
+
+    @override
+    def run(self, computer: "Computer") -> int:
+        ip = computer.ip + computer.get(self.register)
+        instructions = computer.instructions
+        if ip < len(instructions):
+            instruction = instructions[ip]
+            instructions[ip] = instruction.toggle()
+        return 1
+
+    @override
+    def toggle(self) -> Instruction:
+        return Setter(register=self.register, value="1", absolute=False)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Output(Instruction):
+    register: str
+
+    @override
+    def run(self, computer: "Computer") -> int:
+        value = computer.get(self.register)
+        computer.output(value)
+        if not self.outputs_valid(computer.outputs):
+            raise Exception("Invalid Output Values")
+        return 1
+
+    def outputs_valid(self, outputs):
+        pattern = [0, 1]
+        for i, value in enumerate(outputs):
+            expected = pattern[i % len(pattern)]
+            if value != expected:
+                return False
+        return True
+
+    @override
+    def toggle(self) -> Instruction:
+        raise Exception("Toggling is not valid for Output")
+
+
+def parse_instructions(lines: list[str]) -> list[Instruction]:
+    instructions = []
+    for line in lines:
+        parts = line.split()
+        op = parts[0]
+        if op == "cpy":
+            instruction = Setter(register=parts[2], value=parts[1], absolute=True)
+        elif op == "inc":
+            instruction = Setter(register=parts[1], value="1", absolute=False)
+        elif op == "dec":
+            instruction = Setter(register=parts[1], value="-1", absolute=False)
+        elif op == "jnz":
+            instruction = Jump(register=parts[1], value=parts[2])
+        elif op == "tgl":
+            instruction = Toggle(register=parts[1])
+        elif op == "out":
+            instruction = Output(register=parts[1])
+        else:
+            raise Exception(f"Unknown operation: {op}")
+        instructions.append(instruction)
+    return instructions
+
+
+@dataclass
 class Computer:
-    def __init__(self, registers, num_outputs=None):
-        self.registers = {register: 0 for register in registers}
-        self.ip = 0
-        self.instructions = None
+    registers: dict[str, int]
+    num_outputs: Optional[int] = None
+    outputs: list[int] = field(default_factory=list)
+    ip: int = 0
 
-        self.num_outputs = num_outputs
-        self.outputs = []
-
-    def output(self, value):
+    def output(self, value: int) -> None:
         self.outputs.append(value)
 
-    def get(self, value):
+    def get(self, value: str) -> int:
         return self.registers[value] if value in self.registers else int(value)
 
-    def set(self, register, value):
+    def set(self, register: str, value: int) -> None:
         self.registers[register] = value
 
-    def move(self, amount):
+    def move(self, amount: int) -> None:
         self.ip += amount
 
-    def run(self, instructions):
-        self.instructions = instructions
-
+    def run(self, lines: list[str]) -> None:
+        self.instructions = parse_instructions(lines)
         while self.in_range() and not self.met_success():
-            instruction = instructions[self.ip]
-            instruction.run(self)
+            instruction = self.instructions[self.ip]
+            move = instruction.run(self)
+            self.ip += move
 
-        self.instructions = None
-
-    def in_range(self):
+    def in_range(self) -> bool:
         return self.ip >= 0 and self.ip < len(self.instructions)
 
-    def met_success(self):
+    def met_success(self) -> bool:
         if self.num_outputs is None:
             return False
         return len(self.outputs) >= self.num_outputs
