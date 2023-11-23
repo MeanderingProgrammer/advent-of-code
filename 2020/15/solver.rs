e4064b6f26b4140f25f1fb4b32e022e1321ee5b2
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-22 19:43:09 -0800

    Minor edits to 2020/15, slight improvement maybe

--- 2020/15/solver.rs
+++ 2020/15/solver.rs
@@ -1,22 +1,21 @@
 use aoc_lib::answer;
 use aoc_lib::reader;
-use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Stats {
-    latest: i64,
-    previous: Option<i64>,
+    latest: usize,
+    previous: Option<usize>,
 }
 
 impl Stats {
     fn new(turn: usize) -> Self {
         Self {
-            latest: turn.try_into().unwrap(),
+            latest: turn,
             previous: None,
         }
     }
 
-    fn next(&self) -> i64 {
+    fn next(&self) -> usize {
         match self.previous {
             Some(value) => self.latest - value,
             None => 0,
@@ -25,31 +24,31 @@ impl Stats {
 
     fn said(&mut self, turn: usize) {
         self.previous = Some(self.latest);
-        self.latest = turn.try_into().unwrap();
+        self.latest = turn;
     }
 }
 
 fn main() {
-    let starting_numbers = reader::read_csv();
-    answer::part1(240, run(&starting_numbers, 2_020));
-    answer::part2(505, run(&starting_numbers, 30_000_000));
+    let values = reader::read_csv()
+        .into_iter()
+        .map(|value| value as usize)
+        .collect();
+    answer::part1(240, run(&values, 2_020));
+    answer::part2(505, run(&values, 30_000_000));
 }
 
-fn run(starting_numbers: &Vec<i64>, n: usize) -> u32 {
-    let mut number_stats: HashMap<i64, Stats> = HashMap::new();
-    starting_numbers.iter().enumerate().for_each(|(i, value)| {
-        number_stats.insert(value.clone(), Stats::new(i));
+fn run(values: &Vec<usize>, n: usize) -> usize {
+    let mut number_stats: Vec<Option<Stats>> = vec![None; n];
+    values.iter().enumerate().for_each(|(i, value)| {
+        number_stats[*value] = Some(Stats::new(i));
     });
-
-    let mut number: i64 = *starting_numbers.last().unwrap();
-    for i in starting_numbers.len()..n {
-        number = number_stats.get(&number).unwrap().next();
-        match number_stats.get_mut(&number) {
+    let mut number: usize = *values.last().unwrap();
+    for i in values.len()..n {
+        number = number_stats.get(number).unwrap().as_ref().unwrap().next();
+        match number_stats.get_mut(number).unwrap() {
             Some(stats) => stats.said(i),
-            None => {
-                number_stats.insert(number, Stats::new(i));
-            }
+            None => number_stats[number] = Some(Stats::new(i)),
         };
     }
-    number.try_into().unwrap()
+    number
 }
