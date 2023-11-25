12638b1160d053cd1c4e29e8a66a3d7566e8867c
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-25 04:34:28 -0800

    Improve 2017/03 by using map and tuple

--- 2017/03/solver.py
+++ 2017/03/solver.py
@@ -1,60 +1,62 @@
 from aoc import answer
-from aoc.board import Grid, Point
 from aoc.parser import Parser
+
+Point = tuple[int, int]
+Grid = dict[Point, int]
+TO_POINT: dict[str, Point] = dict(
+    u=(0, 1),
+    d=(0, -1),
+    r=(1, 0),
+    l=(-1, 0),
+)
+NEXT_DIRECTION: dict[str, str] = dict(
+    r="u",
+    u="l",
+    l="d",
+    d="r",
+)
+
+
+def add(p1: Point, p2: Point) -> Point:
+    return (p1[0] + p2[0], p1[1] + p2[1])
+
+
+def adjacent(p: Point) -> list[Point]:
+    all_directions = list(TO_POINT.values())
+    all_directions.extend([(1, 1), (1, -1), (-1, 1), (-1, -1)])
+    return [add(p, direction) for direction in all_directions]
 
 
 def main() -> None:
     goal = Parser().integer()
-    answer.part1(419, len(build_grid(goal, updater_v1)[0]))
-    answer.part2(295229, build_grid(goal, updater_v2)[1])
+    answer.part1(419, build_grid(goal, part1)[0])
+    answer.part2(295229, build_grid(goal, part2)[1])
 
 
-def build_grid(goal: int, updater) -> tuple[Point, int]:
-    point, value = Point(0, 0), 1
-    grid = Grid()
-    grid[point] = value
-
+def build_grid(goal: int, updater) -> tuple[int, int]:
+    point: Point = (0, 0)
+    value: int = 1
+    grid: Grid = {point: value}
+    direction: str = "r"
     while value < goal:
-        up_in = point.up() in grid
-        down_in = point.down() in grid
-        left_in = point.left() in grid
-        right_in = point.right() in grid
-
-        if not up_in and not down_in and not left_in and not right_in:
-            point = point.right()
-        elif not up_in and not right_in:
-            if left_in:
-                point = point.up()
-            else:
-                point = point.left()
-        elif not up_in and not left_in:
-            if down_in:
-                point = point.left()
-            else:
-                point = point.down()
-        elif not down_in and not left_in:
-            if right_in:
-                point = point.down()
-            else:
-                point = point.right()
-        elif not down_in and not right_in:
-            if up_in:
-                point = point.right()
-            else:
-                point = point.up()
-
+        point = add(point, TO_POINT[direction])
         value = updater(value, grid, point)
         grid[point] = value
+        next_direction = NEXT_DIRECTION[direction]
+        direction = (
+            direction
+            if add(point, TO_POINT[next_direction]) in grid
+            else next_direction
+        )
+    return abs(point[0]) + abs(point[1]), value
 
-    return point, value
 
-
-def updater_v1(previous: int, grid: Grid, point: Point) -> int:
+def part1(previous: int, grid: Grid, point: Point) -> int:
     return previous + 1
 
 
-def updater_v2(_: int, grid: Grid, point: Point) -> int:
-    return sum([grid.get(neighbor, 0) for neighbor in point.adjacent(True)])
+def part2(_: int, grid: Grid, point: Point) -> int:
+    return sum([grid.get(neighbor, 0) for neighbor in adjacent(point)])
 
 
 if __name__ == "__main__":
