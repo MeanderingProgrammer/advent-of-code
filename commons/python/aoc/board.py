a3d60559f917f8b99334bbcb2e8b8f9666eef395
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-28 21:28:21 -0800

    Add more type hints to python commons

--- commons/python/aoc/board.py
+++ commons/python/aoc/board.py
@@ -42,7 +42,7 @@ class Point:
 
     # Get Adjacent points (either includes diagonals or not)
 
-    def adjacent(self, diagonal=False):
+    def adjacent(self, diagonal=False) -> list[Self]:
         adjacent = [self] if diagonal else []
         for i in range(self.dimensions()):
             to_add = []
@@ -58,67 +58,67 @@ class Point:
 
     # Some Simple Directions (up increases y, careful when reading top to bottom)
 
-    def right(self):
+    def right(self) -> Self:
         return self.__create(0, 1)
 
-    def left(self):
+    def left(self) -> Self:
         return self.__create(0, -1)
 
-    def up(self):
+    def up(self) -> Self:
         return self.__create(1, 1)
 
-    def down(self):
+    def down(self) -> Self:
         return self.__create(1, -1)
 
-    def __create(self, i, amount):
+    def __create(self, i, amount) -> Self:
         coords = list(self.__coords)
         coords[i] += amount
-        return Point(*coords)
+        return type(self)(*coords)
 
-    def validate(self, o):
+    def validate(self, o: Self) -> None:
         if self.dimensions() != o.dimensions():
             raise Exception("Cannot compare points of different dimensionality")
 
-    def __len__(self):
+    def __len__(self) -> int:
         return sum([abs(coord) for coord in self.__coords])
 
     # Math Operations Between Points
 
-    def __add__(self, o):
+    def __add__(self, o: Self) -> Self:
         return self.__math(o, lambda coord_1, coord_2: coord_1 + coord_2)
 
-    def __sub__(self, o):
+    def __sub__(self, o: Self) -> Self:
         return self.__math(o, lambda coord_1, coord_2: coord_1 - coord_2)
 
-    def __math(self, o, f):
+    def __math(self, o: Self, f) -> Self:
         self.validate(o)
         coords = []
         for coord_1, coord_2 in zip(self.__coords, o.__coords):
             coords.append(f(coord_1, coord_2))
-        return Point(*coords)
+        return type(self)(*coords)
 
     # Math Operations Between Point and Scalar
 
-    def __mul__(self, amount):
+    def __mul__(self, amount: int) -> Self:
         coords = []
         for coord in self.__coords:
             coords.append(coord * amount)
-        return Point(*coords)
+        return type(self)(*coords)
 
-    def __rmul__(self, amount):
+    def __rmul__(self, amount: int) -> Self:
         return self.__mul__(amount)
 
     # Equality checks start from highest dimension
 
-    def __lt__(self, o):
+    def __lt__(self, o: Self) -> bool:
         comparison = self.__compare(o, lambda coord_1, coord_2: coord_1 < coord_2)
         return False if comparison is None else comparison
 
-    def __le__(self, o):
+    def __le__(self, o: Self) -> bool:
         comparison = self.__compare(o, lambda coord_1, coord_2: coord_1 < coord_2)
         return True if comparison is None else comparison
 
-    def __compare(self, o, f):
+    def __compare(self, o: Self, f) -> Optional[bool]:
         self.validate(o)
         zipped = list(zip(self.__coords, o.__coords))
         zipped.reverse()
@@ -129,16 +129,16 @@ class Point:
 
     # Basic Equality, Hash, and to String implementations
 
-    def __eq__(self, o):
+    def __eq__(self, o) -> bool:
         return isinstance(o, Point) and self.__coords == o.__coords
 
     def __hash__(self):
         return hash(str(self))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.__coords)
 
 
@@ -147,7 +147,7 @@ class Grid:
         self.__grid = {}
         self.__dimensionality = None
 
-    def get(self, point, default_value):
+    def get(self, point: Point, default_value):
         return self[point] if point in self else default_value
 
     def items(self):
@@ -179,23 +179,23 @@ class Grid:
             return None
         return set([point.y() for point in self.__grid])
 
-    def __setitem__(self, point, value):
+    def __setitem__(self, point: Point, value) -> None:
         if self.__dimensionality is None:
             self.__dimensionality = point.dimensions()
         elif self.__dimensionality != point.dimensions():
             raise Exception("Cannot create grid with mismatching dimensions")
         self.__grid[point] = value
 
-    def __getitem__(self, point):
+    def __getitem__(self, point: Point):
         return self.__grid.get(point, None)
 
-    def __contains__(self, point):
+    def __contains__(self, point: Point) -> bool:
         return point in self.__grid
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self)
 
-    def __str__(self):
+    def __str__(self) -> str:
         ys = self.ys()
         if ys is None:
             return self.__make_row()
@@ -207,7 +207,7 @@ class Grid:
             rows.append(self.__make_row(y))
         return "\n".join(rows)
 
-    def __make_row(self, y=None):
+    def __make_row(self, y=None) -> str:
         row, xs = [], self.xs()
         for x in range(min(xs), max(xs) + 1):
             point = Point(x) if y is None else Point(x, y)
