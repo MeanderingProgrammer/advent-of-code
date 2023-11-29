cb1cd312e0780aa8f2f6129a28f8ba315acfc19f
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-28 17:46:03 -0800

    Add type hints to 2015/07

--- 2015/07/solver.py
+++ 2015/07/solver.py
@@ -1,54 +1,40 @@
-import numpy as np
+from dataclasses import dataclass
+from typing import Optional, Self
+
 from aoc import answer
 from aoc.parser import Parser
 
 
+@dataclass
 class Value:
-    def __init__(self, value):
-        self.value = value.split()
-        self.evaluated = None
+    value: list[str]
+    evaluated: Optional[int] = None
 
-    def evaluate(self, diagram):
-        if self.evaluated is not None:
-            return self.evaluated
+    def evaluate(self, diagram: dict[str, Self]) -> int:
+        if self.evaluated is None:
+            self.evaluated = self.compute(diagram)
+        return self.evaluated
 
+    def compute(self, diagram: dict[str, Self]) -> int:
         if len(self.value) == 1:
-            result = self.get_value(self.value[0], diagram)
+            return self.get_value(diagram, 0)
         elif len(self.value) == 2:
-            result = ~self.get_value(self.value[1], diagram)
+            return ~self.get_value(diagram, 1)
         elif len(self.value) == 3:
-            v1 = self.get_value(self.value[0], diagram)
+            v1 = self.get_value(diagram, 0)
             operator = self.value[1]
+            if operator == "AND":
+                return v1 & self.get_value(diagram, 2)
+            elif operator == "OR":
+                return v1 | self.get_value(diagram, 2)
+            elif operator == "LSHIFT":
+                return v1 << int(self.value[2])
+            elif operator == "RSHIFT":
+                return v1 >> int(self.value[2])
+        raise Exception(f"No idea how to handle: {self.value}")
 
-            if operator in ["AND", "OR"]:
-                v2 = self.get_value(self.value[2], diagram)
-                if operator == "AND":
-                    result = v1 & v2
-                elif operator == "OR":
-                    result = v1 | v2
-            elif operator in ["LSHIFT", "RSHIFT"]:
-                v2 = int(self.value[2])
-                if operator == "LSHIFT":
-                    result = v1 << v2
-                elif operator == "RSHIFT":
-                    result = v1 >> v2
-            else:
-                raise Exception("No idea how to handle: {}".format(self.value))
-        else:
-            raise Exception("No idea how to handle: {}".format(self.value))
-
-        result = np.array([result], dtype="uint16")[0]
-        self.evaluated = result
-        return result
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return " ".join(self.value)
-
-    @staticmethod
-    def get_value(variable, diagram):
+    def get_value(self, diagram: dict[str, Self], index: int):
+        variable: str = self.value[index]
         if variable in diagram:
             return diagram[variable].evaluate(diagram)
         else:
@@ -61,18 +47,18 @@ def main():
     answer.part2(14710, evaluate(first))
 
 
-def evaluate(b_override):
-    diagram = get_diagram()
+def evaluate(b_override: Optional[int]) -> int:
+    diagram: dict[str, Value] = get_diagram()
     if b_override is not None:
-        diagram["b"] = Value(str(b_override))
+        diagram["b"] = Value(str(b_override).split())
     return diagram["a"].evaluate(diagram)
 
 
-def get_diagram():
-    diagram = {}
+def get_diagram() -> dict[str, Value]:
+    diagram: dict[str, Value] = dict()
     for line in Parser().lines():
-        line = line.split(" -> ")
-        diagram[line[1]] = Value(line[0])
+        value, key = line.split(" -> ")
+        diagram[key] = Value(value.split())
     return diagram
 
 
