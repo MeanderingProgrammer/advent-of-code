a157ad177fc9e1421b6775716b65ba92be51b905
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-29 16:20:34 -0800

    Add type hints to 2018/08

--- 2018/08/solver.py
+++ 2018/08/solver.py
@@ -1,49 +1,43 @@
+from typing import Self
+
 from aoc import answer
 from aoc.parser import Parser
 
 
 class Node:
-    def __init__(self, values):
-        num_children = values[0]
-        num_metadata = values[1]
+    def __init__(self, values: list[int]):
+        num_children: int = values[0]
+        num_metadata: int = values[1]
         values = values[2:]
 
-        self.children = []
+        self.children: list[Self] = []
         for _ in range(num_children):
             child = Node(values)
             self.children.append(child)
             values = values[len(child) :]
 
-        self.metadata = values[:num_metadata]
+        self.metadata: list[int] = values[:num_metadata]
 
-    def sum_metadata(self):
-        total = sum(self.metadata)
-        for child in self.children:
-            total += child.sum_metadata()
-        return total
+    def sum_metadata(self) -> int:
+        return sum(self.metadata) + sum(
+            [child.sum_metadata() for child in self.children]
+        )
 
-    def value(self):
+    def value(self) -> int:
         if len(self.children) == 0:
             return sum(self.metadata)
-
-        total = 0
+        total: int = 0
         for index in self.metadata:
             index -= 1
             if index >= 0 and index < len(self.children):
                 total += self.children[index].value()
         return total
 
-    def __len__(self):
+    def __len__(self) -> int:
         return 2 + sum([len(child) for child in self.children]) + len(self.metadata)
 
-    def __repr__(self):
-        return str(self)
 
-    def __str__(self):
-        return "(Children = {}, Metadata = {})".format(self.children, self.metadata)
-
-
-def main():
+def main() -> None:
     tree = Node(Parser().int_entries())
     answer.part1(42472, tree.sum_metadata())
     answer.part2(21810, tree.value())
