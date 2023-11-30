ce7bad3fa1ea83b0be99637498f874212e175431
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-30 15:39:40 -0800

    Finish 2023 day 1

--- 2023/01/solver.ml
+++ 2023/01/solver.ml
@@ -1,25 +1,77 @@
-let rec find_first index value =
+let rec find_matching mapping index value =
+  match mapping with
+  | (k, v) :: xs ->
+      if index + String.length k <= String.length value then
+        let substring = String.sub value index (String.length k) in
+        if String.compare substring k == 0 then Some v
+        else find_matching xs index value
+      else find_matching xs index value
+  | [] -> None
+
+let rec find_first mapping index value =
   if index < String.length value then
-    match value.[index] with
-    | '0' .. '9' as digit -> int_of_char digit - int_of_char '0'
-    | _ -> find_first (index + 1) value
+    match find_matching mapping index value with
+    | Some v -> v
+    | None -> find_first mapping (index + 1) value
   else -1
 
-let rec find_last index value =
+let rec find_last mapping index value =
   if index >= 0 then
-    match value.[index] with
-    | '0' .. '9' as digit -> int_of_char digit - int_of_char '0'
-    | _ -> find_last (index - 1) value
+    match find_matching mapping index value with
+    | Some v -> v
+    | _ -> find_last mapping (index - 1) value
   else -1
 
-let rec sum result values =
+let rec sum mapping result values =
   match values with
   | x :: xs ->
-      let current = (10 * find_first 0 x) + find_last (String.length x - 1) x in
-      sum (current + result) xs
+      let current =
+        (10 * find_first mapping 0 x)
+        + find_last mapping (String.length x - 1) x
+      in
+      sum mapping (current + result) xs
   | _ -> result
 
 let () =
   let values = Aoc.Reader.read_lines () in
-  let part1 = sum 0 values in
-  Aoc.Answer.part1 55538 part1 string_of_int
+  let part1_mapping =
+    [
+      ("0", 0);
+      ("1", 1);
+      ("2", 2);
+      ("3", 3);
+      ("4", 4);
+      ("5", 5);
+      ("6", 6);
+      ("7", 7);
+      ("8", 8);
+      ("9", 9);
+    ]
+  in
+  let part1 = sum part1_mapping 0 values in
+  let part2_mapping =
+    [
+      ("0", 0);
+      ("1", 1);
+      ("2", 2);
+      ("3", 3);
+      ("4", 4);
+      ("5", 5);
+      ("6", 6);
+      ("7", 7);
+      ("8", 8);
+      ("9", 9);
+      ("one", 1);
+      ("two", 2);
+      ("three", 3);
+      ("four", 4);
+      ("five", 5);
+      ("six", 6);
+      ("seven", 7);
+      ("eight", 8);
+      ("nine", 9);
+    ]
+  in
+  let part2 = sum part2_mapping 0 values in
+  Aoc.Answer.part1 55538 part1 string_of_int;
+  Aoc.Answer.part2 54875 part2 string_of_int
