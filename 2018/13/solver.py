d26af699d613e057a59553fdaa6366f48285fdcf
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-30 18:46:05 -0800

    Type hints added up to 2018/16

--- 2018/13/solver.py
+++ 2018/13/solver.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from aoc import answer
 from aoc.board import Grid, Point
 from aoc.parser import Parser
@@ -7,16 +9,16 @@ RIGHT = ">"
 UP = "^"
 DOWN = "v"
 
-OPPOSITES = {LEFT: RIGHT, RIGHT: LEFT, UP: DOWN, DOWN: UP}
+OPPOSITES: dict[str, str] = {LEFT: RIGHT, RIGHT: LEFT, UP: DOWN, DOWN: UP}
 
-AS_POSITION = {
+AS_POSITION: dict[str, Point] = {
     LEFT: Point(-1, 0),
     RIGHT: Point(1, 0),
     UP: Point(0, -1),
     DOWN: Point(0, 1),
 }
 
-INTERSECTION_OPTIONS = {
+INTERSECTION_OPTIONS: dict[str, list[str]] = {
     LEFT: [DOWN, LEFT, UP],
     RIGHT: [UP, RIGHT, DOWN],
     UP: [LEFT, UP, RIGHT],
@@ -24,13 +26,13 @@ INTERSECTION_OPTIONS = {
 }
 
 
+@dataclass
 class Cart:
-    def __init__(self, position, direction):
-        self.position = position
-        self.direction = direction
-        self.choice = 0
+    position: Point
+    direction: str
+    choice: int
 
-    def go(self, options):
+    def go(self, options: list[Point]) -> None:
         if len(options) == 4:
             self.handle_intersection()
         elif len(options) == 2:
@@ -38,81 +40,53 @@ class Cart:
         else:
             raise Exception("Unhandled number of options")
 
-    def handle_intersection(self):
+    def handle_intersection(self) -> None:
         options = INTERSECTION_OPTIONS[self.direction]
         self.direction = options[self.choice % len(options)]
         self.choice += 1
         self.position += AS_POSITION[self.direction]
 
-    def standard_movement(self, options):
+    def standard_movement(self, options: list[Point]) -> None:
         opposite = OPPOSITES[self.direction]
         dont_go = self.position + AS_POSITION[opposite]
         options.remove(dont_go)
-
         if len(options) != 1:
             raise Exception("Unable to eliminate enough options")
-
         new_position = options[0]
         self.direction = self.how_to_get(new_position)
         self.position = new_position
 
-    def how_to_get(self, new_position):
+    def how_to_get(self, new_position: Point) -> str:
         for direction in AS_POSITION:
             if self.position + AS_POSITION[direction] == new_position:
                 return direction
         raise Exception("Unable to determine how to reach position")
 
-    def __eq__(self, o):
-        return str(self) == str(o)
 
-    def __hash__(self):
-        return hash(str(self))
-
-    def __lt__(self, o):
-        if self.position.y() == o.position.y():
-            return self.position.x() < o.position.x()
-        return self.position.y() < o.position.y()
-
-    def __repr__(self):
-        return str(self)
-
-    def __str__(self):
-        return "{} @ {}".format(self.direction, self.position)
-
-
+@dataclass(frozen=True)
 class CartSystem:
-    def __init__(self, track, carts):
-        self.track = track
-        self.carts = carts
+    track: Grid
+    carts: list[Cart]
+    crash_positions: list[Point]
 
-    def run_until_crash(self):
-        crash_positions = []
-        while len(crash_positions) == 0:
-            crash_positions = self.tick()
-        return crash_positions
-
-    def run_unit_one_remains(self):
+    def run(self) -> None:
         while len(self.carts) > 1:
             self.tick()
-        return self.carts[0].position
 
-    def tick(self):
-        crash_positions = []
-        carts_crashed = []
-        self.carts.sort()
+    def tick(self) -> None:
+        carts_crashed: list[Cart] = []
+        self.carts.sort(key=lambda cart: cart.position)
         for cart in self.carts:
-            adjacent = self.adjacent(cart.position)
-            cart.go(adjacent)
+            cart.go(self.get_options(cart.position))
             new_position = cart.position
-            carts_at_same_position = self.carts_at(new_position)
-            if len(carts_at_same_position) > 1:
-                carts_crashed.extend(carts_at_same_position)
-                crash_positions.append(new_position)
+            at_same_position = self.carts_at(new_position)
+            if len(at_same_position) > 1:
+                self.crash_positions.append(new_position)
+                carts_crashed.extend(at_same_position)
         for cart_crashed in carts_crashed:
             self.carts.remove(cart_crashed)
-        return crash_positions
 
-    def adjacent(self, point):
+    def get_options(self, point: Point) -> list[Point]:
         value = self.track[point]
         if value == "+":
             return [point.left(), point.right(), point.up(), point.down()]
@@ -133,45 +107,31 @@ class CartSystem:
         else:
             raise Exception("Unknown value at point")
 
-    def carts_at(self, position):
+    def carts_at(self, position: Point) -> list[Cart]:
         return [cart for cart in self.carts if cart.position == position]
 
-    def __repr__(self):
-        return str(self)
 
-    def __str__(self):
-        values = [[value for value in row] for row in str(self.track).split("\n")]
-
-        for cart in self.carts:
-            position = cart.position
-            values[position.y][position.x] = cart.direction
-
-        values = ["".join(row) for row in values]
-        return "\n".join(values)
+def main() -> None:
+    system = run_system()
+    answer.part1(Point(86, 118), system.crash_positions[0])
+    answer.part2(Point(2, 81), system.carts[0].position)
 
 
-def main():
-    data = get_data()
-
-    system = CartSystem(get_track(data), get_carts(data))
-    answer.part1(Point(86, 118), system.run_until_crash()[0])
-
-    system = CartSystem(get_track(data), get_carts(data))
-    answer.part2(Point(2, 81), system.run_unit_one_remains())
-
-
-def get_data():
-    data = {}
+def run_system() -> CartSystem:
+    data: dict[Point, str] = dict()
     for y, row in enumerate(Parser().nested_lines()):
         for x, value in enumerate(row):
             data[Point(x, y)] = value
-    return data
+    system = CartSystem(
+        track=get_track(data), carts=get_carts(data), crash_positions=[]
+    )
+    system.run()
+    return system
 
 
-def get_track(data):
+def get_track(data: dict[Point, str]) -> Grid:
     track = Grid()
-    for point in data:
-        value = data[point]
+    for point, value in data.items():
         if value != " ":
             value = "-" if value in [LEFT, RIGHT] else value
             value = "|" if value in [UP, DOWN] else value
@@ -179,12 +139,11 @@ def get_track(data):
     return track
 
 
-def get_carts(data):
-    carts = []
-    for point in data:
-        value = data[point]
-        if value in [LEFT, RIGHT, UP, DOWN]:
-            carts.append(Cart(point, value))
+def get_carts(data: dict[Point, str]) -> list[Cart]:
+    carts: list[Cart] = []
+    for point, value in data.items():
+        if value in AS_POSITION:
+            carts.append(Cart(position=point, direction=value, choice=0))
     return carts
 
 
