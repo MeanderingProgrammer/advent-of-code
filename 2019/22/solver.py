be1cb7e90f24e534ac03f8b9ae68b2b411978717
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-30 23:18:47 -0800

    Finish adding type hints up till 2020/05

--- 2019/22/solver.py
+++ 2019/22/solver.py
@@ -1,77 +1,71 @@
+from dataclasses import dataclass
+
 from aoc import answer
 from aoc.parser import Parser
+
+
+@dataclass
+class Deck:
+    n: int
+    a: int = 1
+    b: int = 0
+
+    def deal_into_new(self) -> None:
+        self.apply(-1, -1)
+
+    def cut(self, n: int) -> None:
+        self.apply(1, -n)
+
+    def deal(self, n: int) -> None:
+        self.apply(n, 0)
+
+    def apply(self, la: int, lb: int) -> None:
+        self.a = la * self.a
+        self.b = la * self.b + lb
+
+    def index_of(self, index: int, times: int) -> int:
+        a = self.calc_a(times)
+        b = self.calc_b(a)
+        return (a * index + b) % self.n
+
+    def get(self, index: int, times: int) -> int:
+        a = self.calc_a(times)
+        b = self.calc_b(a)
+        return ((index - b) * self.inv(a)) % self.n
+
+    def calc_a(self, times: int) -> int:
+        return pow(self.a, times, self.n)
+
+    def calc_b(self, a: int) -> int:
+        return (self.b * (a - 1) * self.inv(self.a - 1)) % self.n
+
+    def inv(self, a: int) -> int:
+        return pow(a, self.n - 2, self.n)
+
 
 DEAL = "deal with increment "
 CUT = "cut "
 NEW_STACK = "deal into new stack"
 
 
-class Processors:
-    def __init__(self, raw_values):
-        self.processors = [self.__create(raw_value) for raw_value in raw_values]
+@dataclass(frozen=True)
+class Processor:
+    line: str
 
-    def apply(self, deck):
-        for processor in self.processors:
-            processor(deck)
-
-    @staticmethod
-    def __create(raw_value):
-        if raw_value.startswith(DEAL):
-            argument = int(raw_value[len(DEAL) :])
-            return lambda deck: deck.deal(argument)
-        elif raw_value.startswith(CUT):
-            argument = int(raw_value[len(CUT) :])
-            return lambda deck: deck.cut(argument)
-        elif raw_value == NEW_STACK:
-            return lambda deck: deck.deal_into_new()
+    def apply(self, deck: Deck) -> None:
+        if self.line.startswith(DEAL):
+            argument = int(self.line[len(DEAL) :])
+            deck.deal(argument)
+        elif self.line.startswith(CUT):
+            argument = int(self.line[len(CUT) :])
+            deck.cut(argument)
+        elif self.line == NEW_STACK:
+            deck.deal_into_new()
         else:
-            raise Exception("Unknown processor = {}".format(raw_value))
+            raise Exception(f"Unknown processor = {self.line}")
 
 
-class Deck:
-    def __init__(self, n):
-        # Start in factory order, 0 at start up to n - 1
-        self.__n = n
-        self.__a = 1
-        self.__b = 0
-
-    def deal_into_new(self):
-        self.apply(-1, -1)
-
-    def cut(self, n):
-        self.apply(1, -n)
-
-    def deal(self, n):
-        self.apply(n, 0)
-
-    def apply(self, la, lb):
-        self.__a = la * self.__a
-        self.__b = la * self.__b + lb
-
-    def index_of(self, index, times):
-        a = self.calc_a(times)
-        b = self.calc_b(a)
-        return (a * index + b) % self.__n
-
-    def get(self, index, times):
-        a = self.calc_a(times)
-        b = self.calc_b(a)
-        return ((index - b) * self.inv(a)) % self.__n
-
-    def calc_a(self, times):
-        return pow(self.__a, times, self.__n)
-
-    def calc_b(self, a):
-        return (self.__b * (a - 1) * self.inv(self.__a - 1)) % self.__n
-
-    def inv(self, a):
-        return pow(a, self.__n - 2, self.__n)
-
-    def __str__(self):
-        return str(self.cards)
-
-
-def main():
+def main() -> None:
     # I have no idea how this one works, I definitely took it off the Reddits
     answer.part1(4684, process_deck(10_007).index_of(2019, 1))
     answer.part2(
@@ -79,15 +73,12 @@ def main():
     )
 
 
-def process_deck(n):
+def process_deck(n: int) -> Deck:
     deck = Deck(n)
-    processors = get_processors()
-    processors.apply(deck)
+    processors = [Processor(line) for line in Parser().lines()]
+    for processor in processors:
+        processor.apply(deck)
     return deck
-
-
-def get_processors():
-    return Processors(Parser().lines())
 
 
 if __name__ == "__main__":
--- 2020/02/solver.py
+++ 2020/02/solver.py
@@ -1,41 +1,44 @@
+from dataclasses import dataclass
+
 from aoc import answer
 from aoc.parser import Parser
 
 
+@dataclass(frozen=True)
 class PasswordEntry:
-    def __init__(self, raw_value):
-        parts = raw_value.strip().split()
+    range_start: int
+    range_end: int
+    letter: str
+    password: str
 
-        value_range = parts[0].split("-")
-        self.range_start = int(value_range[0])
-        self.range_end = int(value_range[1])
-
-        self.letter = parts[1][:-1]
-
-        self.password = parts[2]
-
-    def valid_v1(self):
+    def valid_v1(self) -> bool:
         letter_count = sum([letter == self.letter for letter in self.password])
         return letter_count >= self.range_start and letter_count <= self.range_end
 
-    def valid_v2(self):
+    def valid_v2(self) -> bool:
         letter1 = self.password[self.range_start - 1]
         letter2 = self.password[self.range_end - 1]
-
-        match1 = letter1 == self.letter
-        match2 = letter2 == self.letter
-
-        return match1 != match2
+        return (letter1 == self.letter) != (letter2 == self.letter)
 
 
-def main():
+def main() -> None:
     passwords = get_passwords()
     answer.part1(536, sum([password.valid_v1() for password in passwords]))
     answer.part2(558, sum([password.valid_v2() for password in passwords]))
 
 
-def get_passwords():
-    return [PasswordEntry(line) for line in Parser().lines()]
+def get_passwords() -> list[PasswordEntry]:
+    def parse_password(line: str) -> PasswordEntry:
+        parts = line.strip().split()
+        value_range = parts[0].split("-")
+        return PasswordEntry(
+            range_start=int(value_range[0]),
+            range_end=int(value_range[1]),
+            letter=parts[1][:-1],
+            password=parts[2],
+        )
+
+    return [parse_password(line) for line in Parser().lines()]
 
 
 if __name__ == "__main__":
