891e7f3a9fea42244f8991893398d39a25f8561a
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-14 02:08:49 +0900

    Use Core explicitly rather than opening at the top

--- commons/ocaml/reader.ml
+++ commons/ocaml/reader.ml
@@ -1,9 +1,7 @@
-open Core
-
 let get_year_day executable =
-  let parts = String.split executable ~on:'/' in
-  let name = List.last_exn parts in
-  String.lsplit2_exn name ~on:'_'
+  let parts = Core.String.split executable ~on:'/' in
+  let name = Core.List.last_exn parts in
+  Core.String.lsplit2_exn name ~on:'_'
 
 let get_filename () =
   let test = ref false in
@@ -12,23 +10,23 @@ let get_filename () =
   if !test then "sample" else "data"
 
 let get_filepath () =
-  let year, day = get_year_day (Sys.get_argv ()).(0) in
+  let year, day = get_year_day (Core.Sys.get_argv ()).(0) in
   let filename = get_filename () in
-  String.concat ~sep:"/" [ year; day; filename ^ ".txt" ]
+  Core.String.concat ~sep:"/" [ year; day; filename ^ ".txt" ]
 
 let read () : string =
   let filepath = get_filepath () in
-  In_channel.read_all filepath
+  Core.In_channel.read_all filepath
 
 let read_groups () : string list list =
   let data = read () in
   let groups = Str.split (Str.regexp "\n\n") data in
-  List.map ~f:String.split_lines groups
+  Core.List.map ~f:Core.String.split_lines groups
 
 let read_lines () : string list =
   let filepath = get_filepath () in
-  In_channel.read_lines filepath
+  Core.In_channel.read_lines filepath
 
 let read_ints () : int list =
   let lines = read_lines () in
-  List.map ~f:int_of_string lines
+  Core.List.map ~f:int_of_string lines
