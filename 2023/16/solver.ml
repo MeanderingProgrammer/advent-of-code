fd0c41524c7e2b9543da86e7343f592ee962cddd
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-17 00:49:43 +0900

    Add template for 2023/16

---
+++ 2023/16/solver.ml
@@ -0,0 +1,11 @@
+let rec print_it values =
+  match values with
+  | x :: xs ->
+      Printf.printf "%s\n" x;
+      print_it xs
+  | _ -> Printf.printf "done\n"
+
+let () =
+  let values = Aoc.Reader.read_lines () in
+  print_it values;
+  Aoc.Answer.part1 1 1 string_of_int
