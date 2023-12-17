096b04a670f4530a70ccac5e001164fe8e860ba0
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-17 10:52:49 +0900

    Finish migrating 2023/14 to use core, requires making point module compatible with Hashtbl

--- commons/ocaml/point.ml
+++ commons/ocaml/point.ml
@@ -2,6 +2,14 @@ open Core
 
 type t = { x : int; y : int }
 
+let compare = Stdlib.compare
+let equal (p1 : t) (p2 : t) : bool = Int.equal p1.x p2.x && Int.equal p1.y p2.y
+let hash (p : t) : int = Hashtbl.hash (p.x, p.y)
+
+let sexp_of_t (p : t) =
+  Sexp.List
+    [ Sexp.Atom "x"; Int.sexp_of_t p.x; Sexp.Atom "y"; Int.sexp_of_t p.y ]
+
 let adjacent (p : t) : (Direction.t * t) list =
   [
     (Direction.UP, { x = p.x; y = p.y - 1 });
@@ -12,5 +20,3 @@ let adjacent (p : t) : (Direction.t * t) list =
 
 let distance (p1 : t) (p2 : t) : int =
   Int.abs (p1.x - p2.x) + Int.abs (p1.y - p2.y)
-
-let equal (p1 : t) (p2 : t) : bool = Int.equal p1.x p2.x && Int.equal p1.y p2.y
