0a38b14eb54252d9c7530ae58c34b967ff2429e1
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-17 10:11:05 +0900

    Adopt the t convention for type definitions, remove more uses of open

--- commons/ocaml/direction.ml
+++ commons/ocaml/direction.ml
@@ -1,7 +1,6 @@
-type direction = UP | DOWN | LEFT | RIGHT
+type t = UP | DOWN | LEFT | RIGHT
 
-let to_string (d : direction) : string =
+let to_string (d : t) : string =
   match d with UP -> "UP" | DOWN -> "DOWN" | LEFT -> "LEFT" | RIGHT -> "RIGHT"
 
-let equal (d1 : direction) (d2 : direction) : bool =
-  String.equal (to_string d1) (to_string d2)
+let equal (d1 : t) (d2 : t) : bool = String.equal (to_string d1) (to_string d2)
