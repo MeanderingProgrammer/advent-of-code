0fcc35f568725fdce8183bef67da815f002544d1
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-23 12:48:39 -0800

    Finish 2023/18

--- 2023/18/solver.ml
+++ 2023/18/solver.ml
@@ -1,13 +1,9 @@
 open Core
-open Printf
 
-type plan = { direction : Aoc.Direction.t; amount : int; color : string }
+type plan = { direction : Aoc.Direction.t; amount : int }
 
-let plan_string (p : plan) : string =
-  sprintf "%s %d %s" (Aoc.Direction.to_string p.direction) p.amount p.color
-
-(* U *)
-let parse_direction (s : string) : Aoc.Direction.t =
+(* U | D | L | R *)
+let parse_direct_dir (s : string) : Aoc.Direction.t =
   match s with
   | "U" -> UP
   | "D" -> DOWN
@@ -15,50 +11,68 @@ let parse_direction (s : string) : Aoc.Direction.t =
   | "R" -> RIGHT
   | _ -> raise (Invalid_argument s)
 
-(* <direction> 6 (#70c710) *)
-let parse_plan (s : string) : plan =
+(* <direction> 6 _ *)
+let parse_direct (s : string) : plan =
   match String.split s ~on:' ' with
-  | [ direction; amount; color ] ->
-      {
-        direction = parse_direction direction;
-        amount = int_of_string amount;
-        color = String.sub color ~pos:2 ~len:(String.length color - 3);
-      }
+  | [ direction; amount; _ ] ->
+      { direction = parse_direct_dir direction; amount = int_of_string amount }
   | _ -> raise (Invalid_argument s)
 
-let follow_plan (point : Aoc.Point.t) (p : plan) =
-  let rec helper (point : Aoc.Point.t) (direction : Aoc.Direction.t)
-      (amount : int) : Aoc.Point.t list =
-    match Int.equal amount 0 with
-    | true -> []
-    | false ->
-        let next =
-          match direction with
-          | UP -> { point with y = point.y - 1 }
-          | DOWN -> { point with y = point.y + 1 }
-          | LEFT -> { point with x = point.x - 1 }
-          | RIGHT -> { point with x = point.x + 1 }
-        in
-        printf "P = %s\n" (Aoc.Point.to_string next);
-        next :: helper next direction (amount - 1)
-  in
-  helper point p.direction p.amount
+(* 0 | 1 | 2 | 3 *)
+let parse_color_dir (c : char) : Aoc.Direction.t =
+  match c with
+  | '0' -> RIGHT
+  | '1' -> DOWN
+  | '2' -> LEFT
+  | '3' -> UP
+  | _ -> raise (Invalid_argument (Char.to_string c))
 
-let rec follow (current : Aoc.Point.t) (explored : Aoc.Point.t list)
-    (plans : plan list) : Aoc.Point.t list =
+(* _ _ (#70c710) *)
+let parse_color (s : string) : plan =
+  match String.split s ~on:' ' with
+  | [ _; _; color ] ->
+      let direction = String.get color 7 in
+      let hex = "0x" ^ String.sub color ~pos:2 ~len:5 in
+      { direction = parse_color_dir direction; amount = int_of_string hex }
+  | _ -> raise (Invalid_argument s)
+
+let plan_edge (point : Aoc.Point.t) (p : plan) : Aoc.Point.t =
+  match p.direction with
+  | UP -> { point with y = point.y - p.amount }
+  | DOWN -> { point with y = point.y + p.amount }
+  | LEFT -> { point with x = point.x - p.amount }
+  | RIGHT -> { point with x = point.x + p.amount }
+
+let rec plan_edges (current : Aoc.Point.t) (plans : plan list) :
+    Aoc.Point.t list =
   match plans with
-  | [] -> explored
+  | [] -> []
   | x :: xs ->
-      let additional = follow_plan current x in
-      let next = List.last_exn additional in
-      follow next (additional @ explored) xs
+      let edge = plan_edge current x in
+      edge :: plan_edges edge xs
+
+let shoelace (edges : Aoc.Point.t list) : int =
+  let rec helper (edges : Aoc.Point.t list) : int =
+    match edges with
+    | p1 :: p2 :: xs ->
+        let value = (p1.x * p2.y) - (p2.x * p1.y) in
+        let perimeter = Int.abs (p2.x - p1.x + p2.y - p1.y) in
+        value + perimeter + helper (p2 :: xs)
+    | _ -> 0
+  in
+  let result = helper edges in
+  (result / 2) + 1
+
+let calculate_area (plans : plan list) : int =
+  let start : Aoc.Point.t = { x = 0; y = 0 } in
+  let edges = start :: plan_edges start plans in
+  shoelace edges
 
 let () =
   let values = Aoc.Reader.read_lines () in
-  let plans = List.map ~f:parse_plan values in
-  printf "%s\n" (String.concat ~sep:"\n" (List.map ~f:plan_string plans));
-  let start : Aoc.Point.t = { x = 0; y = 0 } in
-  let points = follow start [ start ] plans in
-  printf "RES = %s\n"
-    (String.concat ~sep:"\n" (List.map ~f:Aoc.Point.to_string points));
-  Aoc.Answer.part1 1 1 string_of_int
+  let direct = List.map ~f:parse_direct values in
+  let color = List.map ~f:parse_color values in
+  let part1 = calculate_area direct in
+  let part2 = calculate_area color in
+  Aoc.Answer.part1 48795 part1 string_of_int;
+  Aoc.Answer.part2 40654918441248 part2 string_of_int
