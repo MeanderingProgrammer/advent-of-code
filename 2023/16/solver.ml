ff9921922ffbdbb21baf5a56a7f77c623122a02f
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-23 13:05:33 -0800

    Minor edits to 2023/16

--- 2023/16/solver.ml
+++ 2023/16/solver.ml
@@ -46,23 +46,30 @@ let move (position : Aoc.Point.t) (direction : Aoc.Direction.t) : Aoc.Point.t =
 let next_state (s : state) (direction : Aoc.Direction.t) : state =
   { position = move s.position direction; direction }
 
-let next_states grid s =
+let next_states (grid : Aoc.Grid.t) s =
   let directions = next_directions grid s in
   List.map ~f:(next_state s) directions
 
-let grid_contains grid s = List.Assoc.mem ~equal:Aoc.Point.equal grid s.position
+let grid_contains (grid : Aoc.Grid.t) s =
+  List.Assoc.mem ~equal:Aoc.Point.equal grid s.position
+
 let unexplored explored s = not (List.mem ~equal:state_equal explored s)
 
-let move_states grid explored states =
+let move_states (grid : Aoc.Grid.t) (explored : state list)
+    (states : state list) =
   let states = List.concat (List.map ~f:(next_states grid) states) in
   let states = List.filter ~f:(grid_contains grid) states in
   List.filter ~f:(unexplored explored) states
 
-let rec follow grid explored current =
+let rec follow (grid : Aoc.Grid.t) (explored : state list)
+    (current : state list) =
   let next = move_states grid explored current in
   if List.is_empty next then explored else follow grid (next @ explored) next
 
-let rec get_energized explored =
+let get_explored (grid : Aoc.Grid.t) (start : state) : state list =
+  follow grid [ start ] [ start ]
+
+let rec get_energized (explored : state list) : Aoc.Point.t list =
   match explored with
   | [] -> []
   | x :: xs ->
@@ -72,11 +79,14 @@ let rec get_energized explored =
       else position :: energized
 
 let () =
+  (* For part 2 we can probably ignore positions that were explored as part of *)
+  (* other starting points *)
   let grid = Aoc.Reader.read_grid () in
   let start =
     { position = { x = 0; y = 0 }; direction = Aoc.Direction.RIGHT }
   in
-  let explored = follow grid [ start ] [ start ] in
+  let explored = get_explored grid start in
   let energized = get_energized explored in
   let part1 = List.length energized in
-  Aoc.Answer.part1 8901 part1 string_of_int
+  (* Aoc.Answer.part1 8901 part1 string_of_int; *)
+  Aoc.Answer.part1 46 part1 string_of_int
