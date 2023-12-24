ae55eaf6b263e7b6bf8981f79fc16a8a32a2a505
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-24 15:03:29 -0800

    Use ocaml preprocessor to derive various point module methods

--- commons/ocaml/direction.ml
+++ commons/ocaml/direction.ml
@@ -1,6 +1,4 @@
-type t = UP | DOWN | LEFT | RIGHT
+type t = UP | DOWN | LEFT | RIGHT [@@deriving equal]
 
 let to_string (d : t) : string =
   match d with UP -> "UP" | DOWN -> "DOWN" | LEFT -> "LEFT" | RIGHT -> "RIGHT"
-
-let equal (d1 : t) (d2 : t) : bool = String.equal (to_string d1) (to_string d2)
--- commons/ocaml/point.ml
+++ commons/ocaml/point.ml
@@ -1,15 +1,8 @@
 open Core
 
-type t = { x : int; y : int }
+type t = { x : int; y : int } [@@deriving compare, equal, hash, sexp_of]
 
-let compare = Stdlib.compare
-let equal (p1 : t) (p2 : t) : bool = Int.equal p1.x p2.x && Int.equal p1.y p2.y
-let hash (p : t) : int = Hashtbl.hash (p.x, p.y)
 let to_string (p : t) : string = Printf.sprintf "(%d, %d)" p.x p.y
-
-let sexp_of_t (p : t) =
-  Sexp.List
-    [ Sexp.Atom "x"; Int.sexp_of_t p.x; Sexp.Atom "y"; Int.sexp_of_t p.y ]
 
 let adjacent (p : t) : (Direction.t * t) list =
   [
