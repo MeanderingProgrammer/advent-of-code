1989986d39cae9b655ef12e43955e3b22a6b032e
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-25 12:34:11 -0800

    Add ppx preprocessor to all existing dune files and ocaml template so all solutions can derrive implementations for local modules

--- commons/ocaml/direction.ml
+++ commons/ocaml/direction.ml
@@ -1,4 +1,4 @@
-type t = UP | DOWN | LEFT | RIGHT [@@deriving equal]
+type t = UP | DOWN | LEFT | RIGHT [@@deriving compare, equal, hash, sexp]
 
 let to_string (d : t) : string =
   match d with UP -> "UP" | DOWN -> "DOWN" | LEFT -> "LEFT" | RIGHT -> "RIGHT"
