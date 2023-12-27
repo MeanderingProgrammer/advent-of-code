441266e2460fb06ae81d1abc887c43f835ff4073
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-12-27 01:26:51 -0800

    Update grid string function to handle grid that do not start at 0,0 and grid with holes

--- commons/ocaml/grid.ml
+++ commons/ocaml/grid.ml
@@ -12,6 +12,7 @@ let parse_grid (lines : string list) : t =
   let grid = List.concat (List.mapi ~f:grid_line lines) in
   Hashtbl.of_alist_exn (module Point) grid
 
+let min (grid : t) : Point.t = Point.min (Hashtbl.keys grid)
 let max (grid : t) : Point.t = Point.max (Hashtbl.keys grid)
 
 let find_value (grid : t) (value : char) : Point.t =
@@ -20,12 +21,16 @@ let find_value (grid : t) (value : char) : Point.t =
   assert (Int.equal 1 (List.length points));
   List.hd_exn points
 
+let get (grid : t) (p : Point.t) : char =
+  match Hashtbl.find grid p with None -> '.' | Some ch -> ch
+
 let to_string (grid : t) : string =
-  let max_point = max grid in
-  let xs = List.init (max_point.x + 1) ~f:Util.identity in
-  let ys = List.init (max_point.y + 1) ~f:Util.identity in
+  let min_p = min grid in
+  let max_p = max grid in
+  let xs = List.init (max_p.x - min_p.x + 1) ~f:(fun x -> x + min_p.x) in
+  let ys = List.init (max_p.y - min_p.y + 1) ~f:(fun y -> y + min_p.y) in
   let row (y : int) : string =
-    let values = List.map ~f:(fun x -> Hashtbl.find_exn grid { x; y }) xs in
+    let values = List.map ~f:(fun x -> get grid { x; y }) xs in
     String.of_char_list values
   in
   String.concat ~sep:"\n" (List.map ~f:row ys)
