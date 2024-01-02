d1c242950f8e0494978d030475ff1641601bf94e
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-02 15:30:56 -0800

    Add explanations for quadratic formula solution in 2023/21

--- 2023/21/solver.rs
+++ 2023/21/solver.rs
@@ -55,21 +55,33 @@ fn main() {
     points.insert(garden.start.clone());
     let (part1, points) = garden.step_n(points, 64);
 
-    // Honestly no clue, something about patterns in a growing diamond shape
+    // Quadratic pattern in a growing diamond shape
     // https://www.reddit.com/r/adventofcode/comments/18nevo3/comment/keaiiq7
-    let (a0, points) = garden.step_n(points, 1);
-    let (a1, points) = garden.step_n(points, garden.len);
-    let (a2, _) = garden.step_n(points, garden.len);
-    let iterations = 26501365 / garden.len;
-    let part2 = part2_magic(a0, a1, a2, iterations);
+    let (f0, points) = garden.step_n(points, 1);
+    let (f1, points) = garden.step_n(points, garden.len);
+    let (f2, _) = garden.step_n(points, garden.len);
+    let n = (26501365 - 65) / garden.len;
+    let part2 = solve_quadratic([f0, f1, f2], n);
 
     answer::part1(3847, part1);
     answer::part2(637537341306357, part2);
 }
 
-fn part2_magic(a0: i64, a1: i64, a2: i64, n: i64) -> i64 {
-    let b0 = a0;
-    let b1 = a1 - a0;
-    let b2 = a2 - a1 - b1;
-    b0 + (b1 * n) + (n * (n - 1) / 2 * b2)
+fn solve_quadratic(f: [i64; 3], n: i64) -> i64 {
+    // f(x) = ax² + bx + c = plots in 65 + 131 * x
+    // Solving for c is easy since we have f[0]
+    // f[0] = a(0)² + b(0) + c -> c = f[0]
+    // Solve for b in terms of a using f[1] & c = f[0]
+    // f[1] = a(1)² + b(1) + f[0]
+    // b = f[1] - f[0] - a
+    // Solve for a using f[2], b, & c = f[0]
+    // f[2] = a(2)² + b(2) + f[0]
+    // 4a + 2b = f[2] - f[0]
+    // 4a + 2(f[1] - f[0] - a) = f[2] - f[0]
+    // 4a + 2f[1] - 2f[0] - 2a = f[2] - f[0]
+    // a = (f[2] -2f[1] + f[0]) / 2
+    let a = (f[2] - (2 * f[1]) + f[0]) / 2;
+    let b = f[1] - f[0] - a;
+    let c = f[0];
+    a * n * n + b * n + c
 }
