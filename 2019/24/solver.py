42ba224da6a0829f4d034eac27dd8f49868657bc
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-02 23:45:10 -0800

    Optimize 2019/24 by tracking active bugs and incrementing adjacent locations rather than using all of space and finding the number of adjacent bugs. About a 4x improvement.

--- 2019/24/solver.py
+++ 2019/24/solver.py
@@ -5,124 +5,90 @@ from aoc import answer
 from aoc.parser import Parser
 
 Point = tuple[int, int]
-Grid = dict[Point, str]
+Location = tuple[Point, int]
 
-ALIVE, EMPTY, MIDDLE = "#", ".", (2, 2)
-
-
-def up(p: Point) -> tuple[str, Point]:
-    return ("u", (p[0], p[1] + 1))
+MIDDLE = (2, 2)
 
 
-def down(p: Point) -> tuple[str, Point]:
-    return ("d", (p[0], p[1] - 1))
+def neighbors(p: Point) -> list[Point]:
+    return [
+        (p[0], p[1] + 1),
+        (p[0], p[1] - 1),
+        (p[0] - 1, p[1]),
+        (p[0] + 1, p[1]),
+    ]
 
 
-def left(p: Point) -> tuple[str, Point]:
-    return ("l", (p[0] - 1, p[1]))
+def valid(p: Point) -> bool:
+    return p[0] >= 0 and p[0] <= 4 and p[1] >= 0 and p[1] <= 4
 
 
-def right(p: Point) -> tuple[str, Point]:
-    return ("r", (p[0] + 1, p[1]))
+INNER: list[list[Point]] = [
+    [(x, 0) for x in range(5)],  # bottom row of points
+    [(x, 4) for x in range(5)],  # top row of points
+    [(4, y) for y in range(5)],  # right most points
+    [(0, y) for y in range(5)],  # left most points
+]
 
-
-INNER: dict[str, list[Point]] = dict(
-    u=[(x, 0) for x in range(5)],  # bottom row of points
-    d=[(x, 4) for x in range(5)],  # top row of points
-    l=[(4, y) for y in range(5)],  # right most points
-    r=[(0, y) for y in range(5)],  # left most points
-)
-
-OUTER: dict[str, Point] = dict(
-    u=up(MIDDLE)[1],
-    d=down(MIDDLE)[1],
-    l=left(MIDDLE)[1],
-    r=right(MIDDLE)[1],
-)
+OUTER: list[Point] = neighbors(MIDDLE)
 
 
 @dataclass(frozen=True)
 class Layout:
-    grids: list[Grid]
+    grid: set[Location]
     recursive: bool
 
     def step(self) -> Self:
-        grids = self.grids
-        if self.recursive:
-            grids = [self.new_grid()] + grids + [self.new_grid()]
+        counts: dict[Location, int] = dict()
+        for point, depth in self.grid:
+            for direction, adjacent in enumerate(neighbors(point)):
+                for location in self.adjacent(depth, direction, adjacent):
+                    if valid(location[0]):
+                        counts[location] = counts.get(location, 0) + 1
+        next_grid: set[Location] = set()
+        for location, count in counts.items():
+            if count == 1 or (count == 2 and location not in self.grid):
+                next_grid.add(location)
+        return type(self)(next_grid, self.recursive)
 
-        next_grids = []
-        for level, grid in enumerate(grids):
-            next_grid = dict()
-            for point, value in grid.items():
-                if self.recursive and point == MIDDLE:
-                    continue
-                bugs = self.bugs_adjacent(grids, level, point)
-                bug_values = [1] if value == ALIVE else [1, 2]
-                next_grid[point] = ALIVE if bugs in bug_values else EMPTY
-            next_grids.append(next_grid)
-        return type(self)(next_grids, self.recursive)
-
-    def new_grid(self) -> Grid:
-        return {point: EMPTY for point in self.grids[0]}
-
-    def bugs_adjacent(self, grids: list[Grid], level: int, point: Point) -> int:
-        bugs = 0
-        for direction, adjacent in [up(point), down(point), left(point), right(point)]:
-            if self.recursive and adjacent == MIDDLE:
-                if level - 1 >= 0:
-                    bugs += Layout.bugs(grids[level - 1], INNER[direction])
-            elif self.recursive and adjacent not in grids[level]:
-                if level + 1 < len(grids):
-                    bugs += Layout.bugs(grids[level + 1], [OUTER[direction]])
-            else:
-                bugs += Layout.bugs(grids[level], [adjacent])
-        return bugs
-
-    @staticmethod
-    def bugs(grid: Grid, points: list[Point]) -> int:
-        return sum([grid.get(point) == ALIVE for point in points])
+    def adjacent(self, depth: int, direction: int, adjacent: Point) -> list[Location]:
+        if self.recursive and adjacent == MIDDLE:
+            return [(point, depth - 1) for point in INNER[direction]]
+        elif self.recursive and not valid(adjacent):
+            return [(OUTER[direction], depth + 1)]
+        else:
+            return [(adjacent, depth)]
 
     def diversity(self) -> int:
-        return sum([pow(2, (point[1] * 5) + point[0]) for point in self.bug_points()])
+        return sum([pow(2, (y * 5) + x) for ((x, y), _) in self.grid])
 
-    def count_bugs(self) -> int:
-        return len(self.bug_points())
-
-    def bug_points(self) -> list[Point]:
-        points = []
-        for grid in self.grids:
-            bugs = [point for point, value in grid.items() if value == ALIVE]
-            points.extend(bugs)
-        return points
+    def bugs(self) -> int:
+        return len(self.grid)
 
 
 def main() -> None:
-    answer.part1(32776479, part_1().diversity())
-    answer.part2(2017, part_2().count_bugs())
+    grid: set[Location] = set()
+    for point, value in Parser().as_grid().items():
+        if value == "#":
+            grid.add(((point.x(), point.y()), 0))
+    answer.part1(32776479, part_1(grid))
+    answer.part2(2017, part_2(grid))
 
 
-def part_1() -> Layout:
-    seen = []
-    layout = Layout([get_grid()], False)
+def part_1(start_grid: set[Location]) -> int:
+    seen: list[Layout] = []
+    layout = Layout(start_grid, False)
     while layout not in seen:
         seen.append(layout)
         layout = layout.step()
-    return layout
+    return layout.diversity()
 
 
-def part_2() -> Layout:
-    layout = Layout([get_grid()], True)
+def part_2(start_grid: set[Location]) -> int:
+    layout = Layout(start_grid, True)
     for _ in range(200):
         layout = layout.step()
-    return layout
-
-
-def get_grid() -> Grid:
-    grid = dict()
-    for point, value in Parser().as_grid().items():
-        grid[(point.x(), point.y())] = value
-    return grid
+    return layout.bugs()
 
 
 if __name__ == "__main__":
