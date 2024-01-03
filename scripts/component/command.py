323b455d7ce827f63543cdb562344d0344026e82
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-03 04:11:00 -0800

    Strip stdout/stderr outputs of command

--- scripts/component/command.py
+++ scripts/component/command.py
@@ -5,10 +5,10 @@ def execute(command: list[str]) -> str:
     if len(command) == 0:
         return ""
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    error = result.stderr.decode()
+    error = result.stderr.decode().strip()
     if len(error) != 0:
         print(error)
-    output = result.stdout.decode()
+    output = result.stdout.decode().strip()
     if len(output) != 0:
         print(output)
     if result.returncode != 0:
