3869b22dc655a28024e8479cd38ba1508e8022de
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-03 20:41:42 -0800

    Use plotly sunburst graph to show yearly percentages along with days, update figure saver to handle saving both types

--- scripts/command/graph.py
+++ scripts/command/graph.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-from component.figure_saver import FigureSaver
+import plotly.express as px
+from component.figure_saver import FigType, FigureSaver
 
 
 @dataclass(frozen=True)
@@ -28,35 +28,41 @@ class Grapher:
             archive_directory.mkdir(parents=True, exist_ok=False)
         saver = FigureSaver(archive=self.archive, archive_directory=archive_directory)
 
+        runtimes["all"] = "ALL"
+        runtimes["runtime"] = runtimes["runtime"].round(3)
         self.__create_graphs(runtimes, saver)
 
     def __create_graphs(self, runtimes: pd.DataFrame, saver: FigureSaver) -> None:
         saver.save(
             name="runtime_language",
-            ax=runtimes.boxplot(column="runtime", by="language", figsize=(8, 8)),
+            fig=runtimes.boxplot(column="runtime", by="language", figsize=(8, 8)),
+            fig_type=FigType.MATPLOTLIB,
         )
         saver.save(
             name="runtime_year",
-            ax=runtimes.boxplot(column="runtime", by="year", figsize=(10, 8)),
+            fig=runtimes.boxplot(column="runtime", by="year", figsize=(10, 8)),
+            fig_type=FigType.MATPLOTLIB,
         )
 
-        yearly_runtimes = runtimes.groupby("year")["runtime"].sum()
-
-        def format_label(pct: float) -> str:
-            absolute = (pct / 100.0) * sum(yearly_runtimes)
-            return f"{pct:.2f}%\n({absolute:.2f} seconds)"
-
+        yearly_runtimes = px.sunburst(
+            runtimes,
+            path=["all", "year", "day"],
+            values="runtime",
+            width=1000,
+            height=1000,
+        )
+        yearly_runtimes.update_traces(textinfo="label+percent parent+value", sort=False)
         saver.save(
             name="year_percentage",
-            ax=yearly_runtimes.plot.pie(
-                autopct=format_label, colors=sns.color_palette("tab10"), figsize=(8, 8)
-            ),
+            fig=yearly_runtimes,
+            fig_type=FigType.PLOTLY,
         )
 
         language_counts = runtimes["language"].value_counts()
         saver.save(
             name="usage_langauage",
-            ax=language_counts.plot.pie(y=1, autopct="%.2f", figsize=(8, 6)),
+            fig=language_counts.plot.pie(y=1, autopct="%.2f", figsize=(8, 6)),
+            fig_type=FigType.MATPLOTLIB,
         )
 
         yearly_usage = pd.pivot_table(
@@ -69,6 +75,7 @@ class Grapher:
         )
         saver.save(
             name="usage_langauage_yearly",
-            ax=yearly_usage.plot.bar(stacked=True, legend=False, figsize=(10, 6)),
+            fig=yearly_usage.plot.bar(stacked=True, legend=False, figsize=(10, 6)),
+            fig_type=FigType.MATPLOTLIB,
             legend=dict(loc="upper center", ncol=5),
         )
--- scripts/component/figure_saver.py
+++ scripts/component/figure_saver.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
+from enum import StrEnum, auto
 from pathlib import Path
 from typing import Any, Optional
 
 import matplotlib.pyplot as plt
+
+
+class FigType(StrEnum):
+    MATPLOTLIB = auto()
+    PLOTLY = auto()
 
 
 @dataclass(frozen=True)
@@ -10,17 +16,24 @@ class FigureSaver:
     archive: bool
     archive_directory: Optional[Path]
 
-    def save(self, name: str, ax: Any, legend: Optional[dict] = None) -> None:
+    def save(
+        self, name: str, fig: Any, fig_type: FigType, legend: Optional[dict] = None
+    ) -> None:
         fig_path = Path(f"images/{name}.png")
         self.archive_figure(fig_path)
+
         if fig_path.exists():
             print(f"Skipping {fig_path} as it already exists")
         else:
-            if legend is not None:
-                ax.get_figure().legend(**legend)
-            plt.tight_layout()
-            ax.get_figure().savefig(str(fig_path))
-        ax.get_figure().clear()
+            if fig_type == FigType.MATPLOTLIB:
+                self.save_matplotlib(fig, legend, fig_path)
+            if fig_type == FigType.PLOTLY:
+                self.save_plotly(fig, fig_path)
+            else:
+                raise Exception(f"Unhandled figure type: {fig_type}")
+
+        if fig_type == FigType.MATPLOTLIB:
+            fig.get_figure().clear()
 
     def archive_figure(self, fig_path: Path) -> None:
         if not fig_path.exists() or not self.archive:
@@ -28,3 +41,12 @@ class FigureSaver:
         assert self.archive_directory is not None
         assert self.archive_directory.exists()
         fig_path.rename(self.archive_directory.joinpath(fig_path.name))
+
+    def save_matplotlib(self, fig: Any, legend: Optional[dict], fig_path: Path) -> None:
+        if legend is not None:
+            fig.get_figure().legend(**legend)
+        plt.tight_layout()
+        fig.get_figure().savefig(str(fig_path))
+
+    def save_plotly(self, fig: Any, fig_path: Path) -> None:
+        fig.write_image(file=str(fig_path), format="png")
