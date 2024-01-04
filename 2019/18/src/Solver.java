53c024f0f99bc6ba7d30b6bee54ca5ba9e8dd7a2
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-04 13:02:03 -0800

    Minor edit to 2019/18

--- 2019/18/src/Solver.java
+++ 2019/18/src/Solver.java
@@ -5,13 +5,8 @@ import maze.Maze;
 public class Solver {
 
   public static void main(String[] args) {
-    Answer.part1(5402, solve(args, false));
-    Answer.part2(2138, solve(args, true));
-  }
-
-  private static int solve(String[] args, boolean splitMaze) {
-    FileReader fileReader = new FileReader(args);
-    Maze maze = new Maze(fileReader.read(), splitMaze);
-    return maze.complete();
+    var lines = new FileReader(args).read();
+    Answer.part1(5402, new Maze(lines, false).complete());
+    Answer.part2(2138, new Maze(lines, true).complete());
   }
 }
