aca39e6996e7e44f155a82455d7de5da84e294cf
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-05 02:32:58 -0800

    Print output on failure

--- scripts/component/command.py
+++ scripts/component/command.py
@@ -5,9 +5,9 @@ def execute(command: list[str]) -> str:
     if len(command) == 0:
         return ""
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = result.stdout.decode()
     if result.returncode != 0:
-        error_message = result.stderr.decode()
-        print(error_message)
+        print(output)
+        print(result.stderr.decode())
         exit(1)
-    else:
-        return result.stdout.decode()
+    return output
