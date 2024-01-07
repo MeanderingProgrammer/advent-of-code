986ca6b56f5a8b2ca4a342e19a0a8bc2164306d0
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-06 23:57:58 -0800

    Rewrite 2019/23 in Rust, 1.5 seconds to 0.02

---
+++ 2019/23/solver.rs
@@ -0,0 +1,147 @@
+use aoc_lib::answer;
+use aoc_lib::int_code::{Bus, Computer};
+use aoc_lib::reader;
+use std::collections::VecDeque;
+
+#[derive(Debug, Clone)]
+struct Packet {
+    dest: usize,
+    x: i64,
+    y: i64,
+}
+
+#[derive(Debug)]
+struct Network {
+    nat: Option<Packet>,
+    nodes: Vec<Node>,
+}
+
+impl Network {
+    fn new(memory: Vec<i64>) -> Self {
+        let nodes = (0..50)
+            .map(|i| {
+                let mut node = Node {
+                    computer: Computer::new(NodeBus::new(i), memory.clone()),
+                };
+                let packets = node.run();
+                assert_eq!(true, packets.is_empty());
+                node
+            })
+            .collect();
+        Self { nat: None, nodes }
+    }
+
+    fn run_until_repeat(&mut self) -> Vec<i64> {
+        let mut history = Vec::new();
+        loop {
+            let packets: Vec<Packet> = self.nodes.iter_mut().flat_map(|node| node.run()).collect();
+            packets.iter().for_each(|packet| self.send_packet(packet));
+            if packets.is_empty() {
+                let nat = self.nat.as_ref().unwrap().clone();
+                let destination = nat.y;
+                let seen = history.contains(&destination);
+                history.push(destination);
+                if seen {
+                    return history;
+                }
+                self.send_packet(&Packet {
+                    dest: 0,
+                    x: nat.x,
+                    y: nat.y,
+                });
+            }
+        }
+    }
+
+    fn send_packet(&mut self, packet: &Packet) {
+        if packet.dest == 255 {
+            self.nat = Some(packet.clone());
+        } else {
+            let node = &mut self.nodes[packet.dest];
+            node.send_packet(packet);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Node {
+    computer: Computer<NodeBus>,
+}
+
+impl Node {
+    fn run(&mut self) -> Vec<Packet> {
+        self.computer.bus.running = true;
+        self.computer.run();
+        self.computer.bus.pull_packets()
+    }
+
+    fn send_packet(&mut self, packet: &Packet) {
+        self.computer.bus.send_packet(packet);
+    }
+}
+
+#[derive(Debug)]
+struct NodeBus {
+    packets: VecDeque<i64>,
+    running: bool,
+    buffer: Vec<i64>,
+    outbound_packets: Vec<Packet>,
+}
+
+impl NodeBus {
+    fn new(id: i64) -> Self {
+        Self {
+            packets: [id].into(),
+            running: true,
+            buffer: Vec::new(),
+            outbound_packets: Vec::new(),
+        }
+    }
+
+    fn pull_packets(&mut self) -> Vec<Packet> {
+        let packets = self.outbound_packets.clone();
+        self.outbound_packets.clear();
+        packets
+    }
+
+    fn send_packet(&mut self, packet: &Packet) {
+        self.packets.push_back(packet.x);
+        self.packets.push_back(packet.y);
+    }
+}
+
+impl Bus for NodeBus {
+    fn active(&self) -> bool {
+        self.running
+    }
+
+    fn get_input(&mut self) -> i64 {
+        if self.packets.is_empty() {
+            self.running = false;
+            -1
+        } else {
+            self.packets.pop_front().unwrap()
+        }
+    }
+
+    fn add_output(&mut self, value: i64) {
+        self.buffer.push(value);
+        if self.buffer.len() == 3 {
+            let dest = self.buffer[0] as usize;
+            let (x, y) = (self.buffer[1], self.buffer[2]);
+            self.buffer.clear();
+            self.outbound_packets.push(Packet { dest, x, y });
+        }
+    }
+}
+
+fn main() {
+    answer::timer(solution);
+}
+
+fn solution() {
+    let mut network = Network::new(reader::read_csv());
+    let history = network.run_until_repeat();
+    answer::part1(16549, *history.first().unwrap());
+    answer::part2(11462, *history.last().unwrap());
+}
--- README.md
+++ README.md
@@ -233,7 +233,6 @@ a_graph \
 | ---- | --- | ------- | -------- | ------------------------------------------------------- |
 | 2021 | 23  | 1.856   | Go       | Looks like a complex path finding problem               |
 | 2022 | 16  | 1.723   | Rust     | Path finding with multiple agents is fairly optimized   |
-| 2019 | 23  | 1.583   | Python   | Kinda annoying int-code that hasn't been moved to Rust  |
 | 2016 | 14  | 1.391   | Go       | Already batched implementation of md5                   |
 | 2019 | 18  | 1.245   | Java     | Can likely minimize state to make lookups faster        |
 | 2016 | 5   | 1.220   | Go       | Already batched implementation of md5                   |
