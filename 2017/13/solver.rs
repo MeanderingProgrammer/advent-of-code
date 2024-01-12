7d08154f15827281aa97151dd8e1aaaade283062
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-12 14:24:34 -0800

    Rewrite 2017/13 in Rust, 670ms to 9ms

---
+++ 2017/13/solver.rs
@@ -0,0 +1,65 @@
+use aoc_lib::answer;
+use aoc_lib::reader;
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Scanner {
+    layer: usize,
+    layer_range: usize,
+    roundtrip: usize,
+}
+
+impl FromStr for Scanner {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (layer, layer_range) = s.split_once(": ").unwrap();
+        Ok(Self::new(
+            layer.parse().unwrap(),
+            layer_range.parse().unwrap(),
+        ))
+    }
+}
+
+impl Scanner {
+    fn new(layer: usize, layer_range: usize) -> Self {
+        Self {
+            layer,
+            layer_range,
+            roundtrip: (layer_range - 1) * 2,
+        }
+    }
+
+    fn caught(&self, offset: usize) -> bool {
+        (self.layer + offset) % self.roundtrip == 0
+    }
+
+    fn severity(&self) -> usize {
+        self.layer * self.layer_range
+    }
+}
+
+fn main() {
+    answer::timer(solution);
+}
+
+fn solution() {
+    let scanners: Vec<Scanner> = reader::read(|line| line.parse().unwrap());
+    answer::part1(632, trip_severity(&scanners));
+    answer::part2(3849742, find_wait(&scanners));
+}
+
+fn trip_severity(scanners: &Vec<Scanner>) -> usize {
+    scanners
+        .iter()
+        .filter(|scanner| scanner.caught(0))
+        .map(|scanner| scanner.severity())
+        .sum()
+}
+
+fn find_wait(scanners: &Vec<Scanner>) -> usize {
+    (0..)
+        .filter(|&offset| !scanners.iter().any(|scanner| scanner.caught(offset)))
+        .next()
+        .unwrap()
+}
