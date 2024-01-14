9910bec3161ac4d2f2f0bc9af554c25d44c9f9ab
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-14 08:06:35 -0800

    Update python point implementations to use NamedTuple instead of frozen dataclasses

--- 2017/03/solver.py
+++ 2017/03/solver.py
@@ -1,30 +1,8 @@
 from aoc import answer
+from aoc.board import Direction, Point
 from aoc.parser import Parser
 
-Point = tuple[int, int]
 Grid = dict[Point, int]
-TO_POINT: dict[str, Point] = dict(
-    u=(0, 1),
-    d=(0, -1),
-    r=(1, 0),
-    l=(-1, 0),
-)
-NEXT_DIRECTION: dict[str, str] = dict(
-    r="u",
-    u="l",
-    l="d",
-    d="r",
-)
-
-
-def add(p1: Point, p2: Point) -> Point:
-    return (p1[0] + p2[0], p1[1] + p2[1])
-
-
-def adjacent(p: Point) -> list[Point]:
-    all_directions = list(TO_POINT.values())
-    all_directions.extend([(1, 1), (1, -1), (-1, 1), (-1, -1)])
-    return [add(p, direction) for direction in all_directions]
 
 
 @answer.timer
@@ -35,21 +13,17 @@ def main() -> None:
 
 
 def build_grid(goal: int, updater) -> tuple[int, int]:
-    point: Point = (0, 0)
+    point = Point(0, 0)
+    direction: Direction = Direction.RIGHT
     value: int = 1
     grid: Grid = {point: value}
-    direction: str = "r"
     while value < goal:
-        point = add(point, TO_POINT[direction])
+        point = point.go(direction)
         value = updater(value, grid, point)
         grid[point] = value
-        next_direction = NEXT_DIRECTION[direction]
-        direction = (
-            direction
-            if add(point, TO_POINT[next_direction]) in grid
-            else next_direction
-        )
-    return abs(point[0]) + abs(point[1]), value
+        next_direction = Direction.counter_clockwise(direction)
+        direction = direction if point.go(next_direction) in grid else next_direction
+    return len(point), value
 
 
 def part1(previous: int, grid: Grid, point: Point) -> int:
@@ -57,7 +31,7 @@ def part1(previous: int, grid: Grid, point: Point) -> int:
 
 
 def part2(_: int, grid: Grid, point: Point) -> int:
-    return sum([grid.get(neighbor, 0) for neighbor in adjacent(point)])
+    return sum([grid.get(neighbor, 0) for neighbor in point.neighbors_diagonal()])
 
 
 if __name__ == "__main__":
