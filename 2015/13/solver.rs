cfc3c87e9fb0f6b0a100ea5ce65fd4fa591729c1
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-17 22:42:33 -0800

    Make 2015/13 about 3x faster by using lifetimes and references

--- 2015/13/solver.rs
+++ 2015/13/solver.rs
@@ -3,27 +3,26 @@ use aoc_lib::reader;
 use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 
-type Graph = HashMap<(String, String), i64>;
+type Graph<'a> = HashMap<(&'a str, &'a str), i64>;
 
 #[derive(Debug)]
-struct People {
-    people: Vec<String>,
+struct People<'a> {
+    people: Vec<&'a str>,
+    graph: &'a Graph<'a>,
 }
 
-impl People {
-    fn score(&self, graph: &Graph) -> i64 {
+impl<'a> People<'a> {
+    fn score(&self) -> i64 {
         self.people
             .iter()
             .enumerate()
-            .map(|(i, person)| self.get(person, graph, i - 1) + self.get(person, graph, i + 1))
+            .map(|(i, person)| self.get(person, i - 1) + self.get(person, i + 1))
             .sum()
     }
 
-    fn get(&self, person: &str, graph: &Graph, i: usize) -> i64 {
+    fn get(&self, person: &str, i: usize) -> i64 {
         let neighbor = &self.people[(i + self.people.len()) % self.people.len()];
-        *graph
-            .get(&(person.to_string(), neighbor.to_string()))
-            .unwrap_or(&0)
+        *self.graph.get(&(person, neighbor)).unwrap_or(&0)
     }
 }
 
@@ -32,38 +31,34 @@ fn main() {
 }
 
 fn solution() {
-    let graph = get_graph();
+    let mut graph: Graph = HashMap::new();
+    let lines = reader::read_lines();
+    lines.iter().for_each(|line| {
+        let parts: Vec<&str> = line[0..line.len() - 1].split_whitespace().collect();
+        let multiplier: i64 = match parts[2] {
+            "gain" => 1,
+            "lose" => -1,
+            _ => panic!("Unknown multiplier"),
+        };
+        graph.insert(
+            (parts[0], parts[10]),
+            multiplier * parts[3].parse::<i64>().unwrap(),
+        );
+    });
     answer::part1(709, max_score(&graph, false));
     answer::part2(668, max_score(&graph, true));
 }
 
-fn get_graph() -> Graph {
-    let mut graph = HashMap::new();
-    reader::read_lines().into_iter().for_each(|line| {
-        let parts: Vec<&str> = line[0..line.len() - 1].split_whitespace().collect();
-        let multiplier: Option<i64> = match parts[2] {
-            "gain" => Some(1),
-            "lose" => Some(-1),
-            _ => None,
-        };
-        graph.insert(
-            (parts[0].to_string(), parts[10].to_string()),
-            multiplier.unwrap() * parts[3].parse::<i64>().unwrap(),
-        );
-    });
-    graph
-}
-
 fn max_score(graph: &Graph, include_self: bool) -> i64 {
-    let mut all_people: HashSet<String> = graph.keys().map(|pair| pair.0.clone()).collect();
+    let mut all_people: HashSet<&str> = graph.keys().map(|pair| pair.0).collect();
     if include_self {
-        all_people.insert("Myself".to_string());
+        all_people.insert("Myself");
     }
     let num_people = all_people.len();
     all_people
         .into_iter()
         .permutations(num_people)
-        .map(|people| People { people }.score(graph))
+        .map(|people| People { people, graph }.score())
         .max()
         .unwrap()
 }
