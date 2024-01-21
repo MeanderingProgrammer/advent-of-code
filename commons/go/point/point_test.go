dbbe3bc5dc3318f5289b60b9716ff0dbe67e89dd
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-21 01:12:06 -0800

    Use assert module for unit testing in go

--- commons/go/point/point_test.go
+++ commons/go/point/point_test.go
@@ -1,6 +1,7 @@
 package point
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -8,7 +9,5 @@ func TestAdd(t *testing.T) {
 	p1 := Point{X: 3, Y: 6}
 	p2 := p1.Add(-1, 2)
 	expected := Point{X: 2, Y: 8}
-	if p2 != expected {
-		t.Errorf("got %+v, wanted %+v", p2, expected)
-	}
+	assert.Equal(t, p2, expected)
 }
