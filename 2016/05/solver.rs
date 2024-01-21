cf622465f8bd87d7b6e47b6ee6734405ddefa584
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-21 00:57:36 -0800

    Update rust implementation of 2016/5 to use threads. Runs in about 1.5 seconds so still slower than go version, but much better

--- 2016/05/solver.rs
+++ 2016/05/solver.rs
@@ -1,61 +1,55 @@
 use aoc_lib::answer;
 use aoc_lib::reader::Reader;
+use itertools::Itertools;
 use md5;
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
+use std::sync::Mutex;
+use std::thread;
 
+#[derive(Debug)]
+struct State {
+    prefix: String,
+    index: AtomicUsize,
+    done: AtomicBool,
+}
+
+#[derive(Debug, Default)]
 struct Password {
-    state: [Option<char>; 8],
+    data: Vec<(usize, char)>,
+    filled: Vec<u32>,
+    result: [char; 8],
 }
 
 impl Password {
-    fn new() -> Self {
-        Self { state: [None; 8] }
-    }
-
     fn done(&self) -> bool {
-        !self.state.iter().any(|ch| ch.is_none())
+        self.filled.len() == self.result.len()
     }
 
-    fn next_index(&self) -> usize {
-        self.state.iter().filter(|ch| ch.is_some()).count()
-    }
-
-    fn can_set(&self, index: usize) -> bool {
-        index < self.state.len() && self.state[index].is_none()
-    }
-
-    fn set(&mut self, index: usize, value: Option<char>) {
-        self.state[index] = value;
-    }
-
-    fn to_string(&self) -> String {
-        self.state.iter().map(|ch| ch.unwrap()).collect()
-    }
-}
-
-trait PasswordPopulator {
-    fn populate(&self, password: &mut Password, hash_chars: &mut impl Iterator<Item = char>);
-}
-
-struct Part1;
-
-impl PasswordPopulator for Part1 {
-    fn populate(&self, password: &mut Password, hash_chars: &mut impl Iterator<Item = char>) {
-        let index = password.next_index();
-        password.set(index, hash_chars.next());
-    }
-}
-
-struct Part2;
-
-impl PasswordPopulator for Part2 {
-    fn populate(&self, password: &mut Password, hash_chars: &mut impl Iterator<Item = char>) {
-        let raw_index = hash_chars.next().unwrap().to_digit(10);
-        if raw_index.is_some() {
-            let index = usize::try_from(raw_index.unwrap()).unwrap();
-            if password.can_set(index) {
-                password.set(index, hash_chars.next());
+    fn add(&mut self, i: usize, c1: char, c2: char) {
+        self.data.push((i, c1));
+        match c1.to_digit(10) {
+            None => (),
+            Some(index) => {
+                if (index as usize) < self.result.len() && !self.filled.contains(&index) {
+                    self.filled.push(index);
+                    self.result[index as usize] = c2;
+                }
             }
         }
+    }
+
+    fn part_1(&self) -> String {
+        self.data
+            .clone()
+            .into_iter()
+            .sorted()
+            .take(self.result.len())
+            .map(|(_, ch)| ch)
+            .collect()
+    }
+
+    fn part_2(&self) -> String {
+        self.result.iter().collect()
     }
 }
 
@@ -64,27 +58,44 @@ fn main() {
 }
 
 fn solution() {
-    let door_id = Reader::default().read_line();
-    answer::part1("d4cd2ee1", &get_password(&door_id, Part1));
-    answer::part2("f2c730e5", &get_password(&door_id, Part2));
+    let password = get_password_data(Reader::default().read_line());
+    answer::part1("d4cd2ee1", &password.part_1());
+    answer::part2("f2c730e5", &password.part_2());
 }
 
-fn get_password(door_id: &str, populator: impl PasswordPopulator) -> String {
-    let mut password = Password::new();
-    let mut i = 0;
-
-    while !password.done() {
-        let hash = get_hash(door_id, i);
-        if &hash[0..5] == "00000" {
-            let mut hash_chars = hash.chars().skip(5);
-            populator.populate(&mut password, &mut hash_chars);
+fn get_password_data(prefix: String) -> Password {
+    let shared = State {
+        prefix,
+        index: AtomicUsize::new(0),
+        done: AtomicBool::new(false),
+    };
+    let password = Mutex::new(Password::default());
+    thread::scope(|scope| {
+        let threads = thread::available_parallelism().unwrap().get();
+        for _ in 0..threads {
+            scope.spawn(|| worker(&shared, &password, 1_000));
         }
-        i += 1;
-    }
-
-    password.to_string()
+    });
+    password.into_inner().unwrap()
 }
 
-fn get_hash(door_id: &str, i: i64) -> String {
-    format!("{:x}", md5::compute(format!("{}{}", door_id, i)))
+fn worker(state: &State, mutex: &Mutex<Password>, batch_size: usize) {
+    while !state.done.load(Ordering::Relaxed) {
+        let start = state.index.fetch_add(batch_size, Ordering::Relaxed);
+        for i in start..start + batch_size {
+            update_password_data(state, mutex, i);
+        }
+    }
+}
+
+fn update_password_data(state: &State, mutex: &Mutex<Password>, i: usize) {
+    let hash = format!("{:x}", md5::compute(format!("{}{i}", state.prefix)));
+    if &hash[0..5] == "00000" {
+        let mut password = mutex.lock().unwrap();
+        let mut hash_chars = hash.chars().skip(5);
+        password.add(i, hash_chars.next().unwrap(), hash_chars.next().unwrap());
+        if password.done() {
+            state.done.store(true, Ordering::Relaxed);
+        }
+    }
 }
