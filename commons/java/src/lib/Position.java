7c2ee35270958c3a5cef5d4875534eca71ff9097
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-21 23:39:05 -0800

    Minor edits

--- commons/java/src/lib/Position.java
+++ commons/java/src/lib/Position.java
@@ -4,11 +4,15 @@ import java.util.Set;
 
 public record Position(int x, int y) {
 
+    public Position dx(int dx) {
+        return new Position(x + dx, y);
+    }
+
+    public Position dy(int dy) {
+        return new Position(x, y + dy);
+    }
+
     public Set<Position> adjacent() {
-        return Set.of(
-                new Position(x + 1, y),
-                new Position(x - 1, y),
-                new Position(x, y + 1),
-                new Position(x, y - 1));
+        return Set.of(dx(1), dx(-1), dy(1), dy(-1));
     }
 }
