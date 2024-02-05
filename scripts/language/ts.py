47dcc4d67eb28a6ffe4290ba8d8d442d65be97af
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-02-05 15:25:15 -0800

    Put things in place to be able to run TypeScript solutions using Bun

---
+++ scripts/language/ts.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, field
+from typing import override
+
+from language.language import Language
+from pojo.day import Day
+
+
+@dataclass(kw_only=True, init=False)
+class TypeScript(Language):
+    name: str = field(default="ts", repr=False)
+    solution_file: str = field(default="solver.ts", repr=False)
+
+    @override
+    def test_command(self) -> list[str]:
+        return []
+
+    @override
+    def build_commands(self) -> list[list[str]]:
+        return [["bun", "install"]]
+
+    @override
+    def run_command(self, day: Day, run_args: list[str]) -> list[str]:
+        return ["bun", "run", str(self.solution_path(day))]
+
+    @override
+    def template_processing(self, day: Day) -> None:
+        pass
