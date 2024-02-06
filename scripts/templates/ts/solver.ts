c82b84042cd922021f629cc600b64129ab260837
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-02-05 16:45:58 -0800

    Add typescript template solution

---
+++ scripts/templates/ts/solver.ts
@@ -0,0 +1,9 @@
+import { Answer, Reader } from "aoc";
+
+async function main(): Promise<void> {
+  const data = await new Reader().read();
+  console.log(data);
+  Answer.part1(1, 1);
+}
+
+await Answer.timer(main);
