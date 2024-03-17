81e7eb1b0b367f1b1e87d84475d47478464be1a4
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-03-17 14:48:04 -0700

    Use capture_output and text arguments in call to subprocess

--- scripts/component/command.py
+++ scripts/component/command.py
@@ -4,11 +4,11 @@ import subprocess
 def execute(command: list[str]) -> str:
     if len(command) == 0:
         return ""
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    error = result.stderr.decode().strip()
+    result = subprocess.run(command, capture_output=True, text=True)
+    error = result.stderr.strip()
     if len(error) != 0:
         print(error)
-    output = result.stdout.decode().strip()
+    output = result.stdout.strip()
     if len(output) != 0:
         print(output)
     if result.returncode != 0:
