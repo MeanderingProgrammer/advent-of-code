0d88f12ab683f1b37041552f5959bd4601fbcdf0
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-04-22 20:45:48 -0700

    Rewrite 2019/18 in rust, about 850ms to about 500ms

---
+++ 2019/18/solver.rs
@@ -0,0 +1,197 @@
+use aoc_lib::answer;
+use aoc_lib::grid::Grid;
+use aoc_lib::point::{Heading, Point};
+use aoc_lib::reader::Reader;
+use aoc_lib::search::Dijkstra;
+use fxhash::{FxHashMap, FxHashSet};
+use std::collections::{BTreeSet, VecDeque};
+
+#[derive(Debug, Clone)]
+enum Element {
+    Start,
+    Empty,
+    Key(char),
+    Door(char),
+}
+
+impl Element {
+    fn from_ch(ch: char) -> Option<Self> {
+        if ch == '#' {
+            None
+        } else if ch == '@' {
+            Some(Element::Start)
+        } else if ch == '.' {
+            Some(Element::Empty)
+        } else if ch.is_ascii_lowercase() {
+            Some(Element::Key(ch))
+        } else if ch.is_ascii_uppercase() {
+            Some(Element::Door(ch.to_ascii_lowercase()))
+        } else {
+            None
+        }
+    }
+
+    fn is_start(&self) -> bool {
+        matches!(self, Self::Start)
+    }
+
+    fn is_key(&self) -> bool {
+        matches!(self, Self::Key(_))
+    }
+
+    fn is_main(&self) -> bool {
+        self.is_start() || self.is_key()
+    }
+}
+
+#[derive(Debug)]
+struct Path {
+    point: Point,
+    key: char,
+    need: BTreeSet<char>,
+    distance: i64,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct State {
+    points: Vec<Point>,
+    have: BTreeSet<char>,
+}
+
+#[derive(Debug)]
+struct Maze<'a> {
+    graph: FxHashMap<&'a Point, Vec<Path>>,
+    keys: usize,
+}
+
+impl<'a> Dijkstra for Maze<'a> {
+    type T = State;
+
+    fn done(&self, node: &State) -> bool {
+        node.have.len() == self.keys
+    }
+
+    fn neighbors(&self, node: &State) -> impl Iterator<Item = (State, i64)> {
+        node.points.iter().enumerate().flat_map(move |(i, point)| {
+            self.graph[point]
+                .iter()
+                .filter(|path| !node.have.contains(&path.key))
+                .filter(|path| path.need.iter().all(|ch| node.have.contains(ch)))
+                .map(move |path| {
+                    let mut next_points = node.points.clone();
+                    next_points[i] = path.point.clone();
+                    let mut next_have = node.have.clone();
+                    next_have.insert(path.key);
+                    let next_state = State {
+                        points: next_points,
+                        have: next_have,
+                    };
+                    (next_state, path.distance)
+                })
+        })
+    }
+}
+
+fn main() {
+    answer::timer(solution);
+}
+
+fn solution() {
+    let grid = Reader::default().read_grid(Element::from_ch);
+    answer::part1(5402, solve(&grid));
+    answer::part2(2138, solve(&split(grid.clone())));
+}
+
+fn split(mut grid: Grid<Element>) -> Grid<Element> {
+    let start = grid
+        .points()
+        .into_iter()
+        .find(|point| grid.get(point).is_start())
+        .unwrap()
+        .clone();
+
+    grid.remove(&start);
+    grid.remove(&(&start + &Heading::North));
+    grid.remove(&(&start + &Heading::South));
+    grid.remove(&(&start + &Heading::East));
+    grid.remove(&(&start + &Heading::West));
+
+    grid.add(&start + &Heading::NorthEast, Element::Start);
+    grid.add(&start + &Heading::NorthWest, Element::Start);
+    grid.add(&start + &Heading::SouthEast, Element::Start);
+    grid.add(&start + &Heading::SouthWest, Element::Start);
+
+    grid
+}
+
+fn solve(grid: &Grid<Element>) -> i64 {
+    let mut graph = FxHashMap::default();
+    grid.points()
+        .into_iter()
+        .filter(|point| grid.get(point).is_main())
+        .for_each(|point| {
+            let paths = get_paths(grid, point);
+            graph.insert(point, paths);
+        });
+
+    let keys = grid
+        .values()
+        .into_iter()
+        .filter(|value| value.is_key())
+        .count();
+
+    let maze = Maze { graph, keys };
+    let start = State {
+        points: grid
+            .points()
+            .into_iter()
+            .filter(|point| grid.get(point).is_start())
+            .cloned()
+            .collect(),
+        have: BTreeSet::new(),
+    };
+    maze.run(start).unwrap()
+}
+
+fn get_paths(grid: &Grid<Element>, start: &Point) -> Vec<Path> {
+    let mut queue = VecDeque::new();
+    queue.push_back(((start.clone(), BTreeSet::new()), 0));
+    let mut seen = FxHashSet::default();
+    let mut result = Vec::new();
+    while !queue.is_empty() {
+        let ((point, need), distance) = queue.pop_front().unwrap();
+
+        if seen.contains(&point) {
+            continue;
+        }
+        seen.insert(point.clone());
+
+        if &point != start {
+            if let Element::Key(ch) = grid.get(&point) {
+                result.push(Path {
+                    point: point.clone(),
+                    key: *ch,
+                    need: need.clone(),
+                    distance,
+                });
+            }
+        }
+
+        for adjacent in point.neighbors() {
+            if !seen.contains(&adjacent) {
+                match grid.get_or(&adjacent) {
+                    Some(Element::Door(ch)) => {
+                        let mut next_need = need.clone();
+                        next_need.insert(*ch);
+                        queue.push_back(((adjacent, next_need), distance + 1));
+                    }
+                    Some(_) => {
+                        queue.push_back(((adjacent, need.clone()), distance + 1));
+                    }
+                    None => (),
+                }
+            }
+        }
+    }
+    result
+}
