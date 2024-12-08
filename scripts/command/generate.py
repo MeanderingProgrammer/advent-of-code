1aeb1b8afeacac81a1bab58406af125e4f59efe5
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-12-08 11:00:41 -0800

    Update generate script to automatically push new data to git

--- scripts/command/generate.py
+++ scripts/command/generate.py
@@ -26,69 +26,77 @@ class Generator(Command):
 
     @override
     def run(self) -> None:
-        self.setup_language()
-        self.setup_data_files()
+        self.setup_solution()
+        self.setup_data()
 
-    def setup_language(self) -> None:
+    def setup_solution(self) -> None:
         # Create day directory, okay if it already exists
         self.day.dir().mkdir(parents=True, exist_ok=True)
         # Copy over language template if not already present
         solution_path = self.language.solution_path(self.day)
         if solution_path.exists():
             print(f"Solution already exists under: {solution_path}")
-        else:
-            self.copy_template_files()
-            self.language.template_processing(self.day)
+            return
+        self.copy_template()
+        self.language.template_processing(self.day)
 
-    def copy_template_files(self) -> None:
-        template_directory = Path(f"scripts/templates/{self.language.name}")
-        if not template_directory.is_dir():
-            raise Exception(f"No template defined in {template_directory}")
-        for template_file in template_directory.glob("**/*"):
-            if template_file.is_file():
-                file_path = template_file.relative_to(template_directory)
-                destination = self.day.dir().joinpath(file_path)
-                # Create parent directory if needed for nested solution files
-                if not destination.parent.exists():
-                    destination.parent.mkdir(parents=True)
-                if not destination.exists():
-                    print(f"Copying {template_file} to {destination}")
-                    shutil.copy(template_file, destination)
+    def copy_template(self) -> None:
+        template_dir = Path(f"scripts/templates/{self.language.name}")
+        if not template_dir.is_dir():
+            raise Exception(f"No template defined in {template_dir}")
+        for template_file in template_dir.glob("**/*"):
+            if not template_file.is_file():
+                continue
+            file_path = template_file.relative_to(template_dir)
+            destination = self.day.dir().joinpath(file_path)
+            # Create parent directory if needed for nested solution files
+            if not destination.parent.exists():
+                destination.parent.mkdir(parents=True)
+            if not destination.exists():
+                print(f"Copying {template_file} to {destination}")
+                shutil.copy(template_file, destination)
 
-    def setup_data_files(self) -> None:
+    def setup_data(self) -> None:
         # Create data directory, okay if it already exists
         data_dir = Path("data").joinpath(self.day.dir())
         data_dir.mkdir(parents=True, exist_ok=True)
         # Download the necessary files
-        data_created = self.pull_aoc_file("-I -i", data_dir.joinpath("data.txt"))
-        if data_created:
-            Generator.create_empty_file(data_dir.joinpath("sample.txt"))
+        created = self.pull_aoc_file("-I -i", data_dir.joinpath("data.txt"))
+        if created:
+            Generator.empty_file(data_dir.joinpath("sample.txt"))
+            push: list[str] = [
+                "cd data",
+                "git add .",
+                f"git commit -m 'Add input for {self.day.dir()}'",
+                "git push",
+            ]
+            os.system(" && ".join(push))
         if self.puzzle:
             self.pull_aoc_file("-P -p", data_dir.joinpath("puzzle.md"))
 
-    def pull_aoc_file(self, flags: str, file_path: Path) -> bool:
-        if file_path.exists():
-            print(f"{file_path} already exists, leaving as is")
+    def pull_aoc_file(self, flags: str, file: Path) -> bool:
+        if file.exists():
+            print(f"{file} already exists, leaving as is")
             return False
-        print(f"Creating file at: {file_path}")
-        advent_cookie_file = ".adventofcode.session"
-        if Path(advent_cookie_file).exists() and shutil.which("aoc") is not None:
+        print(f"Creating file at: {file}")
+        cookie = ".adventofcode.session"
+        if Path(cookie).exists() and shutil.which("aoc") is not None:
             print("Downloading input using aoc-cli")
-            download_command = [
+            download: list[str] = [
                 "aoc download",
                 f"-y {self.day.year}",
                 f"-d {self.day.day}",
-                f"-s ./{advent_cookie_file}",
-                f"{flags} {file_path}",
+                f"-s ./{cookie}",
+                f"{flags} {file}",
             ]
-            os.system(" ".join(download_command))
+            os.system(" ".join(download))
         else:
             print("aoc-cli is not setup")
-            Generator.create_empty_file(file_path)
+            Generator.empty_file(file)
         return True
 
     @staticmethod
-    def create_empty_file(file_path: Path) -> None:
-        if not file_path.exists():
-            print(f"Creating empty {file_path}")
-            file_path.touch()
+    def empty_file(file: Path) -> None:
+        if not file.exists():
+            print(f"Creating empty {file}")
+            file.touch()
