81278b410100462e8aadcd18e1a41e7b78a55198
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-01-13 08:37:25 -0800

    Remove tempfile dependency, store test-data files in repo

--- commons/rust/tests/reader_test.rs
+++ commons/rust/tests/reader_test.rs
@@ -1,12 +1,10 @@
 use aoc_lib::reader::Reader;
-use std::io::Write;
-use tempfile::NamedTempFile;
 
 #[test]
 fn test_group_int() {
     assert_eq!(
         vec![vec![123, 45], vec![-68, -9, 0]],
-        reader(&new_file(&["123", "45", "", "-68", "-9", "0"])).read_group_int()
+        reader("group-ints.txt").read_group_int()
     );
 }
 
@@ -14,7 +12,7 @@ fn test_group_int() {
 fn test_group_lines() {
     assert_eq!(
         vec![vec!["ab", "cd"], vec!["efg", "hi", "j"]],
-        reader(&new_file(&["ab", "cd", "", "efg", "hi", "j"])).read_group_lines()
+        reader("group-strings.txt").read_group_lines()
     );
 }
 
@@ -22,53 +20,41 @@ fn test_group_lines() {
 fn test_full_groups() {
     assert_eq!(
         vec!["ab\ncd", "efg\nhi\nj"],
-        reader(&new_file(&["ab", "cd", "", "efg", "hi", "j"])).read_full_groups()
+        reader("group-strings.txt").read_full_groups()
     );
 }
 
 #[test]
 fn test_int() {
-    assert_eq!(
-        vec![12, -75, 105],
-        reader(&new_file(&["12", "-75", "105"])).read_int()
-    );
+    assert_eq!(vec![12, -75, 105], reader("int-lines.txt").read_int());
 }
 
 #[test]
 fn test_lines() {
-    let lines = vec!["zgsnvdmlfuplrubt", "zztdcqzqddaazdjp"];
-    assert_eq!(lines, reader(&new_file(&lines)).read_lines());
+    assert_eq!(
+        vec!["zgsnvdmlfuplrubt", "zztdcqzqddaazdjp"],
+        reader("string-lines.txt").read_lines()
+    );
 }
 
 #[test]
 fn test_line() {
-    assert_eq!("abcd", reader(&new_file(&["abcd"])).read_line());
+    assert_eq!("abcd", reader("string-line.txt").read_line());
 }
 
 #[test]
 fn test_csv() {
-    assert_eq!(
-        vec![1, 5, 9, -12],
-        reader(&new_file(&["1,5,9,-12"])).read_csv()
-    );
+    assert_eq!(vec![1, 5, 9, -12], reader("int-csv.txt").read_csv());
 }
 
 #[test]
 fn test_chars() {
     assert_eq!(
         vec!['a', 'b', 'c', 'd'],
-        reader(&new_file(&["abcd"])).read_chars()
+        reader("string-line.txt").read_chars()
     );
 }
 
-fn new_file(lines: &[&str]) -> NamedTempFile {
-    let mut file = NamedTempFile::new().unwrap();
-    for line in lines {
-        writeln!(file, "{line}").unwrap();
-    }
-    file
-}
-
-fn reader(file: &NamedTempFile) -> Reader {
-    Reader::new(file.path().to_str().unwrap().to_string())
+fn reader(file: &str) -> Reader {
+    Reader::new(format!("test-data/{file}"))
 }
