25758c9d47ffadde8b3f864ffee8678993472057
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-01-23 15:58:28 -0800

    2017/15 400ms -> 220ms by computing values for both generators in parallel before comparing

--- 2017/15/solver.rs
+++ 2017/15/solver.rs
@@ -1,34 +1,37 @@
 use aoc_lib::answer;
 use aoc_lib::reader::Reader;
+use rayon::prelude::*;
 
 #[derive(Debug)]
-struct Generator {
+struct Gen {
     value: usize,
-    wait: bool,
     factor: usize,
-    multiple: usize,
 }
 
-impl Generator {
-    fn new(value: usize, wait: bool, factor: usize, multiple: usize) -> Self {
-        Self {
-            value,
-            wait,
-            factor,
-            multiple,
+impl Gen {
+    fn compute(&mut self, multiple: u16) -> (Vec<u16>, Vec<u16>) {
+        let (n, m) = (40_000_000, 5_000_000);
+        let (mut vals, mut mods) = (Vec::with_capacity(n), Vec::with_capacity(m));
+        while !Self::full(&vals) || !Self::full(&mods) {
+            let value = self.next();
+            if !Self::full(&vals) {
+                vals.push(value);
+            }
+            if !Self::full(&mods) && value % multiple == 0 {
+                mods.push(value);
+            }
         }
+        (vals, mods)
     }
 
     fn next(&mut self) -> u16 {
-        self.calculate();
-        while self.wait && self.value % self.multiple != 0 {
-            self.calculate();
-        }
+        self.value *= self.factor;
+        self.value %= 2_147_483_647;
         self.value as u16
     }
 
-    fn calculate(&mut self) {
-        self.value = (self.value * self.factor) % 2_147_483_647;
+    fn full(values: &Vec<u16>) -> bool {
+        values.len() == values.capacity()
     }
 }
 
@@ -42,18 +45,17 @@ fn solution() {
         .iter()
         .map(|line| line.split(' ').last().unwrap().parse().unwrap())
         .collect();
-    answer::part1(592, matches(&starts, 40_000_000, false));
-    answer::part2(320, matches(&starts, 5_000_000, true));
+    let generators = [(starts[0], 16_807, 4), (starts[1], 48_271, 8)];
+    let values: Vec<(Vec<u16>, Vec<u16>)> = generators
+        .into_par_iter()
+        .map(|(value, factor, multiple)| Gen { value, factor }.compute(multiple))
+        .collect();
+    let (a, b) = (&values[0], &values[1]);
+    answer::part1(592, matches(&a.0, &b.0));
+    answer::part2(320, matches(&a.1, &b.1));
 }
 
-fn matches(starts: &[usize], n: usize, wait: bool) -> usize {
-    let mut a = Generator::new(starts[0], wait, 16_807, 4);
-    let mut b = Generator::new(starts[1], wait, 48_271, 8);
-    let mut count = 0;
-    for _ in 0..n {
-        if a.next() == b.next() {
-            count += 1;
-        }
-    }
-    count
+fn matches(a: &[u16], b: &[u16]) -> usize {
+    assert_eq!(a.len(), b.len());
+    std::iter::zip(a, b).filter(|(a, b)| a == b).count()
 }
