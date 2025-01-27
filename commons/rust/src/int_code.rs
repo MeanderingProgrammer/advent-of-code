5f7fabab5b8cdd5f7ae41419acc22b6f99d08ff4
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-01-26 22:41:13 -0800

    Update how int_code gets mode to avoid converting to and from string

--- commons/rust/src/int_code.rs
+++ commons/rust/src/int_code.rs
@@ -1,40 +1,36 @@
 #[derive(Debug)]
-struct Parameter {
-    value: i64,
-    mode: u8,
+enum Mode {
+    Position,
+    Immediate,
+    Relative,
 }
 
-impl Parameter {
-    fn new(value: i64, mode: u8) -> Self {
-        Self { value, mode }
-    }
-
-    fn get<T>(&self, computer: &Computer<T>) -> i64 {
-        match self.mode {
-            0 | 2 => computer.get(self.index(computer)),
-            1 => self.value,
-            _ => panic!("Unknown mode get: {0}", self.mode),
-        }
-    }
-
-    fn index<T>(&self, computer: &Computer<T>) -> usize {
-        match self.mode {
-            0 => self.value as usize,
-            2 => computer.base + (self.value as usize),
-            _ => panic!("Unknown mode index: {0}", self.mode),
+impl From<i64> for Mode {
+    fn from(value: i64) -> Self {
+        match value {
+            0 => Self::Position,
+            1 => Self::Immediate,
+            2 => Self::Relative,
+            mode => panic!("Unknown mode: {mode}"),
         }
     }
 }
 
 #[derive(Debug)]
+enum Method {
+    Get,
+    Idx,
+}
+
+#[derive(Debug)]
 enum Response {
-    SetValue(usize, i64),
-    Halt,
-    Store(usize),
+    Set(i64),
+    Store(i64),
     Load(i64),
-    Jump(usize),
+    Jump(i64),
     Noop,
-    AdjustBase(usize),
+    Base(i64),
+    Halt,
 }
 
 pub trait Bus {
@@ -63,6 +59,7 @@ pub struct Computer<T> {
     pub bus: T,
     memory: Vec<i64>,
     code: i64,
+    parameter: usize,
     pointer: usize,
     base: usize,
 }
@@ -76,29 +73,32 @@ where
     }
 
     fn run_next(&mut self) -> bool {
-        let response = self.next();
-        // Update pointer
-        self.pointer = match response {
-            Response::Jump(index) => index,
-            _ => self.pointer + 1,
-        };
-        // Update base
-        self.base += match response {
-            Response::AdjustBase(amount) => amount,
-            _ => 0,
-        };
-        // Interactions with bus / memory
-        match response {
-            Response::SetValue(index, value) => self.set(index, value),
+        let mut increment = true;
+        let mut done = false;
+        match self.step() {
+            Response::Set(value) => {
+                let index = self.next(Method::Idx);
+                self.set(index, value);
+            }
             Response::Store(index) => {
                 let value = self.bus.get_input();
                 self.set(index, value);
             }
-            Response::Load(value) => self.bus.add_output(value),
-            _ => (),
+            Response::Load(value) => {
+                self.bus.add_output(value);
+            }
+            Response::Jump(index) => {
+                increment = false;
+                self.pointer = index as usize;
+            }
+            Response::Noop => {}
+            Response::Base(amount) => {
+                self.base += amount as usize;
+            }
+            Response::Halt => done = true,
         };
-        // Whether or not to Halt
-        !matches!(response, Response::Halt)
+        self.pointer += if increment { 1 } else { 0 };
+        !done
     }
 }
 
@@ -117,68 +117,85 @@ impl<T> Computer<T> {
             bus,
             memory: memory.to_vec(),
             code: 0,
+            parameter: 0,
             pointer: 0,
             base: 0,
         }
     }
 
-    fn next(&mut self) -> Response {
+    fn step(&mut self) -> Response {
         self.code = self.memory[self.pointer];
+        self.parameter = 0;
         match self.code % 100 {
-            1 => {
-                let value = self.param(1).get(self) + self.param(2).get(self);
-                Response::SetValue(self.param(3).index(self), value)
-            }
-            2 => {
-                let value = self.param(1).get(self) * self.param(2).get(self);
-                Response::SetValue(self.param(3).index(self), value)
-            }
-            3 => Response::Store(self.param(1).index(self)),
-            4 => Response::Load(self.param(1).get(self)),
+            // Day 2: add
+            1 => Response::Set(self.next(Method::Get) + self.next(Method::Get)),
+            // Day 2: multiply
+            2 => Response::Set(self.next(Method::Get) * self.next(Method::Get)),
+            // Day 5: input
+            3 => Response::Store(self.next(Method::Idx)),
+            // Day 5: output
+            4 => Response::Load(self.next(Method::Get)),
+            // Day 5: jump if true
             5 => {
-                let is_true = self.param(1).get(self) != 0;
-                let p2 = self.param(2);
-                if is_true {
-                    Response::Jump(p2.get(self) as usize)
+                let (v1, v2) = (self.next(Method::Get), self.next(Method::Get));
+                if v1 != 0 {
+                    Response::Jump(v2)
                 } else {
                     Response::Noop
                 }
             }
+            // Day 5: jump if false
             6 => {
-                let is_true = self.param(1).get(self) == 0;
-                let p2 = self.param(2);
-                if is_true {
-                    Response::Jump(p2.get(self) as usize)
+                let (v1, v2) = (self.next(Method::Get), self.next(Method::Get));
+                if v1 == 0 {
+                    Response::Jump(v2)
                 } else {
                     Response::Noop
                 }
             }
+            // Day 5: less than
             7 => {
-                let is_true = self.param(1).get(self) < self.param(2).get(self);
-                Response::SetValue(self.param(3).index(self), if is_true { 1 } else { 0 })
+                let (v1, v2) = (self.next(Method::Get), self.next(Method::Get));
+                Response::Set(if v1 < v2 { 1 } else { 0 })
             }
+            // Day 5: equal
             8 => {
-                let is_true = self.param(1).get(self) == self.param(2).get(self);
-                Response::SetValue(self.param(3).index(self), if is_true { 1 } else { 0 })
+                let (v1, v2) = (self.next(Method::Get), self.next(Method::Get));
+                Response::Set(if v1 == v2 { 1 } else { 0 })
             }
-            9 => Response::AdjustBase(self.param(1).get(self) as usize),
+            // Day 9: adjust relative base
+            9 => Response::Base(self.next(Method::Get)),
+            // Day 2: halt
             99 => Response::Halt,
-            _ => panic!("Unknown code: {0}", self.code),
+            code => panic!("Unknown code: {code}"),
         }
     }
 
-    fn param(&mut self, mode_offset: usize) -> Parameter {
-        let mode = self
-            .code
-            .to_string()
-            .chars()
-            .rev()
-            .nth(mode_offset + 1)
-            .unwrap_or('0')
-            .to_digit(10)
-            .unwrap();
+    fn next(&mut self, method: Method) -> i64 {
+        self.parameter += 1;
+        let n = 10i64.pow((self.parameter + 1) as u32);
+        let mode = (self.code / n % 10).into();
         self.pointer += 1;
-        Parameter::new(self.memory[self.pointer], mode as u8)
+        let value = self.memory[self.pointer];
+        match method {
+            Method::Idx => self.index(&mode, value),
+            Method::Get => match mode {
+                Mode::Position | Mode::Relative => {
+                    let index = self.index(&mode, value);
+                    self.get(index as usize)
+                }
+                Mode::Immediate => value,
+            },
+        }
+    }
+
+    fn index(&self, mode: &Mode, value: i64) -> i64 {
+        let offset = match mode {
+            Mode::Position => 0,
+            Mode::Relative => self.base as i64,
+            Mode::Immediate => panic!("Immediate mode does not support indexing"),
+        };
+        offset + value
     }
 
     pub fn get(&self, index: usize) -> i64 {
@@ -189,7 +206,8 @@ impl<T> Computer<T> {
         }
     }
 
-    fn set(&mut self, index: usize, value: i64) {
+    fn set(&mut self, index: i64, value: i64) {
+        let index = index as usize;
         if index >= self.memory.len() {
             self.memory.resize(index + 1, 0);
         }
