cd6d55b483db97151ae7cb3650cdb6eeac323369
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-01-27 00:21:38 -0800

    2017/24 230ms -> 30ms by adding some heuristics

--- 2017/24/solver.rs
+++ 2017/24/solver.rs
@@ -1,7 +1,8 @@
 use aoc_lib::answer;
 use aoc_lib::reader::Reader;
-use fxhash::FxHashMap;
+use fxhash::{FxHashMap, FxHashSet};
 use std::cmp::Ordering;
+use std::collections::VecDeque;
 
 #[derive(Debug, Clone, Default)]
 struct Bridge(Vec<(u8, u8)>);
@@ -59,24 +60,44 @@ impl BridgeData {
 
 #[derive(Debug)]
 struct BridgeBuilder {
-    components: FxHashMap<u8, Vec<u8>>,
+    components: FxHashMap<u8, FxHashSet<u8>>,
 }
 
 impl BridgeBuilder {
-    fn build(&self) -> BridgeData {
-        let mut data = BridgeData::default();
-        self.generate(&mut data, Bridge::default());
-        data
+    fn new(lines: &[String]) -> Self {
+        let mut components: FxHashMap<u8, FxHashSet<u8>> = FxHashMap::default();
+        lines.iter().for_each(|line| {
+            let (p1, p2) = line.split_once('/').unwrap();
+            let v1: u8 = p1.parse().unwrap();
+            let v2: u8 = p2.parse().unwrap();
+            components.entry(v1).or_default().insert(v2);
+            components.entry(v2).or_default().insert(v1);
+        });
+        Self { components }
     }
 
-    fn generate(&self, data: &mut BridgeData, bridge: Bridge) {
-        data.update(&bridge);
-        let start = bridge.last();
-        for end in self.components[&start].iter() {
-            if !bridge.contains(start, *end) {
-                self.generate(data, bridge.add(start, *end));
+    fn build(&self) -> BridgeData {
+        let mut data = BridgeData::default();
+        let mut q = VecDeque::default();
+        q.push_back(Bridge::default());
+        while !q.is_empty() {
+            let bridge = q.pop_front().unwrap();
+            data.update(&bridge);
+            let start = bridge.last();
+            let ends = &self.components[&start];
+            // If there's a duplicate port, i.e. 35/35, choose it as the only
+            // option since it adds strength & length without changing the port
+            if ends.contains(&start) && !bridge.contains(start, start) {
+                q.push_back(bridge.add(start, start));
+            } else {
+                for end in ends.iter() {
+                    if !bridge.contains(start, *end) {
+                        q.push_back(bridge.add(start, *end));
+                    }
+                }
             }
         }
+        data
     }
 }
 
@@ -85,19 +106,8 @@ fn main() {
 }
 
 fn solution() {
-    let data = get_bridge_builder().build();
+    let lines = Reader::default().read_lines();
+    let data = BridgeBuilder::new(&lines).build();
     answer::part1(1656, data.strongest);
     answer::part2(1642, data.longest_strongest);
-}
-
-fn get_bridge_builder() -> BridgeBuilder {
-    let mut components: FxHashMap<u8, Vec<u8>> = FxHashMap::default();
-    Reader::default().read_lines().iter().for_each(|line| {
-        let (p1, p2) = line.split_once('/').unwrap();
-        let v1: u8 = p1.parse().unwrap();
-        let v2: u8 = p2.parse().unwrap();
-        components.entry(v1).or_default().push(v2);
-        components.entry(v2).or_default().push(v1);
-    });
-    BridgeBuilder { components }
 }
