074b212a9b36b8de2be28677b4c278b32341052c
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-06-27 14:59:18 -0700

    update java

--- buildSrc/src/main/kotlin/common.gradle.kts
+++ buildSrc/src/main/kotlin/common.gradle.kts
@@ -13,11 +13,11 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.guava:guava:33.1.0-jre")
-    compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-    testCompileOnly("org.projectlombok:lombok:1.18.32")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
+    implementation("com.google.guava:guava:33.4.8-jre")
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 testing {
