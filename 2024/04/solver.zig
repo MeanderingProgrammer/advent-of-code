7d0b4853fe0a0850a86296f8e48d9d8d97d6f4f5
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-07-14 20:57:10 -0700

    minor edit, zig pointers

--- 2024/04/solver.zig
+++ 2024/04/solver.zig
@@ -22,7 +22,7 @@ fn part1(grid: Grid) usize {
     var points = grid.points();
     while (points.next()) |point| {
         inline for (std.meta.fields(Heading)) |heading| {
-            const contains = has(grid, point, @enumFromInt(heading.value), "XMAS");
+            const contains = has(grid, point.*, @enumFromInt(heading.value), "XMAS");
             result += if (contains) 1 else 0;
         }
     }
@@ -34,19 +34,19 @@ fn part2(grid: Grid) usize {
     var points = grid.points();
     while (points.next()) |p1| {
         const p2 = p1.plus(Heading.s.point().times(2));
-        const sw = mas(grid, p1, Heading.sw);
-        const nw = mas(grid, &p2, Heading.nw);
+        const sw = mas(grid, p1.*, Heading.sw);
+        const nw = mas(grid, p2, Heading.nw);
         result += if (sw and nw) 1 else 0;
     }
     return result;
 }
 
-fn mas(grid: Grid, start: *const Point, heading: Heading) bool {
+fn mas(grid: Grid, start: Point, heading: Heading) bool {
     return has(grid, start, heading, "MAS") or has(grid, start, heading, "SAM");
 }
 
-fn has(grid: Grid, start: *const Point, heading: Heading, goal: []const u8) bool {
-    var point = start.*;
+fn has(grid: Grid, start: Point, heading: Heading, goal: []const u8) bool {
+    var point = start;
     for (goal) |ch| {
         const matches = if (grid.get(point)) |value| ch == value else false;
         if (!matches) {
